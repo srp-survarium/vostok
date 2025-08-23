9377af227a04f4d536a583af46c264ee701c6878
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2025-08-23 13:46:30 +0200

    First pass of methods related to the damage_model subsystem (#4)
    * Add methods related to the damage_model subsystem
    
    * Make objdiff match functions automatically
    
    * Add is_affect_applied
    
    * First pass on body_part_paramters
    
    * Fix of by one error
    
    * Add damage_model to temp header

---
+++ sources/vostok/game_core/affects_applying_type_enum.h
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created 	: 16.08.2025
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef AFFECTS_APPLYING_TYPE_ENUM_H_INCLUDED
+#define AFFECTS_APPLYING_TYPE_ENUM_H_INCLUDED
+
+namespace survarium {
+
+enum affects_applying_type_enum
+{
+  type_apply_directly = 0x0,
+  type_read_only = 0x1,
+};
+
+} // namespace survarium
+
+#endif // #define AFFECTS_APPLYING_TYPE_ENUM_H_INCLUDED
---
+++ sources/vostok/game_core/hit_type_parameters.h
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created 	: 12.08.2025
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef HIT_TYPE_PARAMETERS_H_INCLUDED
+#define HIT_TYPE_PARAMETERS_H_INCLUDED
+
+#include <vostok/fixed_string.h>
+#include <boost/noncopyable.hpp>
+
+namespace survarium {
+
+//////////////////////////
+// FORWARD DECLARATIONS //
+//////////////////////////
+
+class body_part_parameters;
+
+//////////////////////////
+//     DEFINITIONS      //
+//////////////////////////
+
+class hit_type_parameters : public boost::noncopyable {
+public:
+	hit_type_parameters(
+		pcstr                              type,
+		float                              absorption,
+		float                              armor,
+		float                              reduce,
+		u32                                bdb_count);
+
+	// STATE[UNVERIFIED] sushi@TODO: it was const before, now I removed it. Why was it here and why current header gen doesn't see those? Am I to blame?
+	std::pair<body_part_parameters *,float>* get_bdb_coefficients() {
+		return pointer_cast< std::pair<body_part_parameters*, float> * >( pointer_cast< pbyte >( this ) + sizeof( *this ) );
+	}
+
+	pcstr get_type() const { return m_type.c_str(); }
+
+	float get_armor() const { return m_armor; }
+
+	float get_reduce() const { return m_reduce; }
+
+	float get_absorption() const { return m_absorption_amount; }
+
+	u32 get_bdb_coeffs_count() const { return m_bdb_count; }
+
+	void apply_damage(
+		float                              delta,
+		u32                                time_in_ms);
+
+	// void remove_vertex(body_part_parameters*) /* no source */; sushi@TODO
+
+	void set_parameters(
+		float                              armor,
+		float                              reduce,
+		float                              absorbtion);
+
+public:
+	/* offset 0x0000 */ hit_type_parameters*                next;
+	/* offset 0x0004 */ vostok::fixed_string<16>              m_type;
+	/* offset 0x0020 */ float                               m_absorption_amount;
+	/* offset 0x0024 */ float                               m_armor;
+	/* offset 0x0028 */ float                               m_reduce;
+	/* offset 0x002c */ u32                                 m_bdb_count;
+}; // class hit_type_parameters
+
+namespace {
+	typedef char size_assert[
+		sizeof(hit_type_parameters) == 0x30 ? 1 : -1
+	];
+}
+
+} // namespace survarium
+
+#endif // #ifndef HIT_TYPE_PARAMETERS_H_INCLUDED
