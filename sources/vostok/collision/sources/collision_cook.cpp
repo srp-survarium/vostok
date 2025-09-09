6a0180e8312ac6f5c85002f82364a51843b8fbe9
sushi-shi <sushi.is.shy@gmail.com>
2025-09-09 14:53:38 +0200

    Match collision_cook::query_triangle_mesh to 80%

--- sources/vostok/collision/sources/collision_cook.cpp
+++ sources/vostok/collision/sources/collision_cook.cpp
@@ -38,7 +38,8 @@ void collision_cook::translate_query( resources::query_result_for_cook&	parent )
 
 void collision_cook::query_triangle_mesh ( resources::query_result_for_cook * parent_query )
 {
-	fs_new::virtual_path_string	vertices_path = parent_query->get_requested_path();
+	fs_new::virtual_path_string	vertices_path;
+	vertices_path = parent_query->get_requested_path();
 	fs_new::virtual_path_string	indices_path  = vertices_path;
 	
 	vertices_path.append("/vertices");
