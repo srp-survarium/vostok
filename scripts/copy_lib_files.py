eb265e9b642097151b9874106f59124a75a3d55c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2025-10-20 14:57:37 +0200

    Update script for copying lib files

--- scripts/copy_lib_files.py
+++ scripts/copy_lib_files.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+"""
+copy_lib_files.py - Copy 3rd party libraries ('.dll's and '.lib's) into the project.
+"""
 
 import argparse
 import shutil
@@ -6,19 +8,28 @@ from pathlib import Path
 
 EXTS = {'.pdb', '.exe', '.dll', '.a', '.lib'}
 
+SCRIPT_DIR = Path(__file__).resolve().parent
+ROOT_DIR   = (SCRIPT_DIR / ".." / "..").resolve()
+
+LIBS_DIR   = ROOT_DIR / "vostok-libs"
+VOSTOK_DIR = ROOT_DIR / "xray-orig"
+
+SRC         = LIBS_DIR   / "sources"
+DEST        = VOSTOK_DIR / "sources"
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("src", type=Path)
-    parser.add_argument("dest", type=Path)
-    parser.add_argument("--combine", action="store_true")
+    parser.add_argument("src", nargs="?", type=Path, default=SRC)
+    parser.add_argument("dest", nargs="?", type=Path, default=DEST)
+    parser.add_argument("--reverse", action="store_true", help="Swap src and dest")
     args = parser.parse_args()
 
     src = args.src.resolve()
     dest = args.dest.resolve()
-    copied = 0
-
-    if args.combine:
+    if args.reverse:
         src, dest = dest, src
+
+    copied = 0
 
     for file in src.rglob("*"):
         if file.is_file() and file.suffix.lower() in EXTS:
