e5d6628593e5d6372dfe96beaacf284ffb3555df
sushi-shi <sushi.is.shy@gmail.com>
2025-10-24 21:36:29 +0200

    Match 'http_client' class

--- sources/vostok/logging/sources/filter_tree_node_base.h
+++ sources/vostok/logging/sources/filter_tree_node_base.h
@@ -11,20 +11,19 @@
 
 namespace vostok {
 namespace logging {
-namespace filter_tree {
 
 enum	{ max_name_length = 32 };
 typedef fixed_string<max_name_length>	node_name_type;
 class	node_base;
 
-struct compare_nodes	
+struct compare_nodes
 {
 	bool	operator () (node_base const & left, node_base const & right) const;
 	bool	operator () (pcstr const left, node_base const & right) const;
 	bool	operator () (node_base const & left, pcstr const right) const;
 };
 
-typedef boost::intrusive::set_member_hook< boost::intrusive::link_mode<boost::intrusive::auto_unlink> >		
+typedef boost::intrusive::set_member_hook< boost::intrusive::link_mode<boost::intrusive::auto_unlink> >
 						nodes_member_hook;
 class node_base
 {
@@ -37,12 +36,11 @@ public:
 typedef boost::intrusive::member_hook	<	node_base, nodes_member_hook, & node_base::tree_hook	>
 						nodes_tree_option;
 
-typedef	boost::intrusive::multiset< node_base, 
-							  	 	nodes_tree_option, 
+typedef	boost::intrusive::multiset< node_base,
+							  	 	nodes_tree_option,
 							  	 	boost::intrusive::compare< compare_nodes >,
 							  	 	boost::intrusive::constant_time_size<false> >	nodes_tree_type;
 
-} // namespace filter_tree
 } // namespace logging
 } // namespace vostok
 
