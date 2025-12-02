3d75cef7a6051b02d334d0cf2e6246b29beb3ad1
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2025-12-02 10:45:20 +0100

    Match survarium::collision_geometry and survarium::collision_sensor (#73)

---
+++ sources/vostok/game_core/collision_geometry_subscriber.h
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created 	: 03.11.2025
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef COLLISION_GEOMETRY_SUBSCRIBER_H_INCLUDED
+#define COLLISION_GEOMETRY_SUBSCRIBER_H_INCLUDED
+
+#include <vostok/game_core/base_game_object.h>
+
+namespace survarium {
+
+typedef void usable_object;
+class collision_sensor;
+
+class collision_geometry_subscriber : public base_game_object {
+public:
+									~collision_geometry_subscriber	( ) {}
+
+	virtual	usable_object*			cast_to_usable	( ) { return NULL; }
+	virtual	collision_sensor*		cast_to_sensor	( ) { return NULL; }
+}; // class collision_geometry_subscriber
+
+STATIC_SIZE_ASSERT(collision_geometry_subscriber, 0x4);
+
+} // namespace survarium
+
+#endif // #ifndef COLLISION_GEOMETRY_SUBSCRIBER_H_INCLUDED
