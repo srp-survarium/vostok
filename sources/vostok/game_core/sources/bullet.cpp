2858333b8642398c4d35d9e9e0d56b00c8c633c0
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-01-02 17:18:58 +0100

    Match bullet and bullet_manager (#77)

--- sources/vostok/game_core/sources/bullet.cpp
+++ sources/vostok/game_core/sources/bullet.cpp
@@ -22,7 +22,7 @@ namespace survarium {
 float g_bullet_tracer_exposition = 0.5f;
 static console_commands::cc_float bullet_tracer_exposition( "bullet_tracer_exposition", g_bullet_tracer_exposition, 0.01f, 5.0f, true, console_commands::command_type_engine_internal );
 
-// STATE[UNCHECKED]
+// STATE[97.92%|DONE]
 bullet::bullet(
 	bullet_manager&					bullet_manager,
 	float3 const&					position,
@@ -52,7 +52,7 @@ bullet::bullet(
 	m_max_distance			( wa->distance( ) * 1000.f ),
 	m_flown_distance		( 0.0f ),
 	m_damage_factor			( wa->damage( ) ),
-	m_ricochet_angle		( wa->ricochet_angle( ) * math::pi / 180.f ), // sushi@NOTE: Where should this impl be?
+	m_ricochet_angle		( wa->ricochet_angle( ) * math::pi / 180.f ), // sushi@NOTE: wa->ricochet_angle( ) generates one more useless load
 	m_pierce_factor			( wa->pierce( ) ),
 	m_weapon_bullet_damage	( wc.get_bullet_damage( ) ),
 	m_weapon_bullet_pierce	( wc.get_bullet_pierce( ) ),
@@ -98,32 +98,6 @@ bullet::bullet( bullet const& other )
 	m_tracer_idx				= other.m_tracer_idx;
 
 	ASSERT( UNKNOWN_EXPRESSION );
-
-	// FUNCTION BODY
-	// <0x590e3a>|0x03a|+0x016:'96'
-	// <0x590e50>|0x050|+0x01c:'97'
-	// <0x590e6c>|0x06c|+0x019:'98'
-	// <0x590e85>|0x085|+0x01c:'99'
-	// <0x590ea1>|0x0a1|+0x00c:'100'
-	// <0>
-	// <0x590ead>|0x0ad|+0x00c:'102'
-	// <0x590eb9>|0x0b9|+0x00c:'103'
-	// <0x590ec5>|0x0c5|+0x00c:'104'
-	// <0x590ed1>|0x0d1|+0x00c:'105'
-	// <0x590edd>|0x0dd|+0x00c:'106'
-	// <0x590ee9>|0x0e9|+0x00c:'107'
-	// <0>
-	// <0x590ef5>|0x0f5|+0x00c:'109'
-	// <0x590f01>|0x101|+0x00c:'110'
-	// <0x590f0d>|0x10d|+0x00c:'111'
-	// <0x590f19>|0x119|+0x00c:'112'
-	// <0x590f25>|0x125|+0x00c:'113'
-	// <0x590f31>|0x131|+0x00c:'114'
-	// <0x590f3d>|0x13d|+0x00c:'115'
-	// <0x590f49>|0x149|+0x00e:'116'
-	// <0>
-	// <0x590f57>|0x157|+0x00c:'118'
-	// ******
 }
 
 // STATE[100%|DONE]
@@ -131,14 +105,14 @@ bullet::~bullet( )
 {
 }
 
-// STATE[UNCHECKED]
+// STATE[89.91%|PARTIAL]
 void bullet::tick( u32 current_time_in_ms )
 {
 	m_current_time_in_ms				= current_time_in_ms;
-	const float3	zero_velocity		= float3( 0, 0, 0 );
+	const float3	zero_velocity		= float3( 0, 0, 0 );	// sushi@MATCH: <0x592a3b> Arguments are built slightly differently.
 	u16				invalid_tracer_idx	= u16(-1);
 	float			low_time			= m_life_time;
-	float			high_time			= ( current_time_in_ms - m_born_time_in_ms ) / 1000.0f * m_bullet_manager->get_bullet_time_factor( );
+	float			high_time			= ( current_time_in_ms - m_born_time_in_ms ) / 1000.0f * m_bullet_manager->get_bullet_time_factor( ); // sushi@MATCH: <0x592a6e> target inlined `get_bullet_time_factor`.
 	ASSERT( UNKNOWN_EXPRESSION );
 
 	float3 const&	gravity				= m_bullet_manager->get_gravity( );
@@ -147,7 +121,7 @@ void bullet::tick( u32 current_time_in_ms )
 	{
 		// sushi@NOTE: Added some ifdefs here
 
-		if ( m_velocity.squared_length( ) < 1.f )
+		if ( m_velocity.squared_length( ) < 1.f )	// sushi@MATCH: <0x592aba> fld1 is used instead of movss.
 		{
 			m_start_velocity = zero_velocity;
 			return;
@@ -163,9 +137,11 @@ void bullet::tick( u32 current_time_in_ms )
 			return;
 
 		float	time					= pick_next_permissible_time( low_time, high_time, gravity );
-
 		if( low_time == time )
+		{
+			m_start_velocity = zero_velocity;
 			return;
+		}
 
 		collision_result	result	= check_collision( m_position, low_time, time );
 		if ( result == collision_result_collide )
@@ -213,49 +189,6 @@ void bullet::tick( u32 current_time_in_ms )
 		low_time			= time;
 		R_ASSERT			( low_time < high_time, "start_low[%f] high[%f]", low_time, high_time );
 	}
-
-
-
-	// FUNCTION BODY
-	// ...
-	// <0x592c13>|0x1f3|+0x012:'179'
-	// <0>
-	// <0x592c25>|0x205|+0x01d:'181'		if ( !update_bullet_position( time, gravity ) )
-	// <0>									{
-	// <0x592c42>|0x222|+0x01a:'183'			m_start_velocity = zero_velocity;
-	// <0x592c5c>|0x23c|+0x005:'184'			return;
-	// <0>									}
-	// <1>
-	// <2>
-	// <0x592c61>|0x241|+0x023:'188'	if ( math::is_similar( time, high_time ) )
-	// <0>
-	// <0x592c84>|0x264|+0x016:'190'
-	// <0>
-	// <0x592c9a>|0x27a|+0x01b:'192'
-	// <0x592cb5>|0x295|+0x00b:'193'
-	// <0x592cc0>|0x2a0|+0x023:'194'
-	// <0x592ce3>|0x2c3|+0x011:'195'
-	// <0>
-	// <0x592cf4>|0x2d4|+0x022:'197'
-	// <0>
-	// <1>
-	// <0x592d16>|0x2f6|+0x031:'200'
-	// <0x592d47>|0x327|+0x012:'201'
-	// <0>
-	// <0x592d59>|0x339|+0x017:'203'
-	// <0x592d70>|0x350|+0x00a:'204'
-	// <0>
-	// <0x592d7a>|0x35a|+0x00e:'206'
-	// <0x592d88>|0x368|+0x027:'207'
-	// <0>
-	// <0x592daf>|0x38f|+0x002:'209'
-	// <0>
-	// <1>
-	// <0x592db1>|0x391|+0x00c:'212'	R_ASSERT			( low_time < high_time, "start_low[%f] high_time[%f]", low_time, high_time );
-	// <0x592dbd>|0x39d|+0x00a:'213'	low_time			= time;
-	// <0x592dc7>|0x3a7|+0x011:'214'	R_ASSERT			( low_time < high_time, "start_low[%f] high[%f]", low_time, high_time );
-	// <0>
-	// ******
 }
 
 bool bullet::is_finish_flying ( ) const
@@ -265,25 +198,7 @@ bool bullet::is_finish_flying ( ) const
 	return m_start_velocity == zero_velocity;
 }
 
-float3 const& bullet::get_start_position ( ) const
-{
-	return m_start_position;
-}
-float3 const& bullet::get_start_velocity ( ) const
-{
-	return m_start_velocity;
-}
-
-float3 const& bullet::get_position ( ) const
-{
-	return m_position;
-}
-float3 const& bullet::get_velocity ( ) const
-{
-	return m_velocity;
-}
-
-// STATE[UNCHECKED]
+// STATE[95.60%|DONE]: LTCG for float3
 float3 bullet::compute_parabolic_velocity( float time, float3 const& gravity )
 {
 	float3 const xz_velocity	= float3( m_start_velocity.x, 0.f, m_start_velocity.z );
@@ -295,7 +210,7 @@ float3 bullet::compute_parabolic_velocity( float time, float3 const& gravity )
 	return						m_start_velocity * math::max( 0.f, 1.f - m_current_resistance * time ) + ( gravity * time );
 }
 
-// STATE[UNCHECKED]
+// STATE[99.86%|DONE]: Target allocated less stack
 float3 bullet::compute_trajectory_velocity( float time, float3 const& gravity )
 {
 	float const parabolic_time	= get_parabolic_time( );
@@ -308,7 +223,7 @@ float3 bullet::compute_trajectory_velocity( float time, float3 const& gravity )
 	return						parabolic_vel + gravity * fall_down_time;
 }
 
-// STATE[UNCHECKED]
+// STATE[97.30%|DONE]: LTCG for float3
 float3 bullet::compute_parabolic_position( float time, float3 const& gravity )
 {
 	float3 const xz_velocity	= float3( m_start_velocity.x, 0.f, m_start_velocity.z );
@@ -320,7 +235,7 @@ float3 bullet::compute_parabolic_position( float time, float3 const& gravity )
 	return					m_start_position + m_start_velocity * time + m_start_velocity * -m_current_resistance * sqr_t_div_2 + gravity * sqr_t_div_2;
 }
 
-// STATE[UNCHECKED]
+// STATE[99.95%|DONE]
 float3 bullet::compute_trajectory_position( float time, float3 const& gravity )
 {
 	float const parabolic_time	= get_parabolic_time( );
@@ -335,7 +250,7 @@ float3 bullet::compute_trajectory_position( float time, float3 const& gravity )
 	return parabolic_pos + parabolic_vel * fall_down_time + gravity * ( math::sqr( fall_down_time ) * .5f );
 }
 
-// STATE[UNCHECKED]
+// STATE[99.69%|DONE]
 float bullet::get_parabolic_time( )
 {
 	static const float air_resistance_epsilon = 1.1f;
@@ -434,8 +349,8 @@ float bullet::get_check_time_in_vacuum( float start_low, float high, float3 cons
 	return					result;
 }
 
-// STATE[UNCHECKED]
-float get_max_error_time( float t0, float t1 )
+// STATE[BLOCKED]
+inline static float get_max_error_time( float t0, float t1 )
 {
 	return					( ( t1 + t0 ) * .5f );
 	// this is correct even in our case
@@ -443,7 +358,7 @@ float get_max_error_time( float t0, float t1 )
 	// x(t) = V0x*t - V0x*ar*t^2/2
 }
 
-// STATE[UNCHECKED]
+// STATE[100%|DONE]
 float bullet::get_check_time( float const start_low, float high, float3 const& gravity )
 {
 	float const max_test_distance	= m_max_distance - m_flown_distance;
@@ -469,7 +384,7 @@ float bullet::get_check_time( float const start_low, float high, float3 const& g
 	return					low;
 }
 
-// STATE[UNCHECKED]
+// STATE[99.94%|DONE]
 float bullet::compute_max_error( float low, float high, float3 const& gravity )
 {
 	float					max_error_time = get_max_error_time( low, high );
@@ -488,7 +403,7 @@ float bullet::compute_max_error( float low, float high, float3 const& gravity )
 	return	magnitude * sine_alpha;
 }
 
-// STATE[UNCHECKED]
+// STATE[98.16%|PARTIAL]
 collision_result bullet::check_collision( float3 start_position, float start_time, float current_time )
 {
 	collision_result const result	= collision_result_no_collision;
@@ -514,62 +429,21 @@ collision_result bullet::check_collision( float3 start_position, float start_tim
 
 		float3 triangle_normal		= ray_result.hit_normal_world;
 		float cos_alpha				= triangle_normal | direction;
-		triangle_orientation orientation	= (triangle_orientation)( cos_alpha >= 0.0f );
-		bool ignorable_object_was_hit		= m_ignorable_object && ray_result.object->user_data && ray_result.object->user_data == m_ignorable_object;
+		triangle_orientation orientation	= (triangle_orientation)( 0.0f <= cos_alpha ); // sushi@MATCH: <0x5928de> Somehow done in reverse. Maybe with some inlined function.
+		bool ignorable_object_was_hit		= m_ignorable_object && ray_result.object->user_data && ray_result.object->user_data->cast_to_hit_receiver( ) == m_ignorable_object;
 
 		if ( ignorable_object_was_hit || orientation == triangle_orientation_back_face )
 		{
 			distance -= ( ray_result.hit_point_world - new_start_position ).length( );
-			new_start_position = direction * math::epsilon_5 * new_start_position.length( ) + ray_result.hit_point_world;
+			new_start_position = ray_result.hit_point_world + direction * math::epsilon_5 * new_start_position.length( );
 			continue;
 		}
 
 		return process_ray_query( ray_result, distance, start_position, direction, start_time, current_time );
 	}
-
-	// FUNCTION BODY
-	// <0x5927df>|0x00f|+0x007:'431'
-	// <0>
-	// <0x5927e6>|0x016|+0x029:'433'
-	// <0x59280f>|0x03f|+0x00c:'434'
-	// <0>
-	// <0x59281b>|0x04b|+0x012:'436'
-	// <0x59282d>|0x05d|+0x00b:'437'
-	// <0x592838>|0x068|+0x014:'438'
-	// <0x59284c>|0x07c|+0x008:'439'
-	// <0>
-	// <0x592854>|0x084|+0x015:'441'
-	// <0>
-	// <0x592869>|0x099|+0x00f:'443'	physics::world& p_world			= m_bullet_manager->get_physics_world( );
-	// <0>
-	// <0x592878>|0x0a8|+0x012:'445'	float3 new_start_position		= start_position;
-	// <0>
-	// <1>								for ( ;; ) {
-	// <0x59288a>|0x0ba|+0x024|[1]:'448'
-	// <0x5928ae>|0x0de|+0x006:'449'
-	// <0x5928b4>|0x0e4|+0x008:'450'
-	// <0>
-	// <0x5928bc>|0x0ec|+0x012:'452'
-	// <0x5928ce>|0x0fe|+0x010:'453'
-	// <0x5928de>|0x10e|+0x02d:'454'
-	// <0>
-	// <1>
-	// <2>
-	// <0x59290b>|0x13b|+0x04b:'458'		bool ignorable_object_was_hit		= m_ignorable_ob
-	// <0>
-	// <0x592956>|0x186|+0x012:'460'
-	// <0>
-	// <0x592968>|0x198|+0x01d:'462'		distance -= ( ray_result.hit_point_world - new_start_position ).length( );
-	// <0x592985>|0x1b5|+0x060:'463'		new_start_position = direction * math::epsilon_5 * new_start_position.length( ) + ray_result.hit_point_world;
-	// <0x5929e5>|0x215|+0x005:'464'		continue
-	// <0>
-	// <1>
-	// <0x5929ea>|0x21a|+0x028:'467'		return process_ray_query( ray_result, distance, start_position, direction, start_time, current_time );
-	// <0x592a12>|0x242|+0x005:'468'	}
-	// ******
 }
 
-// STATE[UNCHECKED]
+// STATE[99.16%|PARTIAL]
 collision_result bullet::process_ray_query(
 	physics::closest_ray_result const&	ray_result,
 	float								distance,
@@ -582,10 +456,10 @@ collision_result bullet::process_ray_query(
 	float3 triangle_normal				= ray_result.hit_normal_world;
 	const float distance_to_hit_point	= ( start_position - ray_result.hit_point_world ).length( );
 	float3 collide_point				= start_position + fly_direction * distance_to_hit_point;
-	float collision_time				= start_time + ( current_time - start_time ) * distance_to_hit_point / distance;
+	float collision_time				= start_time + distance_to_hit_point / distance * ( current_time - start_time );
 
 	float cos_alpha						= triangle_normal | fly_direction;
-	triangle_orientation orientation	= ( triangle_orientation )( cos_alpha >= 0.0f );
+	triangle_orientation orientation	= ( triangle_orientation )( cos_alpha >= 0.0f ); // sushi@MATCH: This again
 
 	if ( orientation == triangle_orientation_back_face )
 		return collision_result_no_collision;
@@ -607,10 +481,10 @@ collision_result bullet::process_ray_query(
 
 	float angle_alpha					= math::acos( cos_alpha );
 
-	if ( m_ricochet_count >= 2 || m_collided_material->ricochet_koef( ) * m_ricochet_angle < angle_alpha - math::pi_d2 )
-		return collide_front_face( collide_point, fly_direction, triangle_normal, speed, collision_time, start_position, start_time, current_time, ray_result );
+	if ( m_ricochet_count < 2 && angle_alpha - math::pi_d2 <=  m_collided_material->ricochet_koef( ) * m_ricochet_angle )
+		return try_reflect( collide_point, fly_direction, triangle_normal, speed, collision_time, start_position, start_time, current_time, cos_alpha );
 
-	return try_reflect( collide_point, fly_direction, triangle_normal, speed, collision_time, start_position, start_time, current_time, cos_alpha );
+	return collide_front_face( collide_point, fly_direction, triangle_normal, speed, collision_time, start_position, start_time, current_time, ray_result );
 }
 
 // STATE[100%|DONE]
@@ -657,7 +531,7 @@ void bullet::fix_collision_point_and_time(
 	}
 }
 
-// STATE[UNCHECKED]
+// STATE[94.28%|PARTIAL]
 collision_result bullet::collide_front_face(
 	float3 const&						collide_point,
 	float3 const&						bullet_direction,
@@ -673,20 +547,20 @@ collision_result bullet::collide_front_face(
 	ASSERT( UNKNOWN_EXPRESSION );
 	ASSERT( UNKNOWN_EXPRESSION );
 
-	if ( m_bullet_manager->m_engine )
+	if ( m_bullet_manager->has_engine( ) )
 	{
 		material_pair const* mtl_pair = m_bullet_manager->get_material_manager( ).get_pair( m_bullet_material->id( ), m_collided_material->id( ) );
 		if ( mtl_pair )
 		{
-			if ( mtl_pair->decal1( ) )
+			if ( mtl_pair->decal1( ) )	// sushi@MATCH: decal1 & decal1_size didn't inline normally
 				m_bullet_manager->add_decal( mtl_pair->decal1( ), mtl_pair->decal1_size( ), collide_point, bullet_direction, triangle_normal, true );
 
 			if ( mtl_pair->has_particle( ) )
 				m_bullet_manager->play_particle( mtl_pair->particle( ), collide_point, bullet_direction, triangle_normal );
 		}
 		else
-		{
-			LOG_WARNING( "material pair not exists [%s]-[%s]", m_bullet_material, m_collided_material );
+		{	// sushi@MATCH: Some kind of jump here.
+			LOG_WARNING( "material pair not exists [%s]-[%s]", m_bullet_material->name( ), m_collided_material->name( ) );
 		}
 	}
 
@@ -700,7 +574,7 @@ collision_result bullet::collide_front_face(
 
 	if ( m_collided_material->resistance( ) > m_weapon_bullet_pierce * m_pierce_factor )
 		return collision_result_collide;
-
+	// sushi@MATCH: min and max in incorrect order
 	float new_speed = math::min(	// sushi@NOTE: Use clamp instead
 		math::max( 0.0, m_weapon_bullet_pierce * m_pierce_factor / m_collided_material->resistance( ) - 1.0f ),
 		1.0f
@@ -714,7 +588,7 @@ collision_result bullet::collide_front_face(
 	current_time	-= collision_time;
 }
 
-// STATE[UNCHECKED]
+// STATE[95.43%|PARTIAL]
 collision_result bullet::try_reflect(
 	float3 const&		collide_point,
 	float3				direction,
@@ -732,7 +606,7 @@ collision_result bullet::try_reflect(
 
 	float calculated_koeff	= 1.0f - angle_alpha / fin_ricochet_angle;
 
-	speed					*= math::linear_interpolation( calculated_koeff, 1.0f, m_collided_material->reflection_speed_down( ) );
+	speed					*= math::linear_interpolation( calculated_koeff, 1.0f, m_collided_material->reflection_speed_down( ) ); // sushi@MATCH: <0x5910ae> `linear_interpolation` inlined in target.
 	if( speed < 0 )
 	{
 #ifndef MASTER_GOLD
@@ -756,47 +630,9 @@ collision_result bullet::try_reflect(
 	m_ricochet_count += 1;
 
 	return collision_result_reflected;
-
-	// FUNCTION BODY
-	// <0x591059>|0x009|+0x022:'674'
-	// <0x59107b>|0x02b|+0x018:'675'
-	// <0>
-	// <1>
-	// <0x591093>|0x043|+0x01b:'678'
-	// <0>
-	// <0x5910ae>|0x05e|+0x041:'680'
-	// <0>
-	// <1>
-	// <2>
-	// <3>
-	// <4>
-	// <5>
-	// <6>
-	// <7>
-	// <0x5910ef>|0x09f|+0x00e:'689'
-	// <0>
-	// <1>
-	// <2>
-	// <3>
-	// <4>
-	// <0x5910fd>|0x0ad|+0x00a:'695'
-	// <0>
-	// <1>
-	// <0x591107>|0x0b7|+0x063:'698'
-	// <0>
-	// <0x59116a>|0x11a|+0x026:'700'
-	// <0>
-	// <0x591190>|0x140|+0x019:'702'
-	// <0x5911a9>|0x159|+0x00b:'703'
-	// <0x5911b4>|0x164|+0x013:'704'
-	// <0>
-	// <0x5911c7>|0x177|+0x00f:'706'
-	// <0>
-	// <0x5911d6>|0x186|+0x005:'708'
-	// ******
 }
 
-// STATE[UNCHECKED]
+// STATE[91.68%|PARTIAL]
 void bullet::change_trajectory( float3 const& new_position, float3 const& new_velocity, float collision_time )
 {
 	++m_change_trajectory_count;
@@ -806,27 +642,14 @@ void bullet::change_trajectory( float3 const& new_position, float3 const& new_ve
 	m_position				= m_start_position;
 	m_velocity				= m_start_velocity;
 	m_current_resistance	= m_air_resistance;
-	m_born_time_in_ms		+= math::floor( 1000.f * collision_time / m_bullet_manager->get_bullet_time_factor( ) ); // s_bm_bullet_time_factor
+	m_born_time_in_ms		+= math::floor( 1000.f * collision_time / m_bullet_manager->get_bullet_time_factor( ) ); // sushi@MATCH: <0x590ffb> Different for many things
 	m_life_time				= 0;
-
-
-	// FUNCTION BODY
-	// <0x590f79>|0x009|+0x00f:'714'
-	// <0>
-	// <0x590f88>|0x018|+0x019:'716'
-	// <0x590fa1>|0x031|+0x019:'717'
-	// <0x590fba>|0x04a|+0x019:'718'
-	// <0x590fd3>|0x063|+0x01c:'719'
-	// <0x590fef>|0x07f|+0x00c:'720'
-	// <0x590ffb>|0x08b|+0x039:'721'
-	// <0x591034>|0x0c4|+0x010:'722'
-	// ******
 }
 
-// STATE[UNCHECKED]
+// STATE[99.26%|DONE]
 bool bullet::update_bullet_position( const float time, float3 const& gravity )
 {
-	float3 const new_position	= compute_trajectory_position( time, gravity );
+	float3 const& new_position	= compute_trajectory_position( time, gravity );
 	if ( !m_bullet_manager->is_inside_collision_db( new_position ) )
 		return					false;
 
@@ -843,39 +666,10 @@ bool bullet::update_bullet_position( const float time, float3 const& gravity )
 		return					false;
 
 	m_position					= new_position;
+	ASSERT( UNKNOWN_EXPRESSION );
 	m_life_time					= time;
 
 	return						true;
-
-
-	// FUNCTION BODY
-	// <0x591919>|0x009|+0x01d:'727'
-	// <0x591936>|0x026|+0x016:'728'
-	// <0x59194c>|0x03c|+0x007:'729'
-	// <0>
-	// <0x591953>|0x043|+0x023:'731'
-	// <0>
-	// <0x591976>|0x066|+0x011:'733'	if ( m_flown_distance >= m_max_distance )
-	// <0>
-	// <0x591987>|0x077|+0x00c:'735'
-	// <0x591993>|0x083|+0x007:'736'
-	// <0>
-	// <1>
-	// <0x59199a>|0x08a|+0x030:'739'	m_velocity
-	// <0x5919ca>|0x0ba|+0x022:'740'
-	// <0x5919ec>|0x0dc|+0x004:'741'
-	// <0>
-	// <0x5919f0>|0x0e0|+0x016:'743'
-	// <0x591a06>|0x0f6|+0x00c:'744'
-	// <0>
-	// <0x591a12>|0x102|+0x00d:'746'
-	// <0>
-	// <1>
-	// <2>
-	// <3>
-	// <4>
-	// <0x591a1f>|0x10f|+0x002:'752'
-	// ******
 }
 
 } // namespace survarium
