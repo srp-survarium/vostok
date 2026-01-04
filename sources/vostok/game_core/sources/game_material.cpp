8d896bdffd35aa6c080a92dcf1504854adde56f9
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-01-04 14:01:03 +0100

    Match game_material classes (#78)

--- sources/vostok/game_core/sources/game_material.cpp
+++ sources/vostok/game_core/sources/game_material.cpp
@@ -9,30 +9,46 @@ static float s_aim_transition_time = 0.3f;
 
 namespace survarium {
 
-// STATE[STUB]
+// game_id -> physics_group
+u16 g_material_physics_group[64] = {};
+
+// STATE[83.44%|DONE]: LTCG for fixed_string constructor
 game_material::game_material( )	:
 	m_name							( "default" ),
 	m_material_resistance			( 50.0f ),
 	m_bullet_reflection_speed_down	( 1.0f ),
 	m_width							( 0.1f ),
 	m_ricochet_koef					( s_aim_transition_time ),
-	m_id							( -1 ),
+	m_id							( u16(-1) ),
 	m_mine_can_place				( false ),
 	m_mine_can_stick				( false )
 {
-	// FUNCTION BODY
-	// <0x760310>|0x000|+0x078:'21'	{
-	// <0x760388>|0x078|      :'22'	}
-	// ******
 }
 
-// STATE[STUB]
-// void survarium::game_material::load_from_config(vostok::configs::binary_config_value const&)
+// STATE[88.10%|PARTIAL]: LTCG for binary_config_value access, also buffer_string
 void game_material::load_from_config( configs::binary_config_value const& val )
 {
-	// LOCALS
-	// u16 							physics_group
-	// ******
+	m_id							= (u16)val["id"];
+	m_name							= (pcstr)val["name"];	// sushi@MATCH: Target uses stack for whatever reason
+	m_material_resistance			= (float)val["physic"]["resistance"];
+	m_bullet_reflection_speed_down	= (float)val["physic"]["reflection_speed_down"];
+	m_width							= (float)val["physic"]["width"];
+	m_ricochet_koef					= (float)val["physic"]["k_ricochet"];
+
+	u16 physics_group = 0;
+
+	m_mine_can_place				= (bool)val["mine"]["can_place"];
+	m_mine_can_stick				= (bool)val["mine"]["can_stick"];
+
+	if ( (bool)val["mine"]["hittable"] )
+		physics_group |= 0x8;
+
+	if ( (bool)val["mine"]["walkable"] )
+		physics_group |= 0x2;
+
+	g_material_physics_group[m_id]	= physics_group;
+
+	ASSERT( UNKNOWN_EXPRESSION );
 
 	// FUNCTION BODY
 	// <0x760399>|0x009|+0x019:'28'
