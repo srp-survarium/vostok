47e6212b618b394cf8b83630410d615731d6831d
sushi-shi <sushi.is.shy@gmail.com>
2026-01-13 10:33:06 +0100

    Start matching inventory

--- sources/vostok/game_core/sources/inventory.cpp
+++ sources/vostok/game_core/sources/inventory.cpp
@@ -5,6 +5,10 @@
 #include "pch.h"
 #include <vostok/game_core/inventory.h>
 
+#include <vostok/game_core/base_player.h>
+#include <vostok/game_core/inventory_holder.h>
+#include <vostok/game_core/victory_item_core.h>
+
 namespace survarium {
 
 // STATE[STUB]
@@ -15,44 +19,41 @@ inventory::inventory( ) :
 {
 }
 
+
 // STATE[STUB]
-// void survarium::inventory::set_holder(survarium::inventory_holder*)
 void inventory::set_holder( inventory_holder* holder )
 {
-	// LOCALS
-	// u32 							i<1>
-	// ******
+	ASSERT( UNKNOWN_EXPRESSION_T( !m_holder ) );
+	m_holder = holder;
 
-	// CALL SITE INFO
-	// <0x70048f> -> void <unknown>()
-	// ******
+	for ( u32 i = 0 ; i < max_slots_count ; ++i )
+	{
+		if ( m_slots[i].item )
+			m_slots[i].item->holder_assigned( );
+	}
 
-	// FUNCTION BODY
-	// <0x700420>|0x000|+0x009:'27'	{
+	// FUNCTION BODY[0x700420]: 6
 	// <0x700429>|0x009|+0x00c:'28'
 	// <0x700435>|0x015|+0x00c:'29'
 	// <0>
 	// <0x700441>|0x021|+0x018|[1]:'31'
 	// <0x700459>|0x039|+0x016:'32'
 	// <0x70046f>|0x04f|+0x022:'33'
-	// <0x700491>|0x071|      :'34'	}
 	// ******
 }
 
 // STATE[STUB]
-// void survarium::inventory::unset_holder()
 void inventory::unset_holder( )
 {
 	// LOCALS
-	// u32 							i<1>
+	// u32 								i<1>
 	// ******
 
 	// CALL SITE INFO
-	// <0x700403> -> void <unknown>()
+	// <0x700403> -> void < unknown >()
 	// ******
 
-	// FUNCTION BODY
-	// <0x7003a0>|0x000|+0x009:'37'	{
+	// FUNCTION BODY[0x7003a0]: 7
 	// <0x7003a9>|0x009|+0x00c:'38'
 	// <0>
 	// <0x7003b5>|0x015|+0x018|[1]:'40'
@@ -60,45 +61,46 @@ void inventory::unset_holder( )
 	// <0x7003e3>|0x043|+0x022:'42'
 	// <0>
 	// <0x700405>|0x065|+0x00f:'44'
-	// <0x700414>|0x074|      :'45'	}
 	// ******
 }
 
 // STATE[STUB]
-// void survarium::inventory::set_item(const survarium::profile_slot_enum, vostok::resources::resource_ptr<survarium::inventory_item,vostok::resources::unmanaged_intrusive_base> const&)
-void inventory::set_item( profile_slot_enum slot, resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base> const& item )
+void inventory::set_item(
+	const profile_slot_enum			slot,
+	inventory_item_ptr const&		item
+)
 {
 	// CALL SITE INFO
-	// <0x700cff> -> void <unknown>(inventory*, profile_slot_enum)
+	// <0x700cff> -> void < unknown >( inventory*, profile_slot_enum )
 	// ******
 
-	// FUNCTION BODY
-	// <0x700c80>|0x000|+0x009:'48'	{
+	// FUNCTION BODY[0x700c80]: 4
 	// <0x700c89>|0x009|+0x00c:'49'
 	// <0x700c95>|0x015|+0x00c:'50'
 	// <0x700ca1>|0x021|+0x040:'51'
 	// <0x700ce1>|0x061|+0x020:'52'
-	// <0x700d01>|0x081|      :'53'	}
 	// ******
 }
 
 // STATE[STUB]
-// bool survarium::inventory::action(const survarium::profile_slot_enum, bool)
-bool inventory::action( profile_slot_enum slot_id, bool key_down )
+bool inventory::action(
+	const profile_slot_enum		slot_id,
+	bool						key_down
+)
 {
 	// LOCALS
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base> iitem
+	// inventory_item_ptr 				iitem
 	// inventory_item::action_behaviour_type const& behaviour_type<1>
 	// ******
 
 	// CALL SITE INFO
-	// <0x700bf8> -> void <unknown>(bool)
-	// <0x700c36> -> bool <unknown>(resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base> const&)
+	// <0x700bf8> -> void < unknown >( bool )
+	// <0x700c36> -> bool < unknown >( inventory_item_ptr const& )
 	// ******
 
 	return false;
-	// FUNCTION BODY
-	// <0x700b80>|0x000|+0x009:'56'	{
+
+	// FUNCTION BODY[0x700b80]: 26
 	// <0x700b89>|0x009|+0x015:'57'
 	// <0x700b9e>|0x01e|+0x010:'58'
 	// <0>
@@ -125,28 +127,25 @@ bool inventory::action( profile_slot_enum slot_id, bool key_down )
 	// <0x700c4b>|0x0cb|+0x011:'80'
 	// <0>
 	// <0x700c5c>|0x0dc|+0x00f:'82'
-	// <0x700c6b>|0x0eb|      :'83'	}
 	// ******
 }
 
 // STATE[STUB]
-// void survarium::inventory::setup_demo_profile()
 void inventory::setup_demo_profile( )
 {
 	// LOCALS
-	// u32 							i<1>
-	// resources::resource_ptr<weapon_core,resources::unmanaged_intrusive_base> weapon<2>
-	// profile_slot_enum 			current<2>
-	// u32 							i<2>
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base>& iitem<3>
-	// profile_slot_enum 			current<3>
-	// u32 							i<3>
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base>& iitem<4>
-	// profile_slot_enum 			current<4>
+	// u32 								i<1>
+	// weapon_core_ptr 					weapon<2>
+	// profile_slot_enum 				current<2>
+	// u32 								i<2>
+	// inventory_item_ptr& 				iitem<3>
+	// profile_slot_enum 				current<3>
+	// u32 								i<3>
+	// inventory_item_ptr& 				iitem<4>
+	// profile_slot_enum 				current<4>
 	// ******
 
-	// FUNCTION BODY
-	// <0x700a10>|0x000|+0x009:'86'	{
+	// FUNCTION BODY[0x700a10]: 30
 	// <0x700a19>|0x009|+0x01c|[1]:'87'
 	// <0>
 	// <0x700a35>|0x025|+0x00d|[2]:'89'
@@ -177,31 +176,28 @@ void inventory::setup_demo_profile( )
 	// <0x700b58>|0x148|+0x00c:'114'
 	// <0x700b64>|0x154|+0x012:'115'
 	// <0x700b76>|0x166|+0x002:'116'
-	// <0x700b78>|0x168|      :'117'	}
 	// ******
 }
 
 // STATE[STUB]
-// void survarium::inventory::setup_from_profile(survarium::player_profile&, survarium::items_dictionary const&)
 void inventory::setup_from_profile( player_profile& profile, items_dictionary const& dict )
 {
 	// LOCALS
-	// profile_slot* 				slot
-	// u32 							i<1>
-	// profile_slot_enum 			current<2>
-	// resources::resource_ptr<weapon_core,resources::unmanaged_intrusive_base> weapon<3>
-	// u32 							i<2>
-	// profile_slot_enum 			current<3>
-	// u32 							amount<4>
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base>& iitem<4>
-	// u32 							i<3>
-	// profile_slot_enum 			current<4>
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base>& iitem<5>
-	// u32 							amount<6>
+	// profile_slot* 					slot
+	// u32 								i<1>
+	// profile_slot_enum 				current<2>
+	// weapon_core_ptr 					weapon<3>
+	// u32 								i<2>
+	// profile_slot_enum 				current<3>
+	// const u32 						amount<4>
+	// inventory_item_ptr& 				iitem<4>
+	// u32 								i<3>
+	// profile_slot_enum 				current<4>
+	// inventory_item_ptr& 				iitem<5>
+	// const u32 						amount<6>
 	// ******
 
-	// FUNCTION BODY
-	// <0x700800>|0x000|+0x00a:'120'	{
+	// FUNCTION BODY[0x700800]: 55
 	// <0>
 	// <1>
 	// <0x70080a>|0x00a|+0x01c|[1]:'123'
@@ -257,29 +253,26 @@ void inventory::setup_from_profile( player_profile& profile, items_dictionary co
 	// <0x7009fa>|0x1fa|+0x005:'173'
 	// <0>
 	// <1>
-	// <0x7009ff>|0x1ff|      :'176'	}
 	// ******
 }
 
 // STATE[STUB]
-// void survarium::inventory::unload_to_profile(survarium::player_profile&, survarium::items_dictionary const&)
 void inventory::unload_to_profile( player_profile& profile, items_dictionary const& dict )
 {
 	// LOCALS
-	// profile_slot* 				slot
-	// u32 							i<1>
-	// resources::resource_ptr<weapon_core,resources::unmanaged_intrusive_base> weapon<2>
-	// profile_slot_enum 			current<2>
-	// u32 							i<2>
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base> ammo<3>
-	// profile_slot_enum 			current<3>
-	// u32 							i<3>
-	// profile_slot_enum 			current<4>
-	// resources::resource_ptr<inventory_item,resources::unmanaged_intrusive_base>& iitem<5>
+	// profile_slot* 					slot
+	// u32 								i<1>
+	// weapon_core_ptr 					weapon<2>
+	// profile_slot_enum 				current<2>
+	// u32 								i<2>
+	// inventory_item_ptr 				ammo<3>
+	// profile_slot_enum 				current<3>
+	// u32 								i<3>
+	// profile_slot_enum 				current<4>
+	// inventory_item_ptr& 				iitem<5>
 	// ******
 
-	// FUNCTION BODY
-	// <0x7005f0>|0x000|+0x00a:'179'	{
+	// FUNCTION BODY[0x7005f0]: 47
 	// <0>
 	// <1>
 	// <0x7005fa>|0x00a|+0x01c|[1]:'182'
@@ -327,11 +320,10 @@ void inventory::unload_to_profile( player_profile& profile, items_dictionary con
 	// <0>
 	// <0x7007ee>|0x1fe|+0x005:'225'
 	// <0>
-	// <0x7007f3>|0x203|      :'227'	}
 	// ******
 }
 
-// STATE[STUB]
+// STATE[BLOCKED]
 static void call_item_remove( inventory_slot& slot )
 {
 	if ( slot.item )
@@ -356,72 +348,79 @@ void inventory::remove( )
 	// ******
 }
 
-// STATE[STUB]
-// void survarium::call_item_serialize(survarium::inventory_slot const&, vostok::network_core::udp_match_packet&, const unsigned int)
-void call_item_serialize( inventory_slot const& slot, network_core::udp_match_packet& packet, u32 client_offset )
+// STATE[BLOCKED]
+static void call_item_serialize(
+	inventory_slot const&				slot,
+	network_core::udp_match_packet&		packet,
+	const u32							client_offset
+)
 {
 	// LOCALS
-	// profile_slot_enum const* 	ignored_slots_start
-	// profile_slot_enum const* 	ignored_slots_end
+	// profile_slot_enum const* const 	ignored_slots_start
+	// profile_slot_enum const* const 	ignored_slots_end
 	// ******
 
 	// CALL SITE INFO
-	// <0x700360> -> void <unknown>(network_core::udp_match_packet&, u32) const
+	// <0x700360> -> void < unknown >( network_core::udp_match_packet&, u32 ) const
 	// ******
 
-	// FUNCTION BODY
-	// <0x7002f0>|0x000|+0x006:'252'	{
+	// FUNCTION BODY[0x7002f0]: 5
 	// <0x7002f6>|0x006|+0x007:'253'
 	// <0x7002fd>|0x00d|+0x010:'254'
 	// <0>
 	// <0x70030d>|0x01d|+0x035:'256'
 	// <0x700342>|0x052|+0x020:'257'
-	// <0x700362>|0x072|      :'258'	}
 	// ******
 }
 
-// STATE[STUB]
-// void survarium::call_item_deserialize(survarium::inventory_slot&, vostok::network_core::packet_reader&)
-void call_item_deserialize( inventory_slot& slot, network_core::packet_reader& reader )
+// STATE[BLOCKED]
+static void call_item_deserialize( inventory_slot& slot, network_core::packet_reader& reader )
 {
 	// LOCALS
-	// profile_slot_enum const* 	ignored_slots_start
-	// profile_slot_enum const* 	ignored_slots_end
+	// profile_slot_enum const* const 	ignored_slots_start
+	// profile_slot_enum const* const 	ignored_slots_end
 	// ******
 
 	// CALL SITE INFO
-	// <0x7002dc> -> void <unknown>(network_core::packet_reader&)
+	// <0x7002dc> -> void < unknown >( network_core::packet_reader& )
 	// ******
 
-	// FUNCTION BODY
-	// <0x700270>|0x000|+0x006:'261'	{
+	// FUNCTION BODY[0x700270]: 5
 	// <0x700276>|0x006|+0x007:'262'
 	// <0x70027d>|0x00d|+0x010:'263'
 	// <0>
 	// <0x70028d>|0x01d|+0x035:'265'
 	// <0x7002c2>|0x052|+0x01c:'266'
-	// <0x7002de>|0x06e|      :'267'	}
 	// ******
 }
 
 // STATE[BLOCKED]
 void inventory::deserialize( network_core::packet_reader& reader )
 {
-	// FUNCTION BODY
+	// FUNCTION BODY[0x7004a0]: 1
 	// <0x7004a9>|0x009|+0x075:'276'
 	// ******
 }
 
-// STATE[STUB]
-// void survarium::inventory::set_victory_item(survarium::victory_item_core*)
+// STATE[UNCHECKED]
 void inventory::set_victory_item( victory_item_core* item )
 {
 	// CALL SITE INFO
-	// <0x700246> -> base_player* <unknown>()
+	// <0x700246> -> base_player* < unknown >()
 	// ******
 
-	// FUNCTION BODY
-	// <0x7001f0>|0x000|+0x009:'280'	{
+	if ( !item )
+	{
+		m_victory_item->set_carrier_id( u8(-1) );
+		m_victory_item = NULL;
+	}
+	else
+	{
+		m_victory_item = item;
+		m_victory_item->set_carrier_id( m_holder->cast_to_base_player( )->id );
+	}
+
+	// FUNCTION BODY[0x7001f0]: 10
 	// <0x7001f9>|0x009|+0x006:'281'
 	// <0>
 	// <0x7001ff>|0x00f|+0x016:'283'
@@ -432,7 +431,6 @@ void inventory::set_victory_item( victory_item_core* item )
 	// <0x700223>|0x033|+0x00c:'288'
 	// <0x70022f>|0x03f|+0x037:'289'
 	// <0>
-	// <0x700266>|0x076|      :'291'	}
 	// ******
 }
 
