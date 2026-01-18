63f82c7d89c4f12c3de4967ba34bdd1141f418d0
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-01-18 21:12:45 +0100

    Match damage_model_cook (#86)

--- sources/vostok/console_command_inline.h
+++ sources/vostok/console_command_inline.h
@@ -12,14 +12,13 @@ namespace console_commands{
 
 template<class T>
 inline cc_value<T>::cc_value(pcstr name, T& value, T const min, T const max, bool serializable, command_type const command_type, execution_filter const execution_filter)
-:super		( name, serializable, command_type, execution_filter ), 
-m_value		( value ), 
-m_min		( min ), 
+:super		( name, serializable, command_type, execution_filter ),
+m_value		( value ),
+m_min		( min ),
 m_max		( max )
 {
 	m_need_args		= true;
 }
-
 
 } // namespace console_commands
 } // namespace vostok
