0d0dcccbd6f6c6a1eab5cd22e136348238f21811
sushi-shi <sushi.is.shy@gmail.com>
2026-06-03 23:19:13 +0000

    stack: weapon_core_chamber_a_round_state (docs+anchor+cook union)

--- sources/vostok/game_core/weapon_core_state_cook_template.h
+++ sources/vostok/game_core/weapon_core_state_cook_template.h
@@ -19,6 +19,7 @@ namespace vostok {
 	void use_game_core_pistol_weapon_core_aimed_fire_state( );
 	void use_game_core_weapon_core_reload_state( );
 	void use_game_core_weapon_core_fire_state( );
+	void use_game_core_weapon_core_chamber_a_round_state( );
 }
 
 namespace survarium {
@@ -39,6 +40,7 @@ class weapon_core_state_cook_template : public resources::unmanaged_cook {
 	friend void ::vostok::use_game_core_pistol_weapon_core_aimed_fire_state( );
 	friend void ::vostok::use_game_core_weapon_core_reload_state( );
 	friend void ::vostok::use_game_core_weapon_core_fire_state( );
+	friend void ::vostok::use_game_core_weapon_core_chamber_a_round_state( );
 public:
 	inline	explicit	weapon_core_state_cook_template		( );
 	virtual				~weapon_core_state_cook_template	( );
