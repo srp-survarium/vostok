1034a9faf6cec5157564a18b35c44a74f4fd164b
sushi-shi <sushi.is.shy@gmail.com>
2026-06-03 23:19:14 +0000

    stack: weapon_core_show_state (docs+anchor+cook union)

--- sources/vostok/game_core/sources/temp_include_all.cpp
+++ sources/vostok/game_core/sources/temp_include_all.cpp
@@ -77,6 +77,7 @@
 #include <vostok/game_core/weapon_core_reload_state.h>
 #include <vostok/game_core/pistol_weapon_core_idle_state.h>
 #include <vostok/game_core/pistol_weapon_core_show_state.h>
+#include <vostok/game_core/weapon_core_show_state.h>
 #include <vostok/game_core/double_barreled_weapon_core_idle_state.h>
 #include <vostok/game_core/pistol_weapon_core_aimed_idle_state.h>
 #include <vostok/game_core/double_barreled_weapon_core_aimed_idle_state.h>
@@ -841,6 +842,28 @@ namespace vostok
 		cook_type*									cook	= reinterpret_cast< cook_type* >( NULL );
 		survarium::weapon_state_creation_params const*	params	= reinterpret_cast< survarium::weapon_state_creation_params const* >( NULL );
 		survarium::pistol_weapon_core_show_state*	object	= cook->cook_type::new_object( buffer, params, animations, 12 );
+		example_callback( reinterpret_cast< pcstr >( object ) );
+
+		example_callback( reinterpret_cast< pcstr >( &state ) );
+	}
+
+	void use_game_core_weapon_core_show_state( )
+	{
+		survarium::weapon_core						weapon;
+		vostok::resources::managed_resource_ptr		animations[ 8 ];
+		bool										is_shown	= false;
+		survarium::weapon_core_show_state			state( weapon, 1.0f, animations, 8, is_shown );
+
+		mutable_buffer								buffer;
+		vostok::animation::mixing::animation_lexeme&	lexeme	= *reinterpret_cast< vostok::animation::mixing::animation_lexeme* >( NULL );
+		state.survarium::weapon_core_show_state::weapon_and_hands_expression( buffer, false, survarium::type_stand, lexeme );
+		state.survarium::weapon_core_show_state::get_weapon_lexeme_pair( buffer, false, survarium::type_stand );
+		state.survarium::weapon_core_show_state::get_user_hands_expression( lexeme, buffer, false, survarium::type_stand, lexeme );
+
+		typedef survarium::weapon_core_state_cook_template< survarium::weapon_core_show_state > cook_type;
+		cook_type*									cook	= reinterpret_cast< cook_type* >( NULL );
+		survarium::weapon_state_creation_params const*	params	= reinterpret_cast< survarium::weapon_state_creation_params const* >( NULL );
+		survarium::weapon_core_show_state*			object	= cook->cook_type::new_object( buffer, params, animations, 8 );
 		example_callback( reinterpret_cast< pcstr >( object ) );
 
 		example_callback( reinterpret_cast< pcstr >( &state ) );
@@ -1988,6 +2011,7 @@ IncludeAll::IncludeAll()
 	vostok::use_game_core_weapon_core_reload_state( );
 	vostok::use_game_core_pistol_weapon_core_idle_state( );
 	vostok::use_game_core_pistol_weapon_core_show_state( );
+	vostok::use_game_core_weapon_core_show_state( );
 	vostok::use_game_core_double_barreled_weapon_core_idle_state( );
 	vostok::use_game_core_pistol_weapon_core_aimed_idle_state( );
 	vostok::use_game_core_pistol_weapon_core_aimed_fire_state( );
