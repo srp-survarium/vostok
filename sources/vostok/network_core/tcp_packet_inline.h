75a9af75aad79cc6a076279e1d9db8c76f647b35
sushi-shi <sushi.is.shy@gmail.com>
2026-06-08 19:22:56 +0000

    network_core: recover ~tcp_packet impl (frees real_buffer via VOSTOK_FREE_IMPL)

--- sources/vostok/network_core/tcp_packet_inline.h
+++ sources/vostok/network_core/tcp_packet_inline.h
@@ -15,16 +15,13 @@ inline tcp_packet::tcp_packet( memory::base_allocator& allocator ) :
 	/* no source */
 }
 
-// STATE[STUB]
 inline tcp_packet::~tcp_packet( )
 {
-	// FUNCTION BODY[0xa72b0]: 5
-	// <0xa72b0>|0x000|+0x006:'21'
-	// <0>
-	// <0xa72b6>|0x006|+0x003:'23'
-	// <0xa72b9>|0x009|+0x00d:'24'
-	// <0>
-	// ******
+	if ( buffer( ) )										// <0xa72b0>|0x000|0x000:'21'
+	{
+		pbyte real_buffer	= buffer( ) - 3;				// <0xa72b6>|0x006|0x006:'23'
+		VOSTOK_FREE_IMPL	( m_allocator, real_buffer );	// <0xa72b9>|0x009|0x003:'24'
+	}
 }
 
 inline u32 tcp_packet::allocated_size( ) const
