b54ed45a856b1c4dd89e0ad28515ae45eb1762d3
sushi-shi <sushi.is.shy@gmail.com>
2026-06-08 19:22:56 +0000

    network_core: recover tcp_packet.cpp buffer_to_send/buffer_to_receive_into impls

--- sources/vostok/network_core/sources/tcp_packet.cpp
+++ sources/vostok/network_core/sources/tcp_packet.cpp
@@ -1,5 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////
-//	Created 	: 02.06.2026
+//	Created 	: 12.10.2025
+//	Author		: Dmitriy Iassenev
 ////////////////////////////////////////////////////////////////////////////
 
 #include "pch.h"
@@ -8,40 +9,28 @@
 namespace vostok {
 namespace network_core {
 
-// STATE[STUB]
-boost::asio::const_buffers_1 buffer_to_send( tcp_packet& packet )
+boost::asio::const_buffers_1 buffer_to_send( tcp_packet& packet ) // STATE[STUB]
 {
-	// LOCALS
-	// pbyte const 						buffer
-	// const u32 						buffer_size
-	// ******
+	u32 buffer_size = packet.buffer_size( );
+	pbyte buffer	= packet.buffer();
+	if ( !buffer_size )
+		return boost::asio::buffer( static_cast<pcvoid>( NULL ), 0 );
 
-	return boost::asio::const_buffers_1( NULL, 0 );
+	if ( buffer_size < 256 ) {
+		*(buffer - 1) = static_cast<u8>( buffer_size );
+		return boost::asio::buffer( static_cast<pcbyte>(buffer - 1), buffer_size + 1 );
+	}
 
-	// FUNCTION BODY[0x6e78e0]: 14
-	// <0x6e78e6>|0x006|+0x00b:'14'
-	// <0x6e78f1>|0x011|+0x008:'15'
-	// <0x6e78f9>|0x019|+0x006:'16'
-	// <0x6e78ff>|0x01f|+0x021:'17'
-	// <0x6e7920>|0x040|+0x009:'19'
-	// <0x6e7929>|0x049|+0x009:'20'
-	// <0x6e7932>|0x052|+0x025:'21'
-	// <0x6e7957>|0x077|+0x00c:'24'
-	// <0x6e7963>|0x083|+0x007:'25'
-	// <0x6e796a>|0x08a|+0x00b:'26'
-	// <0x6e7975>|0x095|+0x023:'27'
-	// ******
+	ASSERT( buffer_size < (u32(1) << 16) );
+	*(buffer - 1) = 0;
+	*static_cast<u16*>(static_cast<pvoid>(buffer - 3)) = static_cast<u16>( buffer_size );
+	return boost::asio::buffer( static_cast<pcbyte>(buffer - 3), buffer_size + 3 );	// <0x6e7975>|0x08f|0x00b:'27'
 }
 
-// STATE[STUB]
-boost::asio::mutable_buffers_1 buffer_to_receive_into( tcp_packet& packet )
+boost::asio::mutable_buffers_1 buffer_to_receive_into( tcp_packet& packet ) // STATE[STUB]
 {
-	return boost::asio::mutable_buffers_1( NULL, 0 );
-
-	// FUNCTION BODY[0x6e79a0]: 2
-	// <0x6e79a6>|0x006|+0x00c:'32'
-	// <0x6e79b2>|0x012|+0x030:'33'
-	// ******
+	ASSERT( packet.buffer_size() );
+	return boost::asio::buffer( packet.buffer(), packet.buffer_size() );	// <0x6e79b2>|0x00c|0x00c:'33'
 }
 
 } // namespace network_core
