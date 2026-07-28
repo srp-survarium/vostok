61253bdd4c146e4d285a105f8041539f3cf7f2fb
sushi-shi <sushi.is.shy@gmail.com>
2026-07-28 22:42:20 +0000

    sound: restore environment collision resource

---
+++ sources/vostok/sound/sources/sound_environment.h
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created		: 28.07.2026
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef SOUND_ENVIRONMENT_H_INCLUDED
+#define SOUND_ENVIRONMENT_H_INCLUDED
+
+#include <vostok/resources_unmanaged_resource.h>
+
+namespace vostok {
+
+namespace collision {
+class object;
+} // namespace collision
+
+namespace sound {
+
+class sound_environment : public resources::unmanaged_resource, private boost::noncopyable
+{
+public:
+	explicit					sound_environment	( u32 env_params_id );
+	virtual						~sound_environment	( );
+
+	inline collision::object*	collision			( ) { return m_collision; }
+	inline u32					env_params_id		( ) const { return m_env_params_id; }
+
+private:
+	collision::object*			m_collision;
+	u32							m_env_params_id;
+}; // class sound_environment
+
+STATIC_SIZE_ASSERT			( sound_environment, 0x110 );
+
+} // namespace sound
+} // namespace vostok
+
+#endif // #ifndef SOUND_ENVIRONMENT_H_INCLUDED
