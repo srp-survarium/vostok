950665bcbbfa43f33b26063adeea4e920d50a77c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 23:53:59 +0000

    render: admit ambient volume owner

---
+++ sources/vostok/render/engine/sources/ambient_volume.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "ambient_volume.h"
+
+namespace vostok {
+namespace render {
+
+ambient_volume::ambient_volume(
+	ambient_volume_properties const&	properties,
+	u32									id
+) :
+	m_properties				( properties ),
+	m_aabb						( math::create_zero_aabb( ) ),
+	m_id						( id ),
+	m_occlusion_info_index		( 0 ),
+	m_occluded					( false )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x748920]
+}
+
+bool ambient_volume::is_occluded( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x748870]
+	return false;
+}
+
+void ambient_volume::set_properties( ambient_volume_properties const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x748890]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/facade/ambient_volume_properties.h
@@ -0,0 +1,22 @@
+#ifndef VOSTOK_RENDER_FACADE_AMBIENT_VOLUME_PROPERTIES_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_AMBIENT_VOLUME_PROPERTIES_H_INCLUDED
+
+#include <vostok/math_float4x4.h>
+
+namespace vostok {
+namespace render {
+
+struct ambient_volume_properties {
+	ambient_volume_properties( ) { }
+
+	float4x4	transform;
+	float		ambient_multiplier;
+	bool		enabled;
+};
+
+STATIC_SIZE_ASSERT( ambient_volume_properties, 0x48 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_AMBIENT_VOLUME_PROPERTIES_H_INCLUDED
