887d59fffe1e8eaa48498af15b6457a4f6b03f83
sushi-shi <sushi.is.shy@gmail.com>
2026-07-29 00:51:42 +0000

    sound: restore propagator timing state

---
+++ sources/vostok/sound/sources/sound_voice_params.h
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created		: 29.07.2026
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef SOUND_VOICE_PARAMS_H_INCLUDED
+#define SOUND_VOICE_PARAMS_H_INCLUDED
+
+namespace vostok {
+namespace sound {
+
+struct sound_voice_params
+{
+	float	channel_matrix[2];
+	float	lp_filter_coeff;
+}; // struct sound_voice_params
+
+STATIC_SIZE_ASSERT( sound_voice_params, 0xC );
+
+} // namespace sound
+} // namespace vostok
+
+#endif // #ifndef SOUND_VOICE_PARAMS_H_INCLUDED
