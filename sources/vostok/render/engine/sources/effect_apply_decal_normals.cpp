2dd0e953308f78b33648a04e8c2f790b57f3b0de
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 23:46:54 +0000

    render: admit apply decal effect

---
+++ sources/vostok/render/engine/sources/effect_apply_decal_normals.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_apply_decal_normals.h"
+
+namespace vostok {
+namespace render {
+
+void effect_apply_decal::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7beec0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_apply_decal_normals.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_APPLY_DECAL_NORMALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_APPLY_DECAL_NORMALS_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_apply_decal : public effect_descriptor {
+public:
+	effect_apply_decal( ) { }
+	virtual ~effect_apply_decal( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_apply_decal, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_APPLY_DECAL_NORMALS_H_INCLUDED
