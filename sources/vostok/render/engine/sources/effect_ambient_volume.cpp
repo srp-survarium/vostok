260ab88f9634cce7711d687139ce62714152c893
sushi-shi <sushi.is.shy@gmail.com>
2026-07-29 23:45:38 +0000

    render: admit ambient volume effect

---
+++ sources/vostok/render/engine/sources/effect_ambient_volume.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_ambient_volume.h"
+
+namespace vostok {
+namespace render {
+
+void effect_ambient_volume::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b53e0]
+}
+
+} // namespace render
+} // namespace vostok
