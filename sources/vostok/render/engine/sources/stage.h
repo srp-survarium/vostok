21afa2c1c834bbdc881acc61ba1c6403881f9dcb
sushi-shi <sushi.is.shy@gmail.com>
2026-07-29 22:59:12 +0000

    render: admit stage owner

---
+++ sources/vostok/render/engine/sources/stage.h
@@ -0,0 +1,77 @@
+#ifndef VOSTOK_RENDER_ENGINE_STAGE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_STAGE_H_INCLUDED
+
+#include <boost/noncopyable.hpp>
+
+namespace vostok {
+namespace render {
+
+class renderer;
+class renderer_context;
+
+class stage : public boost::noncopyable {
+public:
+	stage( renderer* in_renderer, renderer_context* in_context );
+	virtual ~stage( ) { }
+
+	virtual void execute( );
+
+	virtual void execute_disabled( )
+	{
+		// STATE[STUB]
+		// FUNCTION BODY[0x4f210]
+	}
+
+	virtual void debug_render( )
+	{
+	}
+
+	virtual pcstr stage_name( ) const
+	{
+		return "";
+	}
+
+	void set_enabled( bool enable );
+	virtual bool is_enabled( ) const;
+
+	void try_disable( )
+	{
+	}
+
+	void try_enable( )
+	{
+	}
+
+	void u_compute_texgen_screen( float4x4& )
+	{
+	}
+
+	void u_compute_texgen_jitter( float4x4& )
+	{
+	}
+
+	static void rm_near( )
+	{
+	}
+
+	static void rm_normal( )
+	{
+	}
+
+	static void rm_far( )
+	{
+	}
+
+protected:
+	renderer_context*	m_context;
+	renderer*			m_renderer;
+	bool				m_enabled;
+	bool				m_prev_enabled;
+};
+
+STATIC_SIZE_ASSERT( stage, 0x10 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_STAGE_H_INCLUDED
