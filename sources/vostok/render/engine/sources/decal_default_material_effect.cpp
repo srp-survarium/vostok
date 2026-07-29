f0cfd60295517901af844a00e74279eb2f66056d
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 23:41:45 +0000

    render: admit decal material effect

---
+++ sources/vostok/render/engine/sources/decal_default_material_effect.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "decal_default_material_effect.h"
+
+namespace vostok {
+namespace render {
+
+void decal_default_material_effect::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b8f20]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/decal_default_material_effect.h
@@ -0,0 +1,32 @@
+#ifndef VOSTOK_RENDER_ENGINE_DECAL_DEFAULT_MATERIAL_EFFECT_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_DECAL_DEFAULT_MATERIAL_EFFECT_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class decal_default_material_effect : public effect_material_base {
+public:
+	explicit decal_default_material_effect( bool is_forward ) :
+		m_is_forward( is_forward )
+	{
+	}
+
+	virtual ~decal_default_material_effect( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+
+private:
+	bool m_is_forward;
+};
+
+STATIC_SIZE_ASSERT( decal_default_material_effect, 0x8 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_DECAL_DEFAULT_MATERIAL_EFFECT_H_INCLUDED
