2da00042bb438ec1b13a90445c1713c01f4abb9e
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 23:47:50 +0000

    render: admit apply distortion effect

---
+++ sources/vostok/render/engine/sources/effect_apply_distortion.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_apply_distortion.h"
+
+namespace vostok {
+namespace render {
+
+void effect_apply_distortion::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bcc00]
+}
+
+} // namespace render
+} // namespace vostok
