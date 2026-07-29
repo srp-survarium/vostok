9ebc5317e78a53b3dea127b1c25d4f07bdd472a8
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 22:42:29 +0000

    render: admit skeleton render model owner

---
+++ sources/vostok/render/engine/sources/render_model_skeleton.h
@@ -0,0 +1,81 @@
+#ifndef VOSTOK_RENDER_ENGINE_RENDER_MODEL_SKELETON_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_RENDER_MODEL_SKELETON_H_INCLUDED
+
+#include <vostok/render/core/memory.h>
+#include "render_model.h"
+#include "render_model_instance_impl.h"
+#include "render_surface.h"
+#include "render_surface_instance.h"
+
+namespace vostok {
+
+namespace memory {
+
+class reader;
+
+} // namespace memory
+
+namespace render {
+
+class shader_constant_host;
+struct surface_stats;
+
+class skeleton_render_surface : public render_surface {
+public:
+	skeleton_render_surface( );
+	virtual void update( vector< float4x4 > const& bones ) = 0;
+	virtual bool is_dynamic( ) const { return true; }
+	virtual ~skeleton_render_surface( ) { }
+};
+
+STATIC_SIZE_ASSERT( skeleton_render_surface, 0x9C );
+
+class skeleton_render_model : public render_model {
+public:
+	skeleton_render_model( );
+	virtual ~skeleton_render_model( ) { }
+
+	void load_bones( memory::reader& bones_chunk );
+	void update( vector< float4x4 > const& bones );
+	void get_bind_pose( float4x4* matrices, u32 count ) const;
+
+	shader_constant_host*	m_bones_matrices_shader_constant;
+	shader_constant_host*	m_prev_bones_matrices_shader_constant;
+	vector< float4x4 >		m_inverted_bones_matrices_in_bind_pose;
+};
+
+STATIC_SIZE_ASSERT( skeleton_render_model, 0x150 );
+
+typedef resources::resource_ptr<
+	skeleton_render_model,
+	resources::unmanaged_intrusive_base
+> skeleton_render_model_ptr;
+
+class skeleton_render_model_instance : public render_model_instance_impl {
+public:
+	skeleton_render_model_instance( );
+	virtual ~skeleton_render_model_instance( );
+
+	void get_surfaces(
+		float4x4 const*						mat_vp,
+		float3 const*						view_pos,
+		vector< render_surface_instance* >&	list,
+		bool								visible_only,
+		u8									lod_id,
+		u32									surface_flags
+	);
+
+	void update( );
+	void set_constants( );
+	void assign_original( skeleton_render_model_ptr v );
+	void update_render_matrices( float4x4 const* matrices, u32 count );
+	bool get_locator( pcstr locator_name, model_locator_item& result ) const;
+	u32 get_surfaces_count( u32 lod_id ) const;
+	void get_surface_stats( u32 surface_id, surface_stats& stats ) const;
+	void get_bind_pose( float4x4* matrices, u32 count ) const;
+};
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_RENDER_MODEL_SKELETON_H_INCLUDED
