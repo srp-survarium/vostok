0b83425ae8fcd04c8145f2327a9e55d95ae86d6f
sushi-shi <sushi.is.shy@gmail.com>
2026-07-29 23:44:43 +0000

    render: admit aberration effect

---
+++ sources/vostok/render/engine/sources/effect_aberration.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_ABERRATION_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_ABERRATION_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_aberration : public effect_descriptor {
+public:
+	effect_aberration( ) { }
+	virtual ~effect_aberration( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	custom_config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_aberration, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_ABERRATION_H_INCLUDED
