9b13a26d06ce039c254d639606e07542e41fc56c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 23:48:49 +0000

    render: admit indirect lighting effect

---
+++ sources/vostok/render/engine/sources/effect_apply_indirect_lighting.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_apply_indirect_lighting.h"
+
+namespace vostok {
+namespace render {
+
+void effect_apply_indirect_lighting::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bd8b0]
+}
+
+} // namespace render
+} // namespace vostok
