0b087e171c8a6714291afab2c6040d8c8cb822e9
sushi-shi <sushi.is.shy@gmail.com>
2026-07-29 23:48:49 +0000

    render: admit indirect lighting effect

---
+++ sources/vostok/render/engine/sources/effect_apply_indirect_lighting.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_APPLY_INDIRECT_LIGHTING_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_APPLY_INDIRECT_LIGHTING_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_apply_indirect_lighting : public effect_descriptor {
+public:
+	effect_apply_indirect_lighting( ) { }
+	virtual ~effect_apply_indirect_lighting( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_apply_indirect_lighting, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_APPLY_INDIRECT_LIGHTING_H_INCLUDED
