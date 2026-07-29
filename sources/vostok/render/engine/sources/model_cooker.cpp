01437927d6ce0215b3da96e9e9ad61a7c7b5c77a
sushi-shi <sushi.is.shy@gmail.com>
2026-07-29 23:04:45 +0000

    render: admit model cooker owner

---
+++ sources/vostok/render/engine/sources/model_cooker.cpp
@@ -0,0 +1,100 @@
+#include "pch.h"
+#include "model_cooker.h"
+
+namespace vostok {
+namespace render {
+
+// STATE[STUB]
+static_model_instance_cook::static_model_instance_cook( ) :
+	resources::translate_query_cook(
+		resources::static_model_instance_class,
+		reuse_false,
+		use_current_thread_id
+	)
+{
+	// FUNCTION BODY[0x76c2d0]
+}
+
+// STATE[STUB]
+void static_model_instance_cook::translate_query( resources::query_result_for_cook& )
+{
+	// FUNCTION BODY[0x76cd40]
+}
+
+// STATE[STUB]
+void static_model_instance_cook::on_subresources_loaded(
+	resources::queries_result&,
+	resources::query_result_for_cook*
+)
+{
+	// FUNCTION BODY[0x76c7a0]
+}
+
+// STATE[STUB]
+void static_model_instance_cook::delete_resource( resources::resource_base* resource )
+{
+	// FUNCTION BODY[0x76c380]
+	DELETE( resource );
+}
+
+// STATE[STUB]
+skeleton_model_instance_cook::skeleton_model_instance_cook( ) :
+	resources::translate_query_cook(
+		resources::skeleton_model_instance_class,
+		reuse_false,
+		use_current_thread_id
+	)
+{
+	// FUNCTION BODY[0x76c260]
+}
+
+// STATE[STUB]
+void skeleton_model_instance_cook::translate_query( resources::query_result_for_cook& )
+{
+	// FUNCTION BODY[0x76cb10]
+}
+
+// STATE[STUB]
+void skeleton_model_instance_cook::on_skeleton_config_loaded(
+	resources::queries_result&,
+	skeleton_model_instance_cook_data*
+)
+{
+	// FUNCTION BODY[0x76c9c0]
+}
+
+// STATE[STUB]
+void skeleton_model_instance_cook::on_skeleton_loaded(
+	resources::queries_result&,
+	skeleton_model_instance_cook_data*
+)
+{
+	// FUNCTION BODY[0x76c6b0]
+}
+
+// STATE[STUB]
+void skeleton_model_instance_cook::on_render_model_loaded(
+	resources::queries_result&,
+	skeleton_model_instance_cook_data*
+)
+{
+	// FUNCTION BODY[0x76c5c0]
+}
+
+// STATE[STUB]
+void skeleton_model_instance_cook::on_all_subresources_ready(
+	skeleton_model_instance_cook_data*
+)
+{
+	// FUNCTION BODY[0x76c3c0]
+}
+
+// STATE[STUB]
+void skeleton_model_instance_cook::delete_resource( resources::resource_base* resource )
+{
+	// FUNCTION BODY[0x76c340]
+	DELETE( resource );
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/model_cooker.h
@@ -0,0 +1,78 @@
+#ifndef VOSTOK_RENDER_ENGINE_MODEL_COOKER_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_MODEL_COOKER_H_INCLUDED
+
+#include <vostok/animation/skeleton.h>
+#include <vostok/render/facade/model.h>
+#include <vostok/resources_cook_classes.h>
+
+namespace vostok {
+
+namespace render {
+
+struct static_model_instance_cook : public resources::translate_query_cook {
+	static_model_instance_cook( );
+	virtual ~static_model_instance_cook( ) { }
+
+	virtual void translate_query( resources::query_result_for_cook& parent );
+	virtual void delete_resource( resources::resource_base* resource );
+
+private:
+	void on_subresources_loaded(
+		resources::queries_result&			data,
+		resources::query_result_for_cook*	parent_query
+	);
+};
+
+STATIC_SIZE_ASSERT( static_model_instance_cook, 0x20 );
+
+struct skeleton_model_instance_cook_data {
+	explicit skeleton_model_instance_cook_data(
+		resources::query_result_for_cook* in_parent_query
+	) :
+		render_model_ready	( false ),
+		skeleton_ready		( false ),
+		parent_query		( in_parent_query )
+	{
+	}
+
+	bool								render_model_ready;
+	bool								skeleton_ready;
+	resources::query_result_for_cook*	parent_query;
+	render_model_instance_ptr			render_model;
+	animation::skeleton_ptr				skeleton;
+};
+
+STATIC_SIZE_ASSERT( skeleton_model_instance_cook_data, 0x10 );
+
+struct skeleton_model_instance_cook : public resources::translate_query_cook {
+	skeleton_model_instance_cook( );
+	virtual ~skeleton_model_instance_cook( ) { }
+
+	virtual void translate_query( resources::query_result_for_cook& parent );
+	virtual void delete_resource( resources::resource_base* resource );
+
+private:
+	void on_render_model_loaded(
+		resources::queries_result&			result,
+		skeleton_model_instance_cook_data*	cook_data
+	);
+
+	void on_skeleton_config_loaded(
+		resources::queries_result&			result,
+		skeleton_model_instance_cook_data*	cook_data
+	);
+
+	void on_skeleton_loaded(
+		resources::queries_result&			result,
+		skeleton_model_instance_cook_data*	cook_data
+	);
+
+	void on_all_subresources_ready( skeleton_model_instance_cook_data* cook_data );
+};
+
+STATIC_SIZE_ASSERT( skeleton_model_instance_cook, 0x20 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_MODEL_COOKER_H_INCLUDED
