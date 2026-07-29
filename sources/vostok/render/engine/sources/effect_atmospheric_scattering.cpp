24e4fc10fc01de2573166ecb3d9ee1de1ec4ab21
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-29 23:49:48 +0000

    render: admit atmospheric scattering effect

---
+++ sources/vostok/render/engine/sources/effect_atmospheric_scattering.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_atmospheric_scattering.h"
+
+namespace vostok {
+namespace render {
+
+void effect_atmospheric_scattering::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bcda0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_atmospheric_scattering.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_ATMOSPHERIC_SCATTERING_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_ATMOSPHERIC_SCATTERING_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_atmospheric_scattering : public effect_descriptor {
+public:
+	effect_atmospheric_scattering( ) { }
+	virtual ~effect_atmospheric_scattering( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_atmospheric_scattering, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_ATMOSPHERIC_SCATTERING_H_INCLUDED
