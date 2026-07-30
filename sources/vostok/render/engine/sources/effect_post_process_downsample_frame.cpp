318d3dd7b420efad1ae00e2463aa49952a07eae1
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:09:13 +0000

    render: admit postprocess downsample-frame effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_downsample_frame.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_downsample_frame.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_downsample_frame::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bbdc0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_post_process_downsample_frame.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_DOWNSAMPLE_FRAME_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_DOWNSAMPLE_FRAME_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_post_process_downsample_frame : public effect_descriptor {
+public:
+	effect_post_process_downsample_frame( ) { }
+	virtual ~effect_post_process_downsample_frame( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_post_process_downsample_frame, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_DOWNSAMPLE_FRAME_H_INCLUDED
