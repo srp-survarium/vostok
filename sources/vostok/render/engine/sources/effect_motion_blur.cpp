24127450bdf3f9d0214d5d7c0cd7bf3d66e9ac17
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:58:28 +0000

    render: admit motion blur effect

---
+++ sources/vostok/render/engine/sources/effect_motion_blur.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_motion_blur.h"
+
+namespace vostok {
+namespace render {
+
+void effect_motion_blur::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bba10]
+}
+
+} // namespace render
+} // namespace vostok
