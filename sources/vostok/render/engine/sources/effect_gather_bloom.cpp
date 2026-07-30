40ea5bbb2dde0aabf1e5f285402dd0a0fd6032f9
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:27:23 +0000

    render: admit bloom gather effect

---
+++ sources/vostok/render/engine/sources/effect_gather_bloom.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_gather_bloom.h"
+
+namespace vostok {
+namespace render {
+
+void effect_gather_bloom::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bc890]
+}
+
+} // namespace render
+} // namespace vostok
