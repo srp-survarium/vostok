bf6732a81d36782098ddf105ad37886b245d9746
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:46:10 +0000

    render: admit Hi-Z occlusion effect

---
+++ sources/vostok/render/engine/sources/effect_hiz_occlusion.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_hiz_occlusion.h"
+
+namespace vostok {
+namespace render {
+
+void effect_hiz_occlusion::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b6ed0]
+}
+
+} // namespace render
+} // namespace vostok
