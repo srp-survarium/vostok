37c671b2b2cb3295ea7ca753a51b53f94eb9bafd
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 12:35:23 +0000

    render: consolidate shader comparison template

--- sources/vostok/render/core/res_xs.h
+++ sources/vostok/render/core/res_xs.h
@@ -2,3 +2,17 @@
 
 #include <vostok/render/core/platform_helper.h>
 #include VOSTOK_RENDER_CORE_FILE(res_xs.h)
+
+namespace vostok {
+namespace render {
+
+template < typename shader_data >
+inline s32 compare( res_xs< shader_data > const&, res_xs< shader_data > const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x126d10] for vs_data
+	return 0;
+}
+
+} // namespace render
+} // namespace vostok
