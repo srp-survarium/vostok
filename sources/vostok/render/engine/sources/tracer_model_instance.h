a3d76b5f74ef9633933cdc78667ef0ab92cb8903
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 03:52:31 +0000

    render: admit tracer model instance cook

--- sources/vostok/render/engine/sources/tracer_model_instance.h
+++ sources/vostok/render/engine/sources/tracer_model_instance.h
@@ -1,15 +1,30 @@
 #ifndef VOSTOK_RENDER_ENGINE_TRACER_MODEL_INSTANCE_H_INCLUDED
 #define VOSTOK_RENDER_ENGINE_TRACER_MODEL_INSTANCE_H_INCLUDED
 
-#include <vostok/resources_unmanaged_resource.h>
+#include <vostok/math_color.h>
+#include <vostok/render/facade/model.h>
 
 namespace vostok {
 namespace render {
 
 class tracer_model_instance : public resources::unmanaged_resource {
 public:
+	tracer_model_instance( ) { }
 	virtual ~tracer_model_instance( ) { }
+
+	void set_color( math::color const& color ) { m_color = color; }
+
+	float4x4 m_transform;
+	render_model_instance_ptr m_model;
+	math::color m_color;
 };
+
+STATIC_SIZE_ASSERT( tracer_model_instance, 0x150 );
+
+typedef resources::resource_ptr<
+	tracer_model_instance,
+	resources::unmanaged_intrusive_base
+> tracer_model_instance_ptr;
 
 } // namespace render
 } // namespace vostok
---
+++ sources/vostok/render/engine/sources/tracer_model_instance_cook.h
@@ -0,0 +1,25 @@
+#ifndef VOSTOK_RENDER_ENGINE_TRACER_MODEL_INSTANCE_COOK_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_TRACER_MODEL_INSTANCE_COOK_H_INCLUDED
+
+#include <vostok/resources_cook_classes.h>
+
+namespace vostok {
+namespace render {
+
+class tracer_model_instance_cook : public resources::translate_query_cook {
+public:
+	tracer_model_instance_cook( );
+	virtual ~tracer_model_instance_cook( ) { }
+
+	virtual void translate_query( resources::query_result_for_cook& parent );
+	virtual void delete_resource( resources::resource_base* resource );
+
+	void on_model_ready( resources::queries_result& data );
+};
+
+STATIC_SIZE_ASSERT( tracer_model_instance_cook, 0x20 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // VOSTOK_RENDER_ENGINE_TRACER_MODEL_INSTANCE_COOK_H_INCLUDED
