adc0d82732590d02488491b0f4098262b5113170
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:20:13 +0000

    render: admit volume cone forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_volume_cone_base_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_fstage_volume_cone_base_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_fstage_volume_cone_base_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7ba2c0]
+}
+
+} // namespace render
+} // namespace vostok
