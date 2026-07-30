40890cea09c5373fd7d36ad595e9db69665bed7f
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:33:10 +0000

    render: admit reflective shadow fill effect

---
+++ sources/vostok/render/engine/sources/effect_fill_reflective_shadow_map.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_fill_reflective_shadow_map.h"
+
+namespace vostok {
+namespace render {
+
+void effect_fill_reflective_shadow_map::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b58a0]
+}
+
+} // namespace render
+} // namespace vostok
