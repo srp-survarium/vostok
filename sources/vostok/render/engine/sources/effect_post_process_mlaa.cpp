5c4956f67fdf850692b9cdb71b4d3676f45bffeb
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:12:52 +0000

    render: admit MLAA effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_mlaa.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_mlaa.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_mlaa::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bc130]
+}
+
+} // namespace render
+} // namespace vostok
