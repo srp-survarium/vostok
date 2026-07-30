a562b6d0002dbdad7d42f14f802b62334886aa37
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 12:28:46 +0000

    render: admit render utility source

---
+++ sources/vostok/render/core/dx11/sources/utils.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include <vostok/render/core/utils.h>
+
+namespace vostok {
+namespace render {
+namespace utils {
+
+u32 get_format_4x4_pixel_size( DXGI_FORMAT )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559120]
+	return 0;
+}
+
+u32 calc_surface_size(
+	u32,
+	u32,
+	DXGI_FORMAT,
+	u32,
+	u32&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559130]
+	return 0;
+}
+
+} // namespace utils
+} // namespace render
+} // namespace vostok
