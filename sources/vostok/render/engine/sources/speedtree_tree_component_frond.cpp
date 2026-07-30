86a72f84e722cd4e8fbf0e05e6a96da78d97b8c7
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 03:35:02 +0000

    render: admit speedtree frond component

---
+++ sources/vostok/render/engine/sources/speedtree_tree_component_frond.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+#include "speedtree_tree_component_frond.h"
+
+namespace vostok {
+namespace render {
+
+speedtree_tree_component_frond::speedtree_tree_component_frond( speedtree_tree& parent ) :
+	speedtree_tree_component( parent )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f7b70]
+}
+
+void speedtree_tree_component_frond::init_index_buffer( SpeedTree::SIndexedTriangles const*, vector<u16>& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f79c0]
+}
+
+void speedtree_tree_component_frond::set_material( material_ptr )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f7b40]
+}
+
+void speedtree_tree_component_frond::set_default_material( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f79a0]
+}
+
+void speedtree_tree_component_frond::render( lod_entry const*, renderer_context* )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f79b0]
+}
+
+} // namespace render
+} // namespace vostok
