38427c8eef5d7aedc4472e51ad17ff1e214b4339
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 14:13:39 +0000

    render: restore probe generation parameters

---
+++ sources/vostok/render/facade/environment_probe_generate_parameters.h
@@ -0,0 +1,25 @@
+#ifndef VOSTOK_RENDER_FACADE_ENVIRONMENT_PROBE_GENERATE_PARAMETERS_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_ENVIRONMENT_PROBE_GENERATE_PARAMETERS_H_INCLUDED
+
+#include <vostok/fixed_string.h>
+#include <vostok/math_float3.h>
+
+namespace vostok {
+namespace render {
+
+struct environment_probe_generate_parameters {
+	environment_probe_generate_parameters( ) { }
+
+	fixed_string<260>	filename;
+	float3				location;
+	u32					resolution;
+	float				ambient_multiplier;
+	bool				depth_texture;
+};
+
+STATIC_SIZE_ASSERT( environment_probe_generate_parameters, 0x128 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_ENVIRONMENT_PROBE_GENERATE_PARAMETERS_H_INCLUDED
