456c58b168c81f163e6d7d0c218ba2394da357e5
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:15:09 +0000

    render: admit sky forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_sky_materials.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_SKY_MATERIALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_SKY_MATERIALS_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_fstage_sky_materials : public effect_material_base {
+public:
+	effect_fstage_sky_materials( ) { }
+	virtual ~effect_fstage_sky_materials( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_fstage_sky_materials, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_SKY_MATERIALS_H_INCLUDED
