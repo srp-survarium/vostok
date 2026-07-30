2502e49f90db589481cd3d1085e27bd9eee7c365
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:21:19 +0000

    render: admit editor wireframe accumulation effect

---
+++ sources/vostok/render/engine/sources/effect_editor_wireframe_accumulation.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_wireframe_accumulation.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_wireframe_accumulation::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b47e0]
+}
+
+} // namespace render
+} // namespace vostok
