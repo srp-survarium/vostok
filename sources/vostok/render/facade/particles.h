94a0165f2033ce139194e3a1328ff22e2a9d85d2
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 15:26:20 +0000

    render: restore scene view root

---
+++ sources/vostok/render/facade/particles.h
@@ -0,0 +1,16 @@
+#ifndef VOSTOK_RENDER_FACADE_PARTICLES_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_PARTICLES_H_INCLUDED
+
+namespace vostok {
+namespace particle {
+
+enum enum_particle_render_mode {
+	normal_particle_render_mode,
+	dots_particle_render_mode,
+	size_particle_render_mode,
+};
+
+} // namespace particle
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_PARTICLES_H_INCLUDED
