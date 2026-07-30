db9b322de9e65667c71258e9d76f94b96b210f23
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:11:10 +0000

    render: admit FXAA effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_fxaa.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_FXAA_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_FXAA_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_post_process_fxaa : public effect_descriptor {
+public:
+	effect_post_process_fxaa( ) { }
+	virtual ~effect_post_process_fxaa( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_post_process_fxaa, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_FXAA_H_INCLUDED
