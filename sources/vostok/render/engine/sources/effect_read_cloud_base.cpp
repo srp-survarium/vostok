a3561994d798309a42a143582895f56ece6b6034
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:50:17 +0000

    render: admit cloud base read effect

---
+++ sources/vostok/render/engine/sources/effect_read_cloud_base.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_read_cloud_base.h"
+
+namespace vostok {
+namespace render {
+
+void effect_read_cloud_base::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bd440]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_read_cloud_base.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_READ_CLOUD_BASE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_READ_CLOUD_BASE_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_read_cloud_base : public effect_descriptor {
+public:
+	effect_read_cloud_base( ) { }
+	virtual ~effect_read_cloud_base( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_read_cloud_base, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_READ_CLOUD_BASE_H_INCLUDED
