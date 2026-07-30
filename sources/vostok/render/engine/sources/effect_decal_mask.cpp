0907699ee017faff14a3a548c2e01be7a817a789
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:19:55 +0000

    render: admit decal mask effect

---
+++ sources/vostok/render/engine/sources/effect_decal_mask.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_decal_mask.h"
+
+namespace vostok {
+namespace render {
+
+void effect_decal_mask::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5710]
+}
+
+} // namespace render
+} // namespace vostok
