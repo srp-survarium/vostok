b266c95ed946fa4d07b9d8d73c42c23c481f34bf
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 02:18:02 +0000

    render: admit distortion accumulation stage

---
+++ sources/vostok/render/engine/sources/stage_accumulate_distortion.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "stage_accumulate_distortion.h"
+
+namespace vostok {
+namespace render {
+
+stage_accumulate_distortion::stage_accumulate_distortion(
+	renderer*			in_renderer,
+	renderer_context*	context
+) :
+	stage( in_renderer, context )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7489c0]
+}
+
+stage_accumulate_distortion::~stage_accumulate_distortion( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7489b0]
+}
+
+void stage_accumulate_distortion::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7489f0]
+}
+
+} // namespace render
+} // namespace vostok
