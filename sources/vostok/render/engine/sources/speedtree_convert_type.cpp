837f912a75d081fa28a0da08cbebbfe11df457b0
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 03:45:09 +0000

    render: admit speedtree type conversions

---
+++ sources/vostok/render/engine/sources/speedtree_convert_type.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "speedtree_convert_type.h"
+
+namespace vostok {
+namespace render {
+
+SpeedTree::Vec3 vostok_to_speedtree( float3 const& value )
+{
+	// FUNCTION BODY[0x709580]
+	return SpeedTree::Vec3( value.elements );
+}
+
+SpeedTree::Mat4x4 vostok_to_speedtree( float4x4 const& value )
+{
+	// FUNCTION BODY[0x7095b0]
+	SpeedTree::Mat4x4 result;
+	memory::copy(
+		&result.m_afRowCol[0],
+		sizeof( result.m_afRowCol ),
+		&value.elements[0],
+		sizeof( value.elements )
+	);
+	return result;
+}
+
+float3 speedtree_to_vostok( SpeedTree::Vec3 const& value )
+{
+	// FUNCTION BODY[0x709560]
+	return float3( value.x, value.y, value.z );
+}
+
+float4 speedtree_to_vostok( SpeedTree::Vec4 const& value )
+{
+	// FUNCTION BODY[0x709540]
+	return float4( value.x, value.y, value.z, value.w );
+}
+
+float4x4 speedtree_to_vostok( SpeedTree::Mat4x4 const& value )
+{
+	// FUNCTION BODY[0x7095a0]
+	float4x4 result;
+	memory::copy(
+		&result.elements[0],
+		sizeof( result.elements ),
+		&value.m_afRowCol[0],
+		sizeof( value.m_afRowCol )
+	);
+	return result;
+}
+
+} // namespace render
+} // namespace vostok
