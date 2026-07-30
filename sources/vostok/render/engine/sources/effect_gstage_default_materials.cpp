521f10b118df82f85076873b28334038787a32eb
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:41:07 +0000

    render: admit default g-stage effect

---
+++ sources/vostok/render/engine/sources/effect_gstage_default_materials.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "effect_gstage_default_materials.h"
+
+#include <vostok/render/facade/vertex_input_type.h>
+
+namespace vostok {
+namespace render {
+
+u32 get_stencil_mask( enum_vertex_input_type, bool )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5e9c90]
+	return 0;
+}
+
+void effect_gstage_default_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5e9cd0]
+}
+
+} // namespace render
+} // namespace vostok
