7a028334bbbf6cd20d636f6458355ef0948321ae
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:47:59 +0000

    render: admit clouds effect

---
+++ sources/vostok/render/engine/sources/effect_clouds.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_clouds.h"
+
+namespace vostok {
+namespace render {
+
+void effect_clouds::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bd510]
+}
+
+} // namespace render
+} // namespace vostok
