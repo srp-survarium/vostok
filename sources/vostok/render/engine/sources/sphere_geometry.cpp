24d3faed9d59640e78db887b7d7e92c5439ef967
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:51:47 +0000

    render: admit sphere geometry owner

---
+++ sources/vostok/render/engine/sources/sphere_geometry.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "sphere_geometry.h"
+
+namespace vostok {
+namespace render {
+
+sphere_geometry::sphere_geometry( u32, u32 ) :
+	m_stride		( sizeof( vertex_type ) ),
+	m_num_indices	( 0 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5ee6b0]
+}
+
+void sphere_geometry::draw( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5ee580]
+}
+
+} // namespace render
+} // namespace vostok
