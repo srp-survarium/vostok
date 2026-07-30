2e49db97a4cf16cef34f6384ad104a199f0b0673
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:43:50 +0000

    render: admit terrain g-stage effect

---
+++ sources/vostok/render/engine/sources/effect_gstage_terrain_materials.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_GSTAGE_TERRAIN_MATERIALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_GSTAGE_TERRAIN_MATERIALS_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_gstage_terrain_materials : public effect_material_base {
+public:
+	effect_gstage_terrain_materials( ) { }
+	virtual ~effect_gstage_terrain_materials( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_gstage_terrain_materials, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_GSTAGE_TERRAIN_MATERIALS_H_INCLUDED
