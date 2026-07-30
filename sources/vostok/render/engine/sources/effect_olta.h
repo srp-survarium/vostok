794a5bfcdee8d96ab70dab19c990eed01e2b5efb
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:01:40 +0000

    render: admit OLTA effect

---
+++ sources/vostok/render/engine/sources/effect_olta.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_OLTA_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_OLTA_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_olta : public effect_descriptor {
+public:
+	effect_olta( ) { }
+	virtual ~effect_olta( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_olta, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_OLTA_H_INCLUDED
