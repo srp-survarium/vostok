791296fee76d463afeedd848a22690529e66a6b9
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:22:52 +0000

    render: admit ssao accumulation effect

---
+++ sources/vostok/render/engine/sources/effect_ssao_accumulation.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_SSAO_ACCUMULATION_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_SSAO_ACCUMULATION_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_ssao_accumulation : public effect_descriptor {
+public:
+	effect_ssao_accumulation( ) { }
+	virtual ~effect_ssao_accumulation( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	custom_config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_ssao_accumulation, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_SSAO_ACCUMULATION_H_INCLUDED
