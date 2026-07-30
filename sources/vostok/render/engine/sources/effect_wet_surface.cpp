c6fe9bed757819b404ff4e0f03ad03f66cee7b73
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:34:44 +0000

    render: admit wet-surface effect

---
+++ sources/vostok/render/engine/sources/effect_wet_surface.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_wet_surface.h"
+
+namespace vostok {
+namespace render {
+
+void effect_wet_surface::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bea30]
+}
+
+} // namespace render
+} // namespace vostok
