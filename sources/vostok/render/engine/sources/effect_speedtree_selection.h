c41515c671f4e765ac5a7bf2def00cc615c0d3f4
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:28:26 +0000

    render: admit SpeedTree selection effect

---
+++ sources/vostok/render/engine/sources/effect_speedtree_selection.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_SPEEDTREE_SELECTION_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_SPEEDTREE_SELECTION_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_speedtree_selection : public effect_descriptor {
+public:
+	effect_speedtree_selection( ) { }
+	virtual ~effect_speedtree_selection( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_speedtree_selection, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_SPEEDTREE_SELECTION_H_INCLUDED
