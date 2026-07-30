86d58cf13060c43d34096c1e77a300c11ebad798
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:35:57 +0000

    render: admit god rays effect

---
+++ sources/vostok/render/engine/sources/effect_god_rays.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_god_rays.h"
+
+namespace vostok {
+namespace render {
+
+void effect_god_rays::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bc500]
+}
+
+} // namespace render
+} // namespace vostok
