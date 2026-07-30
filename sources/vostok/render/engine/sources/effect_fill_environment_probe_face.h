0f71ff3f986a46e1bce7c1aec37b0554f644bae4
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:02:16 +0000

    render: admit environment probe face effect

---
+++ sources/vostok/render/engine/sources/effect_fill_environment_probe_face.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_FILL_ENVIRONMENT_PROBE_FACE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_FILL_ENVIRONMENT_PROBE_FACE_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_fill_environment_probe_face : public effect_descriptor {
+public:
+	effect_fill_environment_probe_face( ) { }
+	virtual ~effect_fill_environment_probe_face( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_fill_environment_probe_face, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_FILL_ENVIRONMENT_PROBE_FACE_H_INCLUDED
