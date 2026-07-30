847bc540c1ec09a0f4436fd8dc697925655d3de6
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:13:07 +0000

    render: admit texture pool source

---
+++ sources/vostok/render/core/dx11/sources/texture_pool.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "texture_pool.h"
+
+namespace vostok {
+namespace render {
+
+texture_pool::~texture_pool( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x781f60]
+}
+
+} // namespace render
+} // namespace vostok
