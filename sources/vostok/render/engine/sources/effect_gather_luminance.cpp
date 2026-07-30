2169c333b3866804ba40614816735203957857e5
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:29:26 +0000

    render: admit luminance gather effect

---
+++ sources/vostok/render/engine/sources/effect_gather_luminance.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_gather_luminance.h"
+
+namespace vostok {
+namespace render {
+
+void effect_gather_luminance::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b4f90]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_gather_luminance.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_GATHER_LUMINANCE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_GATHER_LUMINANCE_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_gather_luminance : public effect_descriptor {
+public:
+	effect_gather_luminance( ) { }
+	virtual ~effect_gather_luminance( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_gather_luminance, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_GATHER_LUMINANCE_H_INCLUDED
