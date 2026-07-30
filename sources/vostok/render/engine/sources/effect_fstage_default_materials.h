d9e9c238e0c3b1a1b8c8444cccbf7a9203704ef0
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:08:53 +0000

    render: admit default forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_default_materials.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_DEFAULT_MATERIALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_DEFAULT_MATERIALS_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_fstage_default_materials : public effect_material_base {
+public:
+	effect_fstage_default_materials( ) { }
+	virtual ~effect_fstage_default_materials( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_fstage_default_materials, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_DEFAULT_MATERIALS_H_INCLUDED
