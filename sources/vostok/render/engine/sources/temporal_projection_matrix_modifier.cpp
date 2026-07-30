f5474cd3b12b9e847f83c0f93efd12f84736c26c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 03:59:42 +0000

    render: admit temporal projection modifier

---
+++ sources/vostok/render/engine/sources/temporal_projection_matrix_modifier.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "temporal_projection_matrix_modifier.h"
+
+namespace vostok {
+namespace render {
+
+temporal_projection_matrix_modifier::temporal_projection_matrix_modifier(
+	renderer_context* const context,
+	u32 const window_size_x,
+	u32 const window_size_y,
+	bool const need_modify
+) :
+	m_renderer_context( context ),
+	m_window_size_x( window_size_x ),
+	m_window_size_y( window_size_y ),
+	m_need_modify( need_modify ),
+	m_jittered( false )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x602b30]
+}
+
+void temporal_projection_matrix_modifier::push_jittering( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x602b80]
+}
+
+void temporal_projection_matrix_modifier::pop_jittering( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x602b50]
+}
+
+} // namespace render
+} // namespace vostok
