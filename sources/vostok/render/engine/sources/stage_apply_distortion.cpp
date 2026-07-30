f8ff6dfd4dde1083e8ba59e9e09b13bd049d3c03
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 02:19:48 +0000

    render: admit distortion application stage

---
+++ sources/vostok/render/engine/sources/stage_apply_distortion.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "stage_apply_distortion.h"
+
+namespace vostok {
+namespace render {
+
+stage_apply_distortion::~stage_apply_distortion( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x70bc50]
+}
+
+bool stage_apply_distortion::is_effects_ready( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x70bc40]
+	return false;
+}
+
+void stage_apply_distortion::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x70bc90]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/stage_apply_distortion.h
@@ -0,0 +1,43 @@
+#ifndef VOSTOK_RENDER_ENGINE_STAGE_APPLY_DISTORTION_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_STAGE_APPLY_DISTORTION_H_INCLUDED
+
+#include <vostok/render/core/res_effect.h>
+
+#include "stage.h"
+
+namespace vostok {
+namespace render {
+
+class renderer;
+class renderer_context;
+
+typedef resources::resource_ptr<
+	res_effect,
+	resources::unmanaged_intrusive_base
+> res_effect_ptr;
+
+class stage_apply_distortion : public stage {
+public:
+	stage_apply_distortion(
+		renderer* in_renderer,
+		renderer_context* in_context
+	) :
+		stage( in_renderer, in_context )
+	{
+	}
+
+	virtual ~stage_apply_distortion( );
+	virtual void execute( ) override;
+
+	bool is_effects_ready( ) const;
+
+private:
+	res_effect_ptr m_sh_apply_distortion;
+};
+
+STATIC_SIZE_ASSERT( stage_apply_distortion, 0x14 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_STAGE_APPLY_DISTORTION_H_INCLUDED
