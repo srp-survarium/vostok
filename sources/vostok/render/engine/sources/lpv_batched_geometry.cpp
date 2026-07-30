1697a27a30a0367d33e839641cea77cb0bc26372
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 01:51:41 +0000

    render: admit lpv batched geometry owner

---
+++ sources/vostok/render/engine/sources/lpv_batched_geometry.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "lpv_batched_geometry.h"
+
+namespace vostok {
+namespace render {
+
+lpv_batched_geometry::lpv_batched_geometry( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x784970]
+}
+
+void lpv_batched_geometry::add_vertex(
+	batched_vertex_source const&,
+	float3 const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x784590]
+}
+
+void lpv_batched_geometry::build(
+	vector<render_model_instance_impl_ptr>&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x784600]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/lpv_batched_geometry.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_LPV_BATCHED_GEOMETRY_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_LPV_BATCHED_GEOMETRY_H_INCLUDED
+
+#include "batched_geometry.h"
+
+namespace vostok {
+namespace render {
+
+struct lpv_batched_geometry : public batched_geometry<lpv_vertex> {
+	lpv_batched_geometry( );
+	virtual ~lpv_batched_geometry( ) { }
+
+	virtual void add_vertex(
+		batched_vertex_source const&	in_vertex,
+		float3 const&				not_modified_position
+	) override;
+
+	virtual void build(
+		vector<render_model_instance_impl_ptr>& model_instances
+	) override;
+};
+
+STATIC_SIZE_ASSERT( lpv_batched_geometry, 0xCC );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_LPV_BATCHED_GEOMETRY_H_INCLUDED
