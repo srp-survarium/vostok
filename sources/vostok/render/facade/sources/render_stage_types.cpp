c0178972ef8cf01c911d00fb5df66c6b5f4d34b7
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:53:54 +0000

    render: admit facade stage names

---
+++ sources/vostok/render/facade/sources/render_stage_types.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <vostok/render/facade/sources/render_stage_types.h>
+
+namespace vostok {
+namespace render {
+
+pcstr stage_type_to_string( enum_render_stage_type )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5872f0]
+	return "unknown";
+}
+
+} // namespace render
+} // namespace vostok
