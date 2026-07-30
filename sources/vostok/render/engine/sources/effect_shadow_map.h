76a546921ed0204036836e266db191193ed19f3e
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:24:46 +0000

    render: admit shadow-map effect

---
+++ sources/vostok/render/engine/sources/effect_shadow_map.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_SHADOW_MAP_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_SHADOW_MAP_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_shadow_map : public effect_descriptor {
+public:
+	effect_shadow_map( ) { }
+	virtual ~effect_shadow_map( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_shadow_map, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_SHADOW_MAP_H_INCLUDED
