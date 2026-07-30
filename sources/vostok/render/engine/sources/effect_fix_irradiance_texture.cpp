5ebc4f5a87fd1472e5794669c87552c66ff68499
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:05:31 +0000

    render: admit irradiance texture fix effect

---
+++ sources/vostok/render/engine/sources/effect_fix_irradiance_texture.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_fix_irradiance_texture.h"
+
+namespace vostok {
+namespace render {
+
+void effect_fix_irradiance_texture::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bdb00]
+}
+
+} // namespace render
+} // namespace vostok
