6cb37277389fff37e5df4ce029599c3feedccb9b
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:03:44 +0000

    render: admit particle selection effect

---
+++ sources/vostok/render/engine/sources/effect_particle_selection.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_particle_selection.h"
+
+namespace vostok {
+namespace render {
+
+void effect_particle_selection::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5a60]
+}
+
+} // namespace render
+} // namespace vostok
