a9ff0330b15df2a70fb2dab75ba381f9da239771
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 13:28:48 +0000

    render: admit debug triangles command

---
+++ sources/vostok/render/facade/sources/debug_draw_triangles_command.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "debug_draw_triangles_command.h"
+
+namespace vostok {
+namespace render {
+namespace debug {
+
+// STATE[STUB]
+draw_triangles_command::~draw_triangles_command( )
+{
+	// FUNCTION BODY[0x757460]
+}
+
+// STATE[STUB]
+void draw_triangles_command::execute( )
+{
+	// FUNCTION BODY[0x7574c0]
+}
+
+// STATE[STUB]
+void draw_triangles_command::defer_execution( )
+{
+	// FUNCTION BODY[0x757420]
+}
+
+} // namespace debug
+} // namespace render
+} // namespace vostok
