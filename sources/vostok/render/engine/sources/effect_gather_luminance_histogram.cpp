6bd90bb6420ceb02a36f429b7d831bb1de3d4af7
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:31:31 +0000

    render: admit luminance histogram gather effect

---
+++ sources/vostok/render/engine/sources/effect_gather_luminance_histogram.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_gather_luminance_histogram.h"
+
+namespace vostok {
+namespace render {
+
+void effect_gather_luminance_histogram::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bc650]
+}
+
+} // namespace render
+} // namespace vostok
