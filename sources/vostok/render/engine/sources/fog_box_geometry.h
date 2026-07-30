d7284d1baaafbe7b2437c171ea2eccdb9e097da4
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 02:28:00 +0000

    render: admit volume fog stage owner

---
+++ sources/vostok/render/engine/sources/fog_box_geometry.h
@@ -0,0 +1,24 @@
+#ifndef VOSTOK_RENDER_ENGINE_FOG_BOX_GEOMETRY_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_FOG_BOX_GEOMETRY_H_INCLUDED
+
+#include "res_geometry.h"
+
+namespace vostok {
+namespace render {
+
+struct fog_box_geometry {
+	fog_box_geometry( );
+	~fog_box_geometry( ) { }
+
+	void render( );
+
+private:
+	res_geometry_ptr m_geometry;
+};
+
+STATIC_SIZE_ASSERT( fog_box_geometry, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_FOG_BOX_GEOMETRY_H_INCLUDED
