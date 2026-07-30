9f9d8db30f571082670192e6f7d1db13b6404eda
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:43:50 +0000

    render: admit terrain g-stage effect

---
+++ sources/vostok/render/engine/sources/effect_gstage_terrain_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_gstage_terrain_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_gstage_terrain_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b7e50]
+}
+
+} // namespace render
+} // namespace vostok
