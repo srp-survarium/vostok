220ccb3242dad45914a4214b8e0bea035f46b476
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 01:54:13 +0000

    render: admit effect registration owner

---
+++ sources/vostok/render/engine/sources/renderer_register_effects.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "renderer_register_effects.h"
+
+namespace vostok {
+namespace render {
+
+void register_effect_descriptors( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x79c4f0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/renderer_register_effects.h
@@ -0,0 +1,12 @@
+#ifndef VOSTOK_RENDER_ENGINE_RENDERER_REGISTER_EFFECTS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_RENDERER_REGISTER_EFFECTS_H_INCLUDED
+
+namespace vostok {
+namespace render {
+
+void register_effect_descriptors( );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_RENDERER_REGISTER_EFFECTS_H_INCLUDED
