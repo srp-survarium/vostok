8a67cdfc1401568d55088802bd8442233da7b3ac
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 10:24:47 +0000

    render: consolidate effect option template utilities

---
+++ sources/vostok/render/core/dx11/sources/static_type.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+#include <vostok/configs.h>
+#include <vostok/render/core/static_type.h>
+
+namespace vostok {
+namespace render {
+
+u16 static_type::type_id_counter = configs::t_embedded_types;
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/core/static_type.h
@@ -0,0 +1,23 @@
+#ifndef VOSTOK_RENDER_CORE_STATIC_TYPE_H_INCLUDED
+#define VOSTOK_RENDER_CORE_STATIC_TYPE_H_INCLUDED
+
+namespace vostok {
+namespace render {
+
+struct static_type {
+	static u16 type_id_counter;
+
+	template < typename T >
+	static u16 get_type_id( )
+	{
+		// Observed instantiations: pcstr [0x636e0], bool [0x12bd90],
+		// and u16 [0x67fb0].
+		static u16 current_id = ++type_id_counter;
+		return current_id;
+	}
+};
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_CORE_STATIC_TYPE_H_INCLUDED
