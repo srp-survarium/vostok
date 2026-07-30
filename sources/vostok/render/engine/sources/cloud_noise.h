4034313f5bdc6dda434e0de7c82d1473b8c08fb2
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:06:23 +0000

    render: admit cloud noise helper

---
+++ sources/vostok/render/engine/sources/cloud_noise.h
@@ -0,0 +1,16 @@
+#ifndef VOSTOK_RENDER_ENGINE_CLOUD_NOISE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_CLOUD_NOISE_H_INCLUDED
+
+namespace vostok {
+namespace render {
+
+struct cloud_noise {
+	static float evaluate( float x, float y, u32 num_octaves );
+};
+
+STATIC_SIZE_ASSERT( cloud_noise, 0x1 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_CLOUD_NOISE_H_INCLUDED
