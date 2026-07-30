bc7fb1ddaac3447f0fb9ef9f23317b4362e23fb9
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:58:26 +0000

    render: admit skylight effect

---
+++ sources/vostok/render/engine/sources/effect_skylight.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_skylight.h"
+
+namespace vostok {
+namespace render {
+
+void effect_skylight::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bde30]
+}
+
+} // namespace render
+} // namespace vostok
