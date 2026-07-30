11dc8354c5a9f36ddeb1f07a756c1ea821826521
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 13:27:06 +0000

    render: admit debug lines command

---
+++ sources/vostok/render/facade/sources/debug_draw_lines_command.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "debug_draw_lines_command.h"
+
+namespace vostok {
+namespace render {
+namespace debug {
+
+// STATE[STUB]
+void draw_lines_command::execute( )
+{
+	// FUNCTION BODY[0x7573d0]
+}
+
+// STATE[STUB]
+void defer_execution( base_command&, base_scene_ptr const& )
+{
+	// FUNCTION BODY[0x757360]
+}
+
+// STATE[STUB]
+void defer_execution( base_command&, base_scene_view_ptr const& )
+{
+	// FUNCTION BODY[0x757330]
+}
+
+// STATE[STUB]
+void draw_lines_command::defer_execution( )
+{
+	// FUNCTION BODY[0x757390]
+}
+
+} // namespace debug
+} // namespace render
+} // namespace vostok
