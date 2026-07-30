9ffffded02c55af1ae691f615506a363423068c4
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 02:01:08 +0000

    render: admit hiz point list owner

---
+++ sources/vostok/render/engine/sources/hw_hiz_point_list.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "hw_hiz_point_list.h"
+
+namespace vostok {
+namespace render {
+
+hw_hiz_point_list::hw_hiz_point_list( ) :
+	m_num_points( 0 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7487d0]
+}
+
+void hw_hiz_point_list::initialize( u32 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x748680]
+}
+
+void hw_hiz_point_list::set_points( float4 const*, u32 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7485b0]
+}
+
+void hw_hiz_point_list::render( u32 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7486d0]
+}
+
+} // namespace render
+} // namespace vostok
