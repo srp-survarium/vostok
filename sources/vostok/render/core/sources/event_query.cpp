da8611f51210dc360c856b4ae009b7cd200f761e
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:37:49 +0000

    render: admit event query source

---
+++ sources/vostok/render/core/sources/event_query.cpp
@@ -0,0 +1,45 @@
+#include "pch.h"
+#include <vostok/render/core/sources/event_query.h>
+
+namespace vostok {
+namespace render {
+
+event_query::event_query( )
+	: m_query( 0 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559900]
+}
+
+event_query::~event_query( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559850]
+}
+
+void event_query::init_query( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5598d0]
+}
+
+void event_query::release_query( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559830]
+}
+
+void event_query::issue( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5598b0]
+}
+
+void event_query::wait( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559870]
+}
+
+} // namespace render
+} // namespace vostok
