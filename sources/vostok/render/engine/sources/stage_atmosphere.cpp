98c64590401280873a01237b3b46363f43562b9b
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:53:59 +0000

    render: admit atmosphere stage owner

---
+++ sources/vostok/render/engine/sources/stage_atmosphere.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "stage_atmosphere.h"
+
+namespace vostok {
+namespace render {
+
+void stage_atmosphere::fill_surfaces(
+	render_target_ptr,
+	render_target_ptr,
+	bool
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x60fb40]
+}
+
+stage_atmosphere::~stage_atmosphere( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x60ff30]
+}
+
+bool stage_atmosphere::is_effects_ready( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x60fb30]
+	return false;
+}
+
+void stage_atmosphere::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x60ffc0]
+}
+
+} // namespace render
+} // namespace vostok
