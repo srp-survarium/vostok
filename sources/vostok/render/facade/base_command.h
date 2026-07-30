e5c5913f9a7d6722d14f9b9116f9626ad35ec5be
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:44:40 +0000

    render: restore facade command owner

---
+++ sources/vostok/render/facade/base_command.h
@@ -0,0 +1,34 @@
+#ifndef VOSTOK_RENDER_FACADE_BASE_COMMAND_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_BASE_COMMAND_H_INCLUDED
+
+#include <vostok/render/engine/base_classes.h>
+
+namespace vostok {
+namespace render {
+
+class VOSTOK_NOVTABLE base_command {
+public:
+	base_command( bool is_deferred = false, bool depth = true )
+		: remove_frame_id( 0 ),
+		  is_deferred_command( is_deferred ),
+		  use_depth( depth )
+	{
+	}
+
+	virtual void execute( ) = 0;
+	virtual void defer_execution( ) { R_ASSERT( !is_deferred_command ); }
+	VOSTOK_DECLARE_PURE_VIRTUAL_DESTRUCTOR( base_command )
+
+public:
+	base_command* next;
+	base_command* deferred_next;
+	bool is_deferred_command;
+	bool use_depth;
+	VOSTOK_MAX_CACHE_LINE_PAD;
+	u32 remove_frame_id;
+};
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_BASE_COMMAND_H_INCLUDED
