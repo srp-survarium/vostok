bc64f4d3f977b74e3e1d473b3c15116c376c89ec
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 13:48:14 +0000

    render: restore skeleton cook data owner

--- sources/vostok/render/engine/sources/combined_model_cooker.h
+++ sources/vostok/render/engine/sources/combined_model_cooker.h
@@ -1,50 +1,11 @@
 #ifndef VOSTOK_RENDER_ENGINE_COMBINED_MODEL_COOKER_H_INCLUDED
 #define VOSTOK_RENDER_ENGINE_COMBINED_MODEL_COOKER_H_INCLUDED
 
-#include <vostok/animation/skeleton.h>
-#include <vostok/configs_binary_config.h>
-#include <vostok/fs/virtual_path_string.h>
+#include <vostok/render/facade/skeleton_combined_cook_data.h>
 #include <vostok/resources_cook_classes.h>
-#include <vostok/resources_managed_resource.h>
-#include <vostok/resources_unmanaged_resource.h>
 
 namespace vostok {
 namespace render {
-
-struct skeleton_combined_cook_data {
-	struct model_def {
-		model_def( ) { }
-		~model_def( ) { }
-
-		fs_new::virtual_path_string			base_model_name;
-		fs_new::virtual_path_string			part_name;
-		fs_new::virtual_path_string			material_name;
-		configs::binary_config_ptr			owner_model_config;
-		configs::binary_config_ptr			export_properties_config;
-		resources::unmanaged_resource_ptr	material_effects;
-		resources::managed_resource_ptr		converted_model;
-	};
-
-	explicit skeleton_combined_cook_data( bool owner_cook ) :
-		models_count	( 0 ),
-		owner_is_cook	( owner_cook )
-	{
-	}
-
-	~skeleton_combined_cook_data( ) { }
-
-	fs_new::virtual_path_string		skeleton_name;
-	animation::skeleton_ptr			skeleton;
-	fs_new::virtual_path_string		bind_pose_name;
-	resources::managed_resource_ptr	bind_pose;
-	configs::binary_config_ptr		model_config;
-	model_def						model_defs[8];
-	u8								models_count;
-	bool							owner_is_cook;
-};
-
-STATIC_SIZE_ASSERT( skeleton_combined_cook_data::model_def, 0x34C );
-STATIC_SIZE_ASSERT( skeleton_combined_cook_data, 0x1C98 );
 
 struct skeleton_combined_model_cook : public resources::translate_query_cook {
 	skeleton_combined_model_cook( );
---
+++ sources/vostok/render/facade/skeleton_combined_cook_data.h
@@ -0,0 +1,53 @@
+#ifndef VOSTOK_RENDER_FACADE_SKELETON_COMBINED_COOK_DATA_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_SKELETON_COMBINED_COOK_DATA_H_INCLUDED
+
+#include <vostok/animation/skeleton.h>
+#include <vostok/configs_binary_config.h>
+#include <vostok/fs/virtual_path_string.h>
+#include <vostok/resources_managed_resource.h>
+#include <vostok/resources_unmanaged_resource.h>
+
+namespace vostok {
+namespace render {
+
+struct skeleton_combined_cook_data {
+	struct model_def {
+		model_def( ) { }
+		~model_def( ) { }
+
+		fs_new::virtual_path_string			base_model_name;
+		fs_new::virtual_path_string			part_name;
+		fs_new::virtual_path_string			material_name;
+		configs::binary_config_ptr			owner_model_config;
+		configs::binary_config_ptr			export_properties_config;
+		resources::unmanaged_resource_ptr	material_effects;
+		resources::managed_resource_ptr		converted_model;
+	};
+
+	// STATE[STUB]
+	explicit skeleton_combined_cook_data( bool owner_cook )
+		: models_count( 0 ),
+		  owner_is_cook( owner_cook )
+	{
+		// FUNCTION BODY[0x62a40]
+	}
+
+	~skeleton_combined_cook_data( ) { }
+
+	fs_new::virtual_path_string		skeleton_name;
+	animation::skeleton_ptr			skeleton;
+	fs_new::virtual_path_string		bind_pose_name;
+	resources::managed_resource_ptr	bind_pose;
+	configs::binary_config_ptr		model_config;
+	model_def						model_defs[8];
+	u8								models_count;
+	bool							owner_is_cook;
+};
+
+STATIC_SIZE_ASSERT( skeleton_combined_cook_data::model_def, 0x34c );
+STATIC_SIZE_ASSERT( skeleton_combined_cook_data, 0x1c98 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_SKELETON_COMBINED_COOK_DATA_H_INCLUDED
