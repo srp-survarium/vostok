b82fb712b9bd900ccfe39feb67ea2b77cc612235
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:00:09 +0000

    render: admit eye adaptation effect

---
+++ sources/vostok/render/engine/sources/effect_eye_adaptation.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_eye_adaptation.h"
+
+namespace vostok {
+namespace render {
+
+void effect_eye_adaptation::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b4d00]
+}
+
+} // namespace render
+} // namespace vostok
