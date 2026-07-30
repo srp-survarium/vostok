76473708c5849ee3739cbb33d05ad309b145831c
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:58:18 +0000

    render: admit editor vertex alpha effect

---
+++ sources/vostok/render/engine/sources/effect_editor_vertex_alpha.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_vertex_alpha.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_vertex_alpha::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b4120]
+}
+
+} // namespace render
+} // namespace vostok
