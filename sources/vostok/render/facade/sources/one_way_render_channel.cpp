f554186a18ae82d015abf72cbec4e919f754e3c5
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:46:45 +0000

    render: admit facade command channel

---
+++ sources/vostok/render/facade/sources/one_way_render_channel.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include <vostok/render/facade/sources/one_way_render_channel.h>
+
+namespace vostok {
+namespace render {
+
+void one_way_render_channel::process_next_frame_commands( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x757510]
+}
+
+bool one_way_render_channel::render_process_commands( bool )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7575f0]
+	return false;
+}
+
+void one_way_render_channel::render_on_draw_scene(
+	base_scene_ptr const&,
+	base_scene_view_ptr const&,
+	bool
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7578b0]
+}
+
+void one_way_render_channel::render_on_end_frame( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x757710]
+}
+
+} // namespace render
+} // namespace vostok
