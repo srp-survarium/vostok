ec81eb2501867b91d985e315ca92abaf17345782
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:58:35 +0000

    render: admit exponential volume fog effect

---
+++ sources/vostok/render/engine/sources/effect_exponential_volume_fog.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_exponential_volume_fog.h"
+
+namespace vostok {
+namespace render {
+
+void effect_exponential_volume_fog::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bc970]
+}
+
+} // namespace render
+} // namespace vostok
