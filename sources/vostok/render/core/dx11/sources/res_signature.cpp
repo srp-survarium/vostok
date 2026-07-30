12c2a8ffb4a880f83586cbe67e9866b4dcf0df31
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 10:41:57 +0000

    render: admit dx11 shader signature source

---
+++ sources/vostok/render/core/dx11/sources/res_signature.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+
+#include <vostok/render/core/dx11/res_signature.h>
+
+namespace vostok {
+namespace render {
+
+res_signature::~res_signature( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x558840]
+}
+
+void res_signature::destroy_impl( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x558860]
+}
+
+} // namespace render
+} // namespace vostok
