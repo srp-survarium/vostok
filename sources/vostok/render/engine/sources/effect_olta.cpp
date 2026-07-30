958007a1d17cfceb9a34675fde6d040bedc71be8
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:01:40 +0000

    render: admit OLTA effect

---
+++ sources/vostok/render/engine/sources/effect_olta.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_olta.h"
+
+namespace vostok {
+namespace render {
+
+void effect_olta::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bb8a0]
+}
+
+} // namespace render
+} // namespace vostok
