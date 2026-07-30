830cf58acab7da4cfcb9bc3a87c82f21ab5f52a3
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:08:39 +0000

    render: admit editor selection effect

---
+++ sources/vostok/render/engine/sources/effect_editor_selection.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_selection.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_selection::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5cf0]
+}
+
+} // namespace render
+} // namespace vostok
