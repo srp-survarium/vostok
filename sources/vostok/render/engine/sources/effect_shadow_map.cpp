1c77f36eb319e18cd3506c78b8726152f6241a85
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:24:46 +0000

    render: admit shadow-map effect

---
+++ sources/vostok/render/engine/sources/effect_shadow_map.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_shadow_map.h"
+
+namespace vostok {
+namespace render {
+
+void effect_shadow_map::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5560]
+}
+
+} // namespace render
+} // namespace vostok
