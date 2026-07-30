cd343bd238f4863e38c63d4ad41e1f63a27f4d77
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 11:56:09 +0000

    render: consolidate shader macro owner

---
+++ sources/vostok/render/core/shader_macros.h
@@ -0,0 +1,86 @@
+#ifndef VOSTOK_RENDER_CORE_SHADER_MACROS_H_INCLUDED
+#define VOSTOK_RENDER_CORE_SHADER_MACROS_H_INCLUDED
+
+#include <cstring>
+#include <vostok/fixed_string.h>
+#include <vostok/fixed_vector.h>
+#include <vostok/fs/virtual_path_string.h>
+#include <vostok/render/core/quasi_singleton.h>
+#include <vostok/render/core/shader_configuration.h>
+
+namespace vostok {
+namespace render {
+
+enum {
+	shader_macros_max_count = 128,
+};
+
+struct shader_macro {
+	shader_macro( ) { }
+
+	shader_macro( shader_macro const& other )
+		: name( other.name ), definition( other.definition )
+	{
+	}
+
+	fs_new::virtual_path_string name;
+	fixed_string<256> definition;
+};
+
+typedef fixed_vector<shader_macro, shader_macros_max_count>
+	shader_defines_list;
+typedef fixed_vector<pcstr, shader_macros_max_count>
+	shader_defines_names_list;
+typedef fixed_vector<fs_new::virtual_path_string, shader_macros_max_count>
+	shader_declarated_macroses_list;
+
+struct shader_macros_sort_predicate {
+	bool operator()( pcstr left, pcstr right ) const
+	{
+		return std::strcmp( left, right ) < 0;
+	}
+};
+
+class shader_macros : public quasi_singleton<shader_macros> {
+public:
+	shader_macros( );
+	~shader_macros( ) { }
+
+	void fill_shader_macro_list(
+		shader_defines_list& macros,
+		shader_configuration shader_config
+	);
+
+	void merge_with_declared_macroses(
+		shader_declarated_macroses_list const&,
+		shader_defines_list&
+	)
+	{
+		// STATE[STUB]
+	}
+
+	void append_macros_name( shader_defines_names_list&, pcstr )
+	{
+		// STATE[STUB]
+	}
+
+private:
+	void register_available_macros( );
+	void fill_global_macros( shader_defines_list& defines );
+	void fill_shader_configuration_macros(
+		shader_defines_list& macros,
+		shader_configuration shader_config
+	);
+
+private:
+	shader_defines_names_list m_available_macros;
+	shader_defines_list m_working_macro_list;
+};
+
+STATIC_SIZE_ASSERT( shader_macro, 0x220 );
+STATIC_SIZE_ASSERT( shader_macros, 0x11210 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_CORE_SHADER_MACROS_H_INCLUDED
