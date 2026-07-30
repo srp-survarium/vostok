4f76e1c5c365b155fe8e3a1988d8ab0bee2fa87f
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:56:17 +0000

    render: admit sun stage owner

---
+++ sources/vostok/render/engine/sources/stage_sun.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "stage_sun.h"
+
+namespace vostok {
+namespace render {
+
+stage_sun::stage_sun(
+	renderer*				in_renderer,
+	renderer_context*		in_context,
+	cloud_interp_textures&	cloud_interp_textures,
+	cloud_simulation&		simulation
+) :
+	stage						( in_renderer, in_context ),
+	m_cloud_interp_textures		( cloud_interp_textures ),
+	m_simulation				( simulation )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x626a70]
+}
+
+bool stage_sun::is_effects_ready( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x626370]
+	return false;
+}
+
+void stage_sun::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x626380]
+}
+
+} // namespace render
+} // namespace vostok
