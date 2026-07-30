83ed4696cfaa93531d6fa2912d67034abd068bc7
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:21:02 +0000

    render: admit rain effect

---
+++ sources/vostok/render/engine/sources/effect_rain.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_rain.h"
+
+namespace vostok {
+namespace render {
+
+void effect_rain::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bca60]
+}
+
+} // namespace render
+} // namespace vostok
