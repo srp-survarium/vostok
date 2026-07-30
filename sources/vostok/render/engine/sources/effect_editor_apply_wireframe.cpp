acc8cc99d0866bffe15e42ec4ad7553aceba10f0
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:10:49 +0000

    render: admit editor apply wireframe effect

---
+++ sources/vostok/render/engine/sources/effect_editor_apply_wireframe.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_apply_wireframe.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_apply_wireframe::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bb510]
+}
+
+} // namespace render
+} // namespace vostok
