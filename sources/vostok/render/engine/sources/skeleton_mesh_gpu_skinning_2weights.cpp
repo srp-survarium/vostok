8fda5c192e18c36c1871ba776e0f4bbd3277d2a6
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:08:32 +0000

    render: admit two-weight gpu skinning

---
+++ sources/vostok/render/engine/sources/skeleton_mesh_gpu_skinning_2weights.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "skeleton_mesh_gpu_skinning_2weights.h"
+
+namespace vostok {
+namespace render {
+
+skeleton_mesh_gpu_skinning_2weights::skeleton_mesh_gpu_skinning_2weights( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3600]
+}
+
+skeleton_mesh_gpu_skinning_2weights::~skeleton_mesh_gpu_skinning_2weights( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f35c0]
+}
+
+void skeleton_mesh_gpu_skinning_2weights::load(
+	configs::binary_config_value const&,
+	memory::chunk_reader&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3430]
+}
+
+void skeleton_mesh_gpu_skinning_2weights::update( vector< float4x4 > const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3420]
+}
+
+} // namespace render
+} // namespace vostok
