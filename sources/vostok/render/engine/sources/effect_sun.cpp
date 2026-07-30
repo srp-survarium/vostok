6983b24ab302acfd9d73307c930293c35d208a75
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:57:27 +0000

    render: admit sun effect

---
+++ sources/vostok/render/engine/sources/effect_sun.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_sun.h"
+
+namespace vostok {
+namespace render {
+
+void effect_sun::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bdbd0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_sun.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_SUN_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_SUN_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_sun : public effect_descriptor {
+public:
+	effect_sun( ) { }
+	virtual ~effect_sun( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_sun, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_SUN_H_INCLUDED
