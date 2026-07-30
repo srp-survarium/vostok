8a091fd4a25df3a98a0044047ad122583e55700d
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:01:18 +0000

    render: admit sampler registration

---
+++ sources/vostok/render/engine/sources/register_samplers.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "register_samplers.h"
+
+namespace vostok {
+namespace render {
+
+void register_samplers( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x79ca50]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/register_samplers.h
@@ -0,0 +1,12 @@
+#ifndef VOSTOK_RENDER_ENGINE_REGISTER_SAMPLERS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_REGISTER_SAMPLERS_H_INCLUDED
+
+namespace vostok {
+namespace render {
+
+void register_samplers( );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_REGISTER_SAMPLERS_H_INCLUDED
