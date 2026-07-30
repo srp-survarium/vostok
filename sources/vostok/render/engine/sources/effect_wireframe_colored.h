b2059f23c9e3b68bbaad1e514fa12ff311e30803
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:48:09 +0000

    render: admit wireframe colored effect

---
+++ sources/vostok/render/engine/sources/effect_wireframe_colored.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_WIREFRAME_COLORED_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_WIREFRAME_COLORED_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_wireframe_colored : public effect_descriptor {
+public:
+	effect_wireframe_colored( ) { }
+	virtual ~effect_wireframe_colored( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_wireframe_colored, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_WIREFRAME_COLORED_H_INCLUDED
