872f744f1fce5e54c50de2faeb3fa6d6ab027d99
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:43:00 +0000

    render: admit system line effect

---
+++ sources/vostok/render/engine/sources/effect_system_line.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_system_line.h"
+
+namespace vostok {
+namespace render {
+
+void effect_system_line::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5e30]
+}
+
+} // namespace render
+} // namespace vostok
