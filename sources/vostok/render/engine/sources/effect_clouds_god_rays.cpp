6bf35a16443b4bd12de2009892fdfe085be2ac28
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:49:00 +0000

    render: admit clouds god rays effect

---
+++ sources/vostok/render/engine/sources/effect_clouds_god_rays.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_clouds_god_rays.h"
+
+namespace vostok {
+namespace render {
+
+void effect_clouds_god_rays::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bd350]
+}
+
+} // namespace render
+} // namespace vostok
