3e7e9a307a1f3ff8879fe4c7430d6c788e22ea95
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:24:51 +0000

    render: admit volume sphere forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_volume_sphere_base_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_fstage_volume_sphere_base_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_fstage_volume_sphere_base_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7ba5c0]
+}
+
+} // namespace render
+} // namespace vostok
