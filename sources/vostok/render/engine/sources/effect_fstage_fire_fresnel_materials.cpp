e42cf464d8019b7a00c2b1361f023910fce7a056
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:11:57 +0000

    render: admit fire fresnel forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_fire_fresnel_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_fstage_fire_fresnel_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_fstage_fire_fresnel_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bab10]
+}
+
+} // namespace render
+} // namespace vostok
