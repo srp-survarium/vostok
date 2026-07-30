485258d3f999e987b10731c7ec3b37e742e174c8
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:21:19 +0000

    render: admit editor wireframe accumulation effect

---
+++ sources/vostok/render/engine/sources/effect_editor_wireframe_accumulation.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_EDITOR_WIREFRAME_ACCUMULATION_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_EDITOR_WIREFRAME_ACCUMULATION_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_editor_wireframe_accumulation : public effect_material_base {
+public:
+	effect_editor_wireframe_accumulation( ) { }
+	virtual ~effect_editor_wireframe_accumulation( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_editor_wireframe_accumulation, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_EDITOR_WIREFRAME_ACCUMULATION_H_INCLUDED
