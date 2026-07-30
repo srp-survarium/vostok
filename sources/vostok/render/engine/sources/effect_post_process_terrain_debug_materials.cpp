591d4c2a5e2a0053937e0f9b7fd5d45379e70d07
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:19:14 +0000

    render: admit terrain-debug postprocess effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_terrain_debug_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_terrain_debug_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_terrain_debug_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b9880]
+}
+
+} // namespace render
+} // namespace vostok
