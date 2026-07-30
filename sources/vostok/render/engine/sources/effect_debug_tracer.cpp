5c2933375a647fd4d3fae3b43d25dbeb8ed92718
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:50:24 +0000

    render: admit debug tracer effect

---
+++ sources/vostok/render/engine/sources/effect_debug_tracer.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_debug_tracer.h"
+
+namespace vostok {
+namespace render {
+
+void effect_debug_tracer::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5130]
+}
+
+} // namespace render
+} // namespace vostok
