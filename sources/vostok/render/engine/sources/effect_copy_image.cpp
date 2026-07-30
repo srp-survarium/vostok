a5f2b7d4db54baf319bd5b5dfaaae57de9efd882
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:44:13 +0000

    render: admit copy image effect

---
+++ sources/vostok/render/engine/sources/effect_copy_image.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_copy_image.h"
+
+namespace vostok {
+namespace render {
+
+void effect_copy_image::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b4e20]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_copy_image.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_COPY_IMAGE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_COPY_IMAGE_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_copy_image : public effect_descriptor {
+public:
+	effect_copy_image( ) { }
+	virtual ~effect_copy_image( ) { }
+
+	virtual void compile(
+		effect_compiler&,
+		custom_config_value const&
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_copy_image, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_COPY_IMAGE_H_INCLUDED
