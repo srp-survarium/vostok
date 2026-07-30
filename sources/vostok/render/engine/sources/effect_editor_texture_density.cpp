6e208544fe27d28d72ae1a83ba3f5ca67c470e2c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:00:00 +0000

    render: admit editor texture density effect

---
+++ sources/vostok/render/engine/sources/effect_editor_texture_density.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_texture_density.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_texture_density::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b46a0]
+}
+
+} // namespace render
+} // namespace vostok
