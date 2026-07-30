60389c2e7f762eca21e5eaafae5c1db38e5884fb
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:40:39 +0000

    render: admit forward system effect

---
+++ sources/vostok/render/engine/sources/effect_forward_system.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_forward_system.h"
+
+namespace vostok {
+namespace render {
+
+void effect_forward_system::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bb380]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_forward_system.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_FORWARD_SYSTEM_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_FORWARD_SYSTEM_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_forward_system : public effect_descriptor {
+public:
+	effect_forward_system( ) { }
+	virtual ~effect_forward_system( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_forward_system, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_FORWARD_SYSTEM_H_INCLUDED
