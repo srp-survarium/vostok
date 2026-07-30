316497ec4fb86e39db23dd351931c891217ba629
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:25:30 +0000

    render: restore state utility owner

---
+++ sources/vostok/render/core/dx11/state_utils.h
@@ -0,0 +1,54 @@
+#ifndef VOSTOK_RENDER_CORE_DX11_STATE_UTILS_H_INCLUDED
+#define VOSTOK_RENDER_CORE_DX11_STATE_UTILS_H_INCLUDED
+
+#include <boost/crc.hpp>
+#include <vostok/render/core/render_include.h>
+
+namespace vostok {
+namespace render {
+namespace state_utils {
+
+namespace crc {
+	typedef boost::crc_optimal<
+		32,
+		0x04c11db7,
+		0,
+		0,
+		true,
+		false
+	> processor;
+
+	void process( processor& value_processor, u32 value );
+} // namespace crc
+
+void reset( D3D11_RASTERIZER_DESC& desc );
+void reset( D3D11_DEPTH_STENCIL_DESC& desc );
+void reset( D3D11_BLEND_DESC& desc );
+
+bool operator==(
+	D3D11_RASTERIZER_DESC const& left,
+	D3D11_RASTERIZER_DESC const& right
+);
+bool operator==(
+	D3D11_DEPTH_STENCIL_DESC const& left,
+	D3D11_DEPTH_STENCIL_DESC const& right
+);
+bool operator==(
+	D3D11_BLEND_DESC const& left,
+	D3D11_BLEND_DESC const& right
+);
+bool operator==(
+	D3D11_SAMPLER_DESC const& left,
+	D3D11_SAMPLER_DESC const& right
+);
+
+u32 get_hash( D3D11_RASTERIZER_DESC const& desc );
+u32 get_hash( D3D11_DEPTH_STENCIL_DESC const& desc );
+u32 get_hash( D3D11_BLEND_DESC const& desc );
+u32 get_hash( D3D11_SAMPLER_DESC const& desc );
+
+} // namespace state_utils
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_CORE_DX11_STATE_UTILS_H_INCLUDED
