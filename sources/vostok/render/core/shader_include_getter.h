cc18e674c4eb23329810d0c260f6bc82acd12890
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 09:50:15 +0000

    render: admit shader include getter

---
+++ sources/vostok/render/core/shader_include_getter.h
@@ -0,0 +1,20 @@
+#ifndef VOSTOK_RENDER_CORE_SHADER_INCLUDE_GETTER_H_INCLUDED
+#define VOSTOK_RENDER_CORE_SHADER_INCLUDE_GETTER_H_INCLUDED
+
+namespace vostok {
+namespace render {
+
+struct shader_include_getter {
+	shader_include_getter( ) { }
+	virtual pcstr get_shader_include( pcstr include_file_name ) const
+	{
+		return include_file_name;
+	}
+};
+
+STATIC_SIZE_ASSERT( shader_include_getter, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_CORE_SHADER_INCLUDE_GETTER_H_INCLUDED
