fd139b1a05d7ee2789b2bfad9f6ceb8152f0c1ad
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:23:52 +0000

    render: admit editor gbuffer display effect

---
+++ sources/vostok/render/engine/sources/effect_editor_gbuffer_to_screen.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_gbuffer_to_screen.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_gbuffer_to_screen::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7c01e0]
+}
+
+} // namespace render
+} // namespace vostok
