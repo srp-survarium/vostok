e7c1b22b025715d9633c007ca913e946541133a0
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 11:33:45 +0000

    render: admit texture cook registration

---
+++ sources/vostok/render/core/dx11/sources/texture_cook_register.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+namespace vostok {
+namespace render {
+
+void register_texture_cook( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x773ad0]
+}
+
+void unregister_texture_cook( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x773c30]
+}
+
+} // namespace render
+} // namespace vostok
