a30f2db284daeb75cda70608c9db5af007be6d12
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:36:19 +0000

    render: admit light propagation effect

---
+++ sources/vostok/render/engine/sources/effect_light_propagation_volumes.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_light_propagation_volumes.h"
+
+namespace vostok {
+namespace render {
+
+void effect_light_propagation_volumes::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b3d70]
+}
+
+} // namespace render
+} // namespace vostok
