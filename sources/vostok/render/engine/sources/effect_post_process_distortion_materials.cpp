5e7eb1d217d0047d4149f0efc4a5778afc24e388
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:07:33 +0000

    render: admit postprocess distortion effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_distortion_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_distortion_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_distortion_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b9af0]
+}
+
+} // namespace render
+} // namespace vostok
