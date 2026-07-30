63d1039e7524ef9ba322f4869001e85d8d3f9fbd
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 11:40:47 +0000

    render: consolidate shader resource template

---
+++ sources/vostok/render/core/dx11/res_xs.h
@@ -0,0 +1,105 @@
+#ifndef VOSTOK_RENDER_CORE_DX11_RES_XS_H_INCLUDED
+#define VOSTOK_RENDER_CORE_DX11_RES_XS_H_INCLUDED
+
+#include <vostok/render/core/res_sampler_list.h>
+#include <vostok/render/core/res_texture_list.h>
+#include <vostok/render/core/resource_intrusive_base.h>
+#include <vostok/render/core/shader_constant_table.h>
+#include <vostok/render/core/dx11/res_xs_hw.h>
+#include <vostok/render/core/dx11/xs_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class resource_manager;
+class res_effect;
+
+template < typename shader_data >
+class res_xs : public resource_intrusive_base {
+	typedef intrusive_ptr<
+		res_xs_hw<shader_data>,
+		resource_intrusive_base,
+		threading::single_threading_policy
+	> hardware_shader_ptr;
+	typedef intrusive_ptr<
+		res_texture_list,
+		resource_intrusive_base,
+		threading::single_threading_policy
+	> texture_list_ptr;
+
+	friend class resource_intrusive_base;
+	friend class resource_manager;
+	friend class res_effect;
+
+	explicit res_xs( xs_descriptor<shader_data> const& )
+		: m_is_registered( false )
+	{
+		// STATE[STUB]
+		// Observed vs_data instantiation [0x12f4d0].
+	}
+
+	void destroy_impl( ) const
+	{
+		// STATE[STUB]
+	}
+
+public:
+	bool equal( xs_descriptor<shader_data> const& descriptor ) const
+	{
+		return compare( descriptor ) == 0;
+	}
+
+	s32 compare( xs_descriptor<shader_data> const& ) const
+	{
+		// STATE[STUB]
+		// Observed vs_data instantiation [0x12f690].
+		return 0;
+	}
+
+	bool less( xs_descriptor<shader_data> const& descriptor ) const
+	{
+		return compare( descriptor ) < 0;
+	}
+
+	void apply( ) const;
+
+	bool is_registered( ) const { return m_is_registered; }
+	void mark_registered( ) { m_is_registered = true; }
+
+	void rebind_samplers( )
+	{
+		if ( m_samplers )
+			m_samplers->rebind( );
+	}
+
+	res_xs_hw<shader_data> const* hardware_shader( ) const
+	{
+		return m_hardware_shader.c_ptr( );
+	}
+
+	~res_xs( ) { }
+
+private:
+	hardware_shader_ptr m_hardware_shader;
+	shader_constant_table_ptr m_constants;
+
+public:
+	texture_list_ptr m_textures;
+
+private:
+	res_sampler_list_ptr m_samplers;
+	bool m_is_registered;
+};
+
+typedef res_xs<vs_data> res_vs;
+typedef res_xs<gs_data> res_gs;
+typedef res_xs<ps_data> res_ps;
+
+STATIC_SIZE_ASSERT( res_xs<vs_data>, 0x18 );
+STATIC_SIZE_ASSERT( res_xs<gs_data>, 0x18 );
+STATIC_SIZE_ASSERT( res_xs<ps_data>, 0x18 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_CORE_DX11_RES_XS_H_INCLUDED
