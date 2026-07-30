b6cd33881364f6b73b4bf6bf8c96111e84dd2ce0
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 13:19:14 +0000

    render: admit facade ui renderer

---
+++ sources/vostok/render/facade/ui_renderer.h
@@ -0,0 +1,6 @@
+#ifndef VOSTOK_RENDER_FACADE_UI_RENDERER_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_UI_RENDERER_H_INCLUDED
+
+#include <vostok/render/facade/sources/ui_renderer.h>
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_UI_RENDERER_H_INCLUDED
