2090c1f0d86c210b4b6754a5dec8d85d58392d7e
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 02:52:47 +0000

    render: admit grass world owner

---
+++ sources/vostok/render/engine/sources/trample_desc.h
@@ -0,0 +1,27 @@
+#ifndef VOSTOK_RENDER_ENGINE_TRAMPLE_DESC_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_TRAMPLE_DESC_H_INCLUDED
+
+#include <vostok/math_float3.h>
+
+namespace vostok {
+namespace render {
+
+struct trample_desc {
+	trample_desc( ) :
+		position	( 0.0f, 0.0f, 0.0f ),
+		radius		( 0.0f ),
+		multiplier	( 0.0f )
+	{
+	}
+
+	float3	position;
+	float	radius;
+	float	multiplier;
+};
+
+STATIC_SIZE_ASSERT( trample_desc, 0x14 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_TRAMPLE_DESC_H_INCLUDED
