a474763ebc1ac7c7b7080b91545171332baa6b12
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:25:13 +0000

    render: admit ssao downsample effect

---
+++ sources/vostok/render/engine/sources/effect_ssao_downsample_position_and_normal.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_ssao_downsample_position_and_normal.h"
+
+namespace vostok {
+namespace render {
+
+void effect_ssao_downsample_position_and_normal::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7be290]
+}
+
+} // namespace render
+} // namespace vostok
