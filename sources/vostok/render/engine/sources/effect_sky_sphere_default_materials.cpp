7a1731e70eeffcd80f6f09ca78d6c57b308e5c1a
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:26:34 +0000

    render: admit sky-sphere material effect

---
+++ sources/vostok/render/engine/sources/effect_sky_sphere_default_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_sky_sphere_default_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_sky_sphere_default_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b7770]
+}
+
+} // namespace render
+} // namespace vostok
