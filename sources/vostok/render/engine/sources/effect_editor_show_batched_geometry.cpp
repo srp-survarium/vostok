2a9baef3849a7d0307d944c3cd08c76f46f53307
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:15:11 +0000

    render: admit editor batched geometry effect

---
+++ sources/vostok/render/engine/sources/effect_editor_show_batched_geometry.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_show_batched_geometry.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_show_batched_geometry::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b4620]
+}
+
+} // namespace render
+} // namespace vostok
