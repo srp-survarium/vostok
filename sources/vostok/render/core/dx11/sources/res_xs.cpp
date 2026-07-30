d8a4d52be304146cd5a2c931eadb84bf5e541d32
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 11:41:30 +0000

    render: admit shader resource source

---
+++ sources/vostok/render/core/dx11/sources/res_xs.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <vostok/render/core/res_xs.h>
+
+namespace vostok {
+namespace render {
+
+template <>
+void res_xs<ps_data>::apply( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b1b00]
+}
+
+template <>
+void res_xs<gs_data>::apply( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b1950]
+}
+
+template <>
+void res_xs<vs_data>::apply( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b1880]
+}
+
+} // namespace render
+} // namespace vostok
