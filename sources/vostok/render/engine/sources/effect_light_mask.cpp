b628930011a982417e61cf175964308974d2a7a2
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:50:23 +0000

    render: admit light mask effect

---
+++ sources/vostok/render/engine/sources/effect_light_mask.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_light_mask.h"
+
+namespace vostok {
+namespace render {
+
+void effect_light_mask::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7be000]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_light_mask.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_LIGHT_MASK_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_LIGHT_MASK_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_light_mask : public effect_descriptor {
+public:
+	effect_light_mask( ) { }
+	virtual ~effect_light_mask( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_light_mask, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_LIGHT_MASK_H_INCLUDED
