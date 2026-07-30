311060b6b46a7aeea44c4c672b671e7122a527e5
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 02:00:06 +0000

    render: admit hiz occlusion manager owner

---
+++ sources/vostok/render/engine/sources/hw_hiz_point_list.h
@@ -0,0 +1,30 @@
+#ifndef VOSTOK_RENDER_ENGINE_HW_HIZ_POINT_LIST_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_HW_HIZ_POINT_LIST_H_INCLUDED
+
+#include <vostok/math_float4.h>
+#include <vostok/render/core/dx11/res_declaration.h>
+#include <vostok/render/core/untyped_buffer.h>
+
+namespace vostok {
+namespace render {
+
+struct hw_hiz_point_list {
+	hw_hiz_point_list( );
+	~hw_hiz_point_list( ) { }
+
+	void set_points( float4 const* in_data, u32 culling_results_buffer_width );
+	void initialize( u32 num_points );
+	void render( u32 num_points );
+
+private:
+	res_declaration_ptr	m_declaration;
+	untyped_buffer_ptr	m_vertex_buffer;
+	u32					m_num_points;
+};
+
+STATIC_SIZE_ASSERT( hw_hiz_point_list, 0xC );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_HW_HIZ_POINT_LIST_H_INCLUDED
---
+++ sources/vostok/render/engine/sources/sphere_occluder_geometry.h
@@ -0,0 +1,24 @@
+#ifndef VOSTOK_RENDER_ENGINE_SPHERE_OCCLUDER_GEOMETRY_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_SPHERE_OCCLUDER_GEOMETRY_H_INCLUDED
+
+#include "res_geometry.h"
+
+namespace vostok {
+namespace render {
+
+struct sphere_occluder_geometry {
+	sphere_occluder_geometry( );
+	~sphere_occluder_geometry( ) { }
+
+	void render( );
+
+private:
+	res_geometry_ptr m_geometry;
+};
+
+STATIC_SIZE_ASSERT( sphere_occluder_geometry, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_SPHERE_OCCLUDER_GEOMETRY_H_INCLUDED
