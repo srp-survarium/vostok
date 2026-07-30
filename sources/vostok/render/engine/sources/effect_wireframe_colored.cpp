c74c957713d0751d130baa59658bb9f61d61147a
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:48:09 +0000

    render: admit wireframe colored effect

---
+++ sources/vostok/render/engine/sources/effect_wireframe_colored.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_wireframe_colored.h"
+
+namespace vostok {
+namespace render {
+
+void effect_wireframe_colored::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5dc0]
+}
+
+} // namespace render
+} // namespace vostok
