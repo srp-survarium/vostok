b674bca8b947b22f99b94513d689a691790a8e85
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 13:38:16 +0000

    render: admit model vertex buffer command

---
+++ sources/vostok/render/facade/sources/update_model_vertex_buffer_command.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "update_model_vertex_buffer_command.h"
+
+namespace vostok {
+namespace render {
+
+// STATE[STUB]
+update_model_vertex_buffer_command::~update_model_vertex_buffer_command( )
+{
+	// FUNCTION BODY[0x757200]
+}
+
+// STATE[STUB]
+void update_model_vertex_buffer_command::execute( )
+{
+	// FUNCTION BODY[0x7571f0]
+}
+
+} // namespace render
+} // namespace vostok
