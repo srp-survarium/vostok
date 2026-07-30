beb96c19828a6a2287ed3fc0dd99b507efca6c24
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:54:56 +0000

    render: admit organic lighting-stage effect

---
+++ sources/vostok/render/engine/sources/effect_lighting_stage_organic_base_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_lighting_stage_organic_base_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_lighting_stage_organic_base_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5e8f90]
+}
+
+} // namespace render
+} // namespace vostok
