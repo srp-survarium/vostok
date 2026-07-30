6adbbe22e08d77881a71a58e89b6445e0e248a29
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:49:03 +0000

    render: admit default distortion material effect

---
+++ sources/vostok/render/engine/sources/effect_distortion_stage_default_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_distortion_stage_default_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_distortion_stage_default_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7ba150]
+}
+
+} // namespace render
+} // namespace vostok
