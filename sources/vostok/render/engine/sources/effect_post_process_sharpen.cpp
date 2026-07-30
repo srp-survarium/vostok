3aa78500c006dc7d1f3ceae4a6a0967135aacb97
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:15:05 +0000

    render: admit sharpen effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_sharpen.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_sharpen.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_sharpen::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bbc70]
+}
+
+} // namespace render
+} // namespace vostok
