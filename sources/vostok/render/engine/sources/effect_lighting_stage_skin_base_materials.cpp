0cdfbe5ecc8c9a8453d1af895b11bdd8447d7aa7
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:56:32 +0000

    render: admit skin lighting-stage effect

---
+++ sources/vostok/render/engine/sources/effect_lighting_stage_skin_base_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_lighting_stage_skin_base_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_lighting_stage_skin_base_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5eca00]
+}
+
+} // namespace render
+} // namespace vostok
