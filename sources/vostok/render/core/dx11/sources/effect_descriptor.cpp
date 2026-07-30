a39049108af876dc6d134f5cfe09747cd6c729d9
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:07:48 +0000

    render: admit effect descriptor source

---
+++ sources/vostok/render/core/dx11/sources/effect_descriptor.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+effect_descriptor::effect_descriptor( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559810]
+}
+
+effect_descriptor::~effect_descriptor( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559800]
+}
+
+void effect_descriptor::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x559820]
+}
+
+} // namespace render
+} // namespace vostok
