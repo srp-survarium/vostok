cbe1cce3c311ad567ad3b825c53d337e6f82904b
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 02:21:37 +0000

    render: admit lighting resolve stage

---
+++ sources/vostok/render/engine/sources/stage_resolve_lighting.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "stage_resolve_lighting.h"
+
+namespace vostok {
+namespace render {
+
+bool stage_resolve_lighting::is_effects_ready( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x613990]
+	return false;
+}
+
+void stage_resolve_lighting::render_models(
+	vector<render_surface_instance*>&,
+	u32&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6139a0]
+}
+
+void stage_resolve_lighting::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x613c90]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/stage_resolve_lighting.h
@@ -0,0 +1,56 @@
+#ifndef VOSTOK_RENDER_ENGINE_STAGE_RESOLVE_LIGHTING_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_STAGE_RESOLVE_LIGHTING_H_INCLUDED
+
+#include <vostok/render/core/memory.h>
+#include <vostok/render/core/res_effect.h>
+
+#include "stage.h"
+
+namespace vostok {
+namespace render {
+
+class renderer;
+class renderer_context;
+class shader_constant_host;
+struct render_surface_instance;
+
+typedef resources::resource_ptr<
+	res_effect,
+	resources::unmanaged_intrusive_base
+> res_effect_ptr;
+
+class stage_resolve_lighting : public stage {
+public:
+	stage_resolve_lighting(
+		renderer* in_renderer,
+		renderer_context* in_context
+	) :
+		stage( in_renderer, in_context ),
+		m_wind_info_parameters( 0 ),
+		m_sun_light_parameters( 0 ),
+		m_eye_ray_corner_parameter( 0 )
+	{
+	}
+
+	virtual ~stage_resolve_lighting( ) { }
+	virtual void execute( ) override;
+
+	bool is_effects_ready( ) const;
+	void render_models(
+		vector<render_surface_instance*>& models,
+		u32& out_num_rendered
+	);
+
+private:
+	res_effect_ptr			m_resolve_lighting_effect;
+	shader_constant_host*	m_wind_info_parameters;
+	shader_constant_host*	m_sun_light_parameters;
+	shader_constant_host*	m_eye_ray_corner_parameter;
+};
+
+STATIC_SIZE_ASSERT( stage_resolve_lighting, 0x20 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_STAGE_RESOLVE_LIGHTING_H_INCLUDED
