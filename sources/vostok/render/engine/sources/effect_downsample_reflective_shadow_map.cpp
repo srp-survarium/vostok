173ad8bf65649d89f823f38c2c0ec0e97320e15f
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:34:19 +0000

    render: admit reflective shadow downsample effect

---
+++ sources/vostok/render/engine/sources/effect_downsample_reflective_shadow_map.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_downsample_reflective_shadow_map.h"
+
+namespace vostok {
+namespace render {
+
+void effect_downsample_reflective_shadow_map::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5280]
+}
+
+} // namespace render
+} // namespace vostok
