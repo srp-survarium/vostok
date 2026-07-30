7e314fcfb8da89392d0161eb5688adef3396f167
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:46:17 +0000

    render: admit system ui effect

---
+++ sources/vostok/render/engine/sources/effect_system_ui.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_system_ui.h"
+
+namespace vostok {
+namespace render {
+
+void effect_system_ui::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bf980]
+}
+
+} // namespace render
+} // namespace vostok
