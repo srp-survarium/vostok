59c2e6a63387a44a2b1da5a3827c17fe9a71f5df
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 13:21:27 +0000

    render: admit ui draw command

---
+++ sources/vostok/render/facade/sources/ui_draw_vertices_command.h
@@ -0,0 +1,53 @@
+#ifndef VOSTOK_RENDER_FACADE_SOURCES_UI_DRAW_VERTICES_COMMAND_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_SOURCES_UI_DRAW_VERTICES_COMMAND_H_INCLUDED
+
+#include <boost/noncopyable.hpp>
+#include <vostok/render/facade/base_command.h>
+#include <vostok/render/facade/sources/ui_renderer.h>
+#include <vostok/vectora.h>
+
+namespace vostok {
+
+namespace memory {
+class base_allocator;
+}
+
+namespace render {
+
+namespace engine {
+class world;
+}
+
+namespace ui {
+
+class draw_vertices_command : public base_command, public boost::noncopyable {
+public:
+	draw_vertices_command(
+		engine::world& render_engine_world,
+		base_scene_view_ptr const& scene_view,
+		vertex const* begin,
+		vertex const* end,
+		memory::base_allocator& allocator,
+		u32 primitives_type,
+		u32 points_type
+	);
+
+	virtual void execute( );
+	virtual void defer_execution( );
+	virtual ~draw_vertices_command( ) { /* no source */ }
+
+private:
+	vectora< vertex > m_vertices;
+	engine::world& m_render_engine_world;
+	base_scene_view_ptr m_scene_view;
+	u32 m_primitives_type;
+	u32 m_points_type;
+};
+
+STATIC_SIZE_ASSERT( draw_vertices_command, 0x74 );
+
+} // namespace ui
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_SOURCES_UI_DRAW_VERTICES_COMMAND_H_INCLUDED
