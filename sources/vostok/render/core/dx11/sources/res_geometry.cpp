5c78b5d2846c3ec48de8fe9b9f50d5a200896c11
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 11:00:18 +0000

    render: admit dx11 geometry resource source

---
+++ sources/vostok/render/core/dx11/sources/res_geometry.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+
+#include <vostok/render/core/dx11/res_geometry.h>
+
+namespace vostok {
+namespace render {
+
+res_geometry::~res_geometry( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6e83c0]
+}
+
+void res_geometry::destroy_impl( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6e8420]
+}
+
+s32 res_geometry::compare( res_geometry const& ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6e82b0]
+	return 0;
+}
+
+void res_geometry::apply( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6e8300]
+}
+
+} // namespace render
+} // namespace vostok
