508def5351539b32ee03e5e46aaf54d738d8fe68
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 09:03:41 +0000

    render: admit sampler slot structure

---
+++ sources/vostok/render/core/dx11/sampler_slot.h
@@ -0,0 +1,33 @@
+#ifndef VOSTOK_RENDER_CORE_DX11_SAMPLER_SLOT_H_INCLUDED
+#define VOSTOK_RENDER_CORE_DX11_SAMPLER_SLOT_H_INCLUDED
+
+#include <vostok/fixed_string.h>
+#include <vostok/render/core/render_include.h>
+
+namespace vostok {
+namespace render {
+
+class sampler_slot {
+public:
+	sampler_slot( ) :
+		slot_id( enum_slot_ind_null ),
+		state( 0 )
+	{
+	}
+
+	sampler_slot( fixed_string<64> const& name, u32 slot_id, ID3D11SamplerState* state ) :
+		name( name ),
+		slot_id( slot_id ),
+		state( state )
+	{
+	}
+
+	fixed_string<64> name;
+	u32 slot_id;
+	ID3D11SamplerState* state;
+};
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_CORE_DX11_SAMPLER_SLOT_H_INCLUDED
