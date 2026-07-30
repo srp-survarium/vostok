17a25b5e3474f9dce1cd1d33ad2e78f5f123195b
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 01:56:56 +0000

    render: admit visibility stage owner

---
+++ sources/vostok/render/engine/sources/stage_visibility.cpp
@@ -0,0 +1,184 @@
+#include "pch.h"
+#include "stage_visibility.h"
+
+#include <vostok/math_aabb.h>
+#include <vostok/math_float4x4.h>
+
+#include "light.h"
+#include "render_surface_instance.h"
+
+namespace vostok {
+namespace render {
+
+float4 aabb_to_occlusion_bound(
+	math::aabb const&,
+	float4x4 const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604bd0]
+	return float4( 1.0f, 1.0f, 1.0f, 1.0f );
+}
+
+stage_visibility::stage_visibility(
+	renderer*			in_renderer,
+	renderer_context*	context
+) :
+	stage								( in_renderer, context ),
+	m_data_ready						( false ),
+	m_occlusion_manager					( 0 ),
+	m_static_bounds_array				( 0 ),
+	m_static_results_array				( 0 ),
+	m_current_occlusion_buffer_size		( 0 ),
+	m_portals_offset_to_results			( 0 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x606490]
+}
+
+stage_visibility::~stage_visibility( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605f20]
+}
+
+void stage_visibility::debug_render( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605f90]
+}
+
+void stage_visibility::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x606050]
+}
+
+bool stage_visibility::query_data( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604680]
+	return false;
+}
+
+void stage_visibility::frustum_culling( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605760]
+}
+
+void stage_visibility::occlusion_culling( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605fb0]
+}
+
+bool stage_visibility::occluded( u32 ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6044a0]
+	return false;
+}
+
+void stage_visibility::get_results_and_prepare_bounds_models( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605600]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_lights( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605470]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_grass( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6052f0]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_decals( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x605150]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_env_probes( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604fb0]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_ambient_volumes( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604e20]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_particles( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604ca0]
+}
+
+void stage_visibility::get_results_and_prepare_bounds_portals( float4*&, u32& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604ae0]
+}
+
+bool is_not_occluded_predicate_light( light_ptr const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604640]
+	return false;
+}
+
+bool is_occluded_predicate_light( light_ptr const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604610]
+	return false;
+}
+
+template <typename T>
+bool is_not_occluded_predicate( T const* const )
+{
+	return false;
+}
+
+template <>
+bool is_not_occluded_predicate<render_surface_instance>(
+	render_surface_instance const* const
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604740]
+	return false;
+}
+
+template <typename T>
+bool is_occluded_predicate( T const* const )
+{
+	return false;
+}
+
+template <>
+bool is_occluded_predicate<render_surface_instance>(
+	render_surface_instance const* const
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x6046d0]
+	return false;
+}
+
+void stage_visibility::gather_statistics( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x604780]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/stage_visibility.h
@@ -0,0 +1,54 @@
+#ifndef VOSTOK_RENDER_ENGINE_STAGE_VISIBILITY_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_STAGE_VISIBILITY_H_INCLUDED
+
+#include <vostok/math_float4.h>
+
+#include "stage.h"
+
+namespace vostok {
+namespace render {
+
+class renderer;
+class renderer_context;
+struct hw_hiz_occlusion_manager;
+
+class stage_visibility : public stage {
+public:
+	stage_visibility( renderer* in_renderer, renderer_context* context );
+	virtual ~stage_visibility( );
+
+	virtual void execute( ) override;
+	virtual void debug_render( ) override;
+
+private:
+	void frustum_culling( );
+	void occlusion_culling( );
+	bool query_data( );
+
+	void get_results_and_prepare_bounds_models( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_lights( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_grass( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_decals( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_env_probes( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_ambient_volumes( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_particles( float4*& out_bounds, u32& out_counter );
+	void get_results_and_prepare_bounds_portals( float4*& out_bounds, u32& out_counter );
+
+	bool occluded( u32 index ) const;
+	void gather_statistics( ) const;
+
+private:
+	bool						m_data_ready;
+	hw_hiz_occlusion_manager*	m_occlusion_manager;
+	float4*						m_static_bounds_array;
+	pbyte						m_static_results_array;
+	u32							m_current_occlusion_buffer_size;
+	u32							m_portals_offset_to_results;
+};
+
+STATIC_SIZE_ASSERT( stage_visibility, 0x28 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_STAGE_VISIBILITY_H_INCLUDED
