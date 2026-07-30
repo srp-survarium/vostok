8a47f0dbdf1bdb59c18592e19a5867505df2bb54
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:00:00 +0000

    render: admit editor texture density effect

---
+++ sources/vostok/render/engine/sources/effect_editor_texture_density.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_EDITOR_TEXTURE_DENSITY_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_EDITOR_TEXTURE_DENSITY_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_editor_texture_density : public effect_material_base {
+public:
+	effect_editor_texture_density( ) { }
+	virtual ~effect_editor_texture_density( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_editor_texture_density, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_EDITOR_TEXTURE_DENSITY_H_INCLUDED
