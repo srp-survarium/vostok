adde211c6d8491bf46dca5bb44cbbc3c2381b9f9
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:53:38 +0000

    render: admit panner distortion material effect

---
+++ sources/vostok/render/engine/sources/effect_distortion_stage_panner_materials.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_DISTORTION_STAGE_PANNER_MATERIALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_DISTORTION_STAGE_PANNER_MATERIALS_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_distortion_stage_panner_materials : public effect_material_base {
+public:
+	effect_distortion_stage_panner_materials( ) { }
+	virtual ~effect_distortion_stage_panner_materials( ) { }
+
+	virtual void compile(
+		effect_compiler&,
+		custom_config_value const&
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_distortion_stage_panner_materials, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_DISTORTION_STAGE_PANNER_MATERIALS_H_INCLUDED
