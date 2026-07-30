59a6b16aab1aa880499d12fadfdfd33299e90ebc
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:06:23 +0000

    render: admit cloud noise helper

---
+++ sources/vostok/render/engine/sources/cloud_noise.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "cloud_noise.h"
+
+namespace vostok {
+namespace render {
+
+float cloud_noise::evaluate( float, float, u32 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f2b20]
+	return 0.0f;
+}
+
+} // namespace render
+} // namespace vostok
