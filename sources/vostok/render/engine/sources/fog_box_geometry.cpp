d5bf541f3b8b33e776a2becf914fc0fde544731e
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 03:57:41 +0000

    render: admit fog box geometry

---
+++ sources/vostok/render/engine/sources/fog_box_geometry.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "fog_box_geometry.h"
+
+namespace vostok {
+namespace render {
+
+fog_box_geometry::fog_box_geometry( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b2e80]
+}
+
+void fog_box_geometry::render( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b2df0]
+}
+
+} // namespace render
+} // namespace vostok
