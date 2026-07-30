d6b62031dc6cdf00b1e7f42bbb84e5b27a56a807
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:03:15 +0000

    render: admit custom config value source

---
+++ sources/vostok/render/core/dx11/sources/custom_config_value.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <vostok/render/core/custom_config_value.h>
+
+namespace vostok {
+namespace render {
+
+bool custom_config_value::value_exists( pcstr ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x73a440]
+	return false;
+}
+
+void custom_config_value::call_data_destructor( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x73a320]
+}
+
+custom_config_value const& custom_config_value::operator[]( pcstr ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x73a380]
+	return *this;
+}
+
+} // namespace render
+} // namespace vostok
