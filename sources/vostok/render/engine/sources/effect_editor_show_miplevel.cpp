c7ccc83e31e43337e8d849bd2269b1ab3ae9670a
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:12:46 +0000

    render: admit editor show miplevel effect

---
+++ sources/vostok/render/engine/sources/effect_editor_show_miplevel.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_show_miplevel.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_show_miplevel::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bb450]
+}
+
+} // namespace render
+} // namespace vostok
