5bc303c2e8a53f9b98c9af4b192bee8399e25d91
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:17:04 +0000

    render: admit editor overdraw accumulation effect

---
+++ sources/vostok/render/engine/sources/effect_editor_accumulate_overdraw.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_accumulate_overdraw.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_accumulate_overdraw::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b4560]
+}
+
+} // namespace render
+} // namespace vostok
