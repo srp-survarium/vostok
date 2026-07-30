7510a6f2232badfee07fe3bd32d06750e4fe875b
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:09:32 +0000

    render: admit three-weight gpu skinning

---
+++ sources/vostok/render/engine/sources/skeleton_mesh_gpu_skinning_3weights.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "skeleton_mesh_gpu_skinning_3weights.h"
+
+namespace vostok {
+namespace render {
+
+skeleton_mesh_gpu_skinning_3weights::skeleton_mesh_gpu_skinning_3weights( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3800]
+}
+
+skeleton_mesh_gpu_skinning_3weights::~skeleton_mesh_gpu_skinning_3weights( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f37c0]
+}
+
+void skeleton_mesh_gpu_skinning_3weights::load(
+	configs::binary_config_value const&,
+	memory::chunk_reader&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3640]
+}
+
+void skeleton_mesh_gpu_skinning_3weights::update( vector< float4x4 > const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3630]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/skeleton_mesh_gpu_skinning_3weights.h
@@ -0,0 +1,27 @@
+#ifndef VOSTOK_RENDER_ENGINE_SKELETON_MESH_GPU_SKINNING_3WEIGHTS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_SKELETON_MESH_GPU_SKINNING_3WEIGHTS_H_INCLUDED
+
+#include <vostok/render/core/untyped_buffer.h>
+#include "render_model_skeleton.h"
+
+namespace vostok {
+namespace render {
+
+class skeleton_mesh_gpu_skinning_3weights : public skeleton_render_surface {
+public:
+	skeleton_mesh_gpu_skinning_3weights( );
+	virtual ~skeleton_mesh_gpu_skinning_3weights( );
+
+	virtual void load( configs::binary_config_value const& properties, memory::chunk_reader& r );
+	virtual void update( vector< float4x4 > const& bones );
+
+private:
+	untyped_buffer_ptr m_vertex_buffer;
+};
+
+STATIC_SIZE_ASSERT( skeleton_mesh_gpu_skinning_3weights, 0xA0 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_SKELETON_MESH_GPU_SKINNING_3WEIGHTS_H_INCLUDED
