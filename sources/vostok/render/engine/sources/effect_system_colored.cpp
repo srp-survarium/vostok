a73075a636c98e26d06ebabc3ac52aacf48d1ba6
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:44:34 +0000

    render: admit system colored effect

---
+++ sources/vostok/render/engine/sources/effect_system_colored.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_system_colored.h"
+
+namespace vostok {
+namespace render {
+
+void effect_system_colored::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bfcd0]
+}
+
+} // namespace render
+} // namespace vostok
