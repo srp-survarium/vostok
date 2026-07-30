461002b1e751f6856bd195f07e0738970b693fb5
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 06:20:13 +0000

    render: admit volume cone forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_volume_cone_base_materials.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_VOLUME_CONE_BASE_MATERIALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_VOLUME_CONE_BASE_MATERIALS_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_fstage_volume_cone_base_materials : public effect_material_base {
+public:
+	effect_fstage_volume_cone_base_materials( ) { }
+	virtual ~effect_fstage_volume_cone_base_materials( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_fstage_volume_cone_base_materials, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_FSTAGE_VOLUME_CONE_BASE_MATERIALS_H_INCLUDED
