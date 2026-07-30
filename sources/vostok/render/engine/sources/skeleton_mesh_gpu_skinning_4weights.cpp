add1e7e705d815656e01239b6ab98bb56694f124
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:10:34 +0000

    render: admit four-weight gpu skinning

---
+++ sources/vostok/render/engine/sources/skeleton_mesh_gpu_skinning_4weights.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "skeleton_mesh_gpu_skinning_4weights.h"
+
+namespace vostok {
+namespace render {
+
+skeleton_mesh_gpu_skinning_4weights::skeleton_mesh_gpu_skinning_4weights( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3a00]
+}
+
+skeleton_mesh_gpu_skinning_4weights::~skeleton_mesh_gpu_skinning_4weights( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f39c0]
+}
+
+void skeleton_mesh_gpu_skinning_4weights::load(
+	configs::binary_config_value const&,
+	memory::chunk_reader&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3840]
+}
+
+void skeleton_mesh_gpu_skinning_4weights::update( vector< float4x4 > const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3830]
+}
+
+} // namespace render
+} // namespace vostok
