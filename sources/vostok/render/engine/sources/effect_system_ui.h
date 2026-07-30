ab5d6f6cbff75e1e67e57e21cdefd25f395e059b
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:46:17 +0000

    render: admit system ui effect

---
+++ sources/vostok/render/engine/sources/effect_system_ui.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_SYSTEM_UI_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_SYSTEM_UI_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_system_ui : public effect_descriptor {
+public:
+	effect_system_ui( ) { }
+	virtual ~effect_system_ui( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_system_ui, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_SYSTEM_UI_H_INCLUDED
