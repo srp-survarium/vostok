295cae9ff0fa155b0e184ed06a8341cbdb35ea3b
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:16:53 +0000

    render: admit SRAA effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_sraa.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_sraa.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_sraa::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bbfa0]
+}
+
+} // namespace render
+} // namespace vostok
