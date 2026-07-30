43119d863a52b2f993eaee21ee27fb97a44b18fb
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 00:54:52 +0000

    render: admit sky dome geometry owner

---
+++ sources/vostok/render/engine/sources/sky_dome_geometry.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "sky_dome_geometry.h"
+
+namespace vostok {
+namespace render {
+
+sky_dome_geometry::sky_dome_geometry( ) :
+	m_stride		( sizeof( vertex_type ) ),
+	m_num_indices	( 0 )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b30a0]
+}
+
+void sky_dome_geometry::draw( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b2f70]
+}
+
+} // namespace render
+} // namespace vostok
