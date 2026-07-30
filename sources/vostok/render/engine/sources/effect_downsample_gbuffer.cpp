b914046b76ea096220034219f04c09f0e5ae5e80
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 00:35:20 +0000

    render: admit gbuffer downsample effect

---
+++ sources/vostok/render/engine/sources/effect_downsample_gbuffer.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_downsample_gbuffer.h"
+
+namespace vostok {
+namespace render {
+
+void effect_downsample_gbuffer::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bda30]
+}
+
+} // namespace render
+} // namespace vostok
