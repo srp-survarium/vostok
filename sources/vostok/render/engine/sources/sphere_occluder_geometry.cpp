ea67f1af461da240eb3ff1b4e80450dd4742f1ae
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 02:02:09 +0000

    render: admit sphere occluder geometry owner

---
+++ sources/vostok/render/engine/sources/sphere_occluder_geometry.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "sphere_occluder_geometry.h"
+
+namespace vostok {
+namespace render {
+
+sphere_occluder_geometry::sphere_occluder_geometry( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b2d00]
+}
+
+void sphere_occluder_geometry::render( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b2c60]
+}
+
+} // namespace render
+} // namespace vostok
