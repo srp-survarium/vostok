912077f64357624c4c09bc01d500a9ceb3a649eb
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:40:41 +0000

    render: restore facade precompiled header

---
+++ sources/vostok/render/facade/sources/pch.h
@@ -0,0 +1,16 @@
+#ifndef VOSTOK_RENDER_FACADE_SOURCES_PCH_H_INCLUDED
+#define VOSTOK_RENDER_FACADE_SOURCES_PCH_H_INCLUDED
+
+#define VOSTOK_RENDER_BUILDING
+
+#ifndef VOSTOK_STATIC_LIBRARIES
+#	define VOSTOK_ENGINE_BUILDING
+#endif // #ifndef VOSTOK_STATIC_LIBRARIES
+
+#define VOSTOK_LOG_MODULE_INITIATOR "render_facade"
+#include <vostok/extensions.h>
+
+#include "logic_allocator.h"
+#include "editor_allocator.h"
+
+#endif // #ifndef VOSTOK_RENDER_FACADE_SOURCES_PCH_H_INCLUDED
