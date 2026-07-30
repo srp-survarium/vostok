563d7946bba5e3ab8fc9ff684cfe5e40f0f392aa
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:22:59 +0000

    render: admit resolve-lighting effect

---
+++ sources/vostok/render/engine/sources/effect_resolve_lighting.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_resolve_lighting.h"
+
+namespace vostok {
+namespace render {
+
+void effect_resolve_lighting::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bd630]
+}
+
+} // namespace render
+} // namespace vostok
