02dfc0a93d194ca20cc8fca6ace0a90b3670e71b
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:55:58 +0000

    render: admit lens flares effect

---
+++ sources/vostok/render/engine/sources/effect_lens_flares.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_lens_flares.h"
+
+namespace vostok {
+namespace render {
+
+void effect_lens_flares::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bbb90]
+}
+
+} // namespace render
+} // namespace vostok
