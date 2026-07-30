ce45bfa6f5de671761a3f7d5ce51c5fb65b46eaf
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:52:12 +0000

    render: admit grass trample effect

---
+++ sources/vostok/render/engine/sources/effect_grass_trample.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_grass_trample.h"
+
+namespace vostok {
+namespace render {
+
+void effect_grass_trample::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bff00]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_grass_trample.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_GRASS_TRAMPLE_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_GRASS_TRAMPLE_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_grass_trample : public effect_descriptor {
+public:
+	effect_grass_trample( ) { }
+	virtual ~effect_grass_trample( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_grass_trample, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_GRASS_TRAMPLE_H_INCLUDED
