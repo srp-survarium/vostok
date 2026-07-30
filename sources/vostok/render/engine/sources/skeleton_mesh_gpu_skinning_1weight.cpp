eb20133029452d4309c7ba0e238bc22c52db06b7
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:07:33 +0000

    render: admit one-weight gpu skinning

---
+++ sources/vostok/render/engine/sources/skeleton_mesh_gpu_skinning_1weight.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "skeleton_mesh_gpu_skinning_1weight.h"
+
+namespace vostok {
+namespace render {
+
+skeleton_mesh_gpu_skinning_1weight::skeleton_mesh_gpu_skinning_1weight( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f33f0]
+}
+
+skeleton_mesh_gpu_skinning_1weight::~skeleton_mesh_gpu_skinning_1weight( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f33b0]
+}
+
+void skeleton_mesh_gpu_skinning_1weight::load(
+	configs::binary_config_value const&,
+	memory::chunk_reader&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3230]
+}
+
+void skeleton_mesh_gpu_skinning_1weight::update( vector< float4x4 > const& )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5f3220]
+}
+
+} // namespace render
+} // namespace vostok
