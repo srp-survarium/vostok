3084c508981bf29a1836c07c58cde2b4fa5beeef
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 01:47:46 +0000

    render: fold shadow batched geometry template

---
+++ sources/vostok/render/engine/sources/shadow_batched_geometry.cpp
@@ -0,0 +1,57 @@
+#include "pch.h"
+#include "shadow_batched_geometry.h"
+
+#include "render_surface.h"
+
+namespace vostok {
+namespace render {
+
+shadow_batched_geometry::shadow_batched_geometry( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x70b890]
+}
+
+void shadow_batched_geometry::add_vertex(
+	batched_vertex_source const&,
+	float3 const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x70b650]
+}
+
+void shadow_batched_geometry::build(
+	vector<render_model_instance_impl_ptr>&
+)
+{
+	struct surface_set {
+		surface_set( ) :
+			surface( 0 )
+		{
+		}
+
+		float4x4		transform;
+		render_surface*	surface;
+	};
+
+	struct sort_predicate {
+		bool operator()( surface_set const&, surface_set const& ) const
+		{
+			// STATE[STUB]
+			// FUNCTION BODY[0x70b630]
+			return false;
+		}
+	};
+
+	typedef vector<surface_set> surfaces_type;
+
+	(void)sizeof( surfaces_type );
+	(void)sizeof( sort_predicate );
+
+	// STATE[STUB]
+	// FUNCTION BODY[0x70b6f0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/shadow_vertex.h
@@ -0,0 +1,25 @@
+#ifndef VOSTOK_RENDER_ENGINE_SHADOW_VERTEX_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_SHADOW_VERTEX_H_INCLUDED
+
+#include <vostok/math_float2.h>
+#include <vostok/math_float3.h>
+
+namespace vostok {
+namespace render {
+
+struct shadow_vertex {
+	shadow_vertex( )
+	{
+	}
+
+	float3	position;
+	float3	object_position;
+	float2	uv;
+};
+
+STATIC_SIZE_ASSERT( shadow_vertex, 0x20 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_SHADOW_VERTEX_H_INCLUDED
