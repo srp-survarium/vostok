5d487a3f0fa529429db38a1a293278b50fb03fee
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 07:11:10 +0000

    render: admit FXAA effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_fxaa.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_fxaa.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_fxaa::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7bc080]
+}
+
+} // namespace render
+} // namespace vostok
