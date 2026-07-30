a5bbeec8fd020172fd18dcc963350242bd3e430b
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 01:00:16 +0000

    render: admit ambient lighting stage

---
+++ sources/vostok/render/engine/sources/stage_ambient_lighting.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "stage_ambient_lighting.h"
+
+namespace vostok {
+namespace render {
+
+stage_ambient_lighting::stage_ambient_lighting(
+	renderer*			in_renderer,
+	renderer_context*	context
+) :
+	stage					( in_renderer, context ),
+	m_ambient_multiplier	( 0.0f ),
+	m_use_probes			( false )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x62a800]
+}
+
+bool stage_ambient_lighting::is_effects_ready( ) const
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x629a20]
+	return false;
+}
+
+void stage_ambient_lighting::execute( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x629a80]
+}
+
+} // namespace render
+} // namespace vostok
