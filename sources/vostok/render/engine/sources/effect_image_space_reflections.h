35f1272cc7ee88d04f336e7d5e32e6eb38716d58
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:48:21 +0000

    render: admit image-space reflections effect

---
+++ sources/vostok/render/engine/sources/effect_image_space_reflections.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_IMAGE_SPACE_REFLECTIONS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_IMAGE_SPACE_REFLECTIONS_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_image_space_reflections : public effect_descriptor {
+public:
+	effect_image_space_reflections( ) { }
+	virtual ~effect_image_space_reflections( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_image_space_reflections, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_IMAGE_SPACE_REFLECTIONS_H_INCLUDED
