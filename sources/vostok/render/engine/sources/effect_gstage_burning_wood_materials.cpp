e332f8b19ca66943d7d2db5899bbbfbd5b40d4ce
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:37:49 +0000

    render: admit burning wood g-stage effect

---
+++ sources/vostok/render/engine/sources/effect_gstage_burning_wood_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_gstage_burning_wood_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_gstage_burning_wood_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x5e9c80]
+}
+
+} // namespace render
+} // namespace vostok
