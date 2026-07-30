3306ead8efcd81912b26c250de744ba91bc3a3c2
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 12:12:18 +0000

    render: admit texture storage source

---
+++ sources/vostok/render/core/dx11/sources/texture_storage.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "texture_storage.h"
+
+namespace vostok {
+namespace render {
+
+texture_storage::texture_storage( )
+	: m_initialized( false )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x782050]
+}
+
+texture_storage::~texture_storage( )
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x781fe0]
+}
+
+} // namespace render
+} // namespace vostok
