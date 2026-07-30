995261b28670f3e624ba033dae4f9f8b4f011383
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 06:13:30 +0000

    render: admit simple water forward material effect

---
+++ sources/vostok/render/engine/sources/effect_fstage_simpe_water_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_fstage_simpe_water_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_fstage_simpe_water_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b7a40]
+}
+
+} // namespace render
+} // namespace vostok
