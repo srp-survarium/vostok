2387033caa6d4aead02336defb9366952eecd1b2
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 04:55:58 +0000

    render: admit lens flares effect

---
+++ sources/vostok/render/engine/sources/effect_lens_flares.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_LENS_FLARES_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_LENS_FLARES_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_lens_flares : public effect_descriptor {
+public:
+	effect_lens_flares( ) { }
+	virtual ~effect_lens_flares( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	custom_config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_lens_flares, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_LENS_FLARES_H_INCLUDED
