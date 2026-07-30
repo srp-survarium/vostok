59792474c8a8df05b312f1a41de9e7ada16dc758
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:04:01 +0000

    render: admit editor geometry complexity effect

---
+++ sources/vostok/render/engine/sources/effect_editor_geometry_complexity.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_editor_geometry_complexity.h"
+
+namespace vostok {
+namespace render {
+
+void effect_editor_geometry_complexity::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b41c0]
+}
+
+} // namespace render
+} // namespace vostok
