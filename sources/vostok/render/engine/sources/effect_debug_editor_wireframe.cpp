ea8be3f55047e0b3b3ffd638c8cba24918b93cd1
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 05:45:48 +0000

    render: admit debug editor wireframe effect

---
+++ sources/vostok/render/engine/sources/effect_debug_editor_wireframe.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_debug_editor_wireframe.h"
+
+namespace vostok {
+namespace render {
+
+void effect_debug_editor_wireframe::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b94f0]
+}
+
+} // namespace render
+} // namespace vostok
