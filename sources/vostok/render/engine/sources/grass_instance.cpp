608497f5d1e6504a67db18a9c93af7e1abc53802
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 03:17:24 +0000

    render: admit grass instance child

---
+++ sources/vostok/render/engine/sources/grass_instance.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "grass_instance.h"
+
+namespace vostok {
+namespace render {
+
+grass_instance::grass_instance(
+	u32 in_id,
+	grass_template* in_template,
+	math::color const& in_color,
+	float4x4 const& in_transform,
+	u8 in_layer,
+	float in_wind_scale
+) :
+	m_template( in_template ),
+	m_color( in_color ),
+	m_transform( in_transform ),
+	m_wind_scale( in_wind_scale ),
+	m_index( in_id ),
+	m_layer_id( in_layer )
+{
+	// FUNCTION BODY[0x5f5550]
+}
+
+} // namespace render
+} // namespace vostok
