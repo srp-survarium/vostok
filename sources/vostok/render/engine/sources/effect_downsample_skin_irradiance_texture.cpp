3dc981855e2ced3c9ac64c2bbc96d827e2db6198
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 05:56:24 +0000

    render: admit skin irradiance downsample effect

---
+++ sources/vostok/render/engine/sources/effect_downsample_skin_irradiance_texture.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_downsample_skin_irradiance_texture.h"
+
+namespace vostok {
+namespace render {
+
+void effect_downsample_skin_irradiance_texture::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b5320]
+}
+
+} // namespace render
+} // namespace vostok
