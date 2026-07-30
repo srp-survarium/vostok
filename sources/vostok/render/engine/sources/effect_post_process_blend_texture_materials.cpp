38ef1c5d2eeb0aaff3b1ffaed8a4c667c8324e2f
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-30 07:05:40 +0000

    render: admit postprocess blend-texture effect

---
+++ sources/vostok/render/engine/sources/effect_post_process_blend_texture_materials.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_post_process_blend_texture_materials.h"
+
+namespace vostok {
+namespace render {
+
+void effect_post_process_blend_texture_materials::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b9c10]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_post_process_blend_texture_materials.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_BLEND_TEXTURE_MATERIALS_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_BLEND_TEXTURE_MATERIALS_H_INCLUDED
+
+#include <vostok/render/core/effect_descriptor.h>
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_post_process_blend_texture_materials : public effect_descriptor {
+public:
+	effect_post_process_blend_texture_materials( ) { }
+	virtual ~effect_post_process_blend_texture_materials( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_post_process_blend_texture_materials, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_POST_PROCESS_BLEND_TEXTURE_MATERIALS_H_INCLUDED
