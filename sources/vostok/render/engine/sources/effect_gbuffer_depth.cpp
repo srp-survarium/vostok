70b2ed47981c664641e90e2cd6628c6a9543a961
sushi-shi <sushi.is.shy@gmail.com>
2026-07-30 04:54:01 +0000

    render: admit gbuffer depth effect

---
+++ sources/vostok/render/engine/sources/effect_gbuffer_depth.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "effect_gbuffer_depth.h"
+
+namespace vostok {
+namespace render {
+
+void effect_gbuffer_depth::compile(
+	effect_compiler&,
+	custom_config_value const&
+)
+{
+	// STATE[STUB]
+	// FUNCTION BODY[0x7b51d0]
+}
+
+} // namespace render
+} // namespace vostok
---
+++ sources/vostok/render/engine/sources/effect_gbuffer_depth.h
@@ -0,0 +1,28 @@
+#ifndef VOSTOK_RENDER_ENGINE_EFFECT_GBUFFER_DEPTH_H_INCLUDED
+#define VOSTOK_RENDER_ENGINE_EFFECT_GBUFFER_DEPTH_H_INCLUDED
+
+#include "effect_material_base.h"
+
+namespace vostok {
+namespace render {
+
+class custom_config_value;
+class effect_compiler;
+
+class effect_gbuffer_depth : public effect_material_base {
+public:
+	effect_gbuffer_depth( ) { }
+	virtual ~effect_gbuffer_depth( ) { }
+
+	virtual void compile(
+		effect_compiler&			compiler,
+		custom_config_value const&	config
+	) override;
+};
+
+STATIC_SIZE_ASSERT( effect_gbuffer_depth, 0x4 );
+
+} // namespace render
+} // namespace vostok
+
+#endif // #ifndef VOSTOK_RENDER_ENGINE_EFFECT_GBUFFER_DEPTH_H_INCLUDED
