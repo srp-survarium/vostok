3c517d1f61a5bb41489259da16984d24698fd61c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-07 17:35:48 +0000

    match(render): restore portal selection cone

--- sources/vostok/math_frustum.h
+++ sources/vostok/math_frustum.h
@@ -16,8 +16,11 @@ namespace math {
 class VOSTOK_CORE_API frustum : public cuboid {
 public:
 	inline				frustum		( ) { }
+	inline explicit		frustum		( plane const (&planes)[plane_count] ) : cuboid( planes ) { }
 						frustum		( float4x4 const& view_multiplied_by_projection );
 }; // class frustum
+
+VOSTOK_CORE_API bool get_frustum_vertices( frustum const& f, float3 (&vertices)[8] );
 
 } // namespace math
 } // namespace vostok
