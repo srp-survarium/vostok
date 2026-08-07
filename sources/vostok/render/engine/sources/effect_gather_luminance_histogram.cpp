597861433a830028a6e24346bae562070574a007
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-07 07:29:36 +0000

    match(render): restore luminance histogram downsample

--- sources/vostok/render/engine/sources/effect_gather_luminance_histogram.cpp
+++ sources/vostok/render/engine/sources/effect_gather_luminance_histogram.cpp
@@ -30,7 +30,14 @@ void effect_gather_luminance_histogram::compile(
 	compiler.begin_technique();
 		compiler.begin_pass("gather_luminance2", NULL, "gather_luminance_histogram", shader_configuration(), NULL);
 			compiler.set_depth( false, false);
-			compiler.set_texture("t_frame_luminance", r2_rt_frame_luminance, 0, false, 0);
+			compiler.set_texture("t_frame_luminance", r2_rt_frame_luminance, 0, false, u32(-1));
+		compiler.end_pass();
+	compiler.end_technique();
+
+	compiler.begin_technique();
+		compiler.begin_pass("gather_luminance2", NULL, "gather_luminance_histogram_downsample_scene_color", shader_configuration(), NULL);
+			compiler.set_depth( false, false);
+			compiler.set_texture("t_frame_color", r2_rt_generic0, 0, false, u32(-1));
 		compiler.end_pass();
 	compiler.end_technique();
 }
