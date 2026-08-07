cd104f82c6dbc176455a7b83163d9883636fd422
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-07 03:32:11 +0000

    match(render): reconstruct sun effect

--- sources/vostok/render/engine/sources/effect_sun.h
+++ sources/vostok/render/engine/sources/effect_sun.h
@@ -11,18 +11,6 @@ class effect_compiler;
 
 class effect_sun : public effect_descriptor {
 public:
-	enum
-	{
-		tech_sun_near,
-		tech_sun_near_mat2,
-		tech_sun_near_clip,
-		tech_sun_near_mat2_clip,
-		tech_sun_far
-	};
-
-	effect_sun( ) { }
-	virtual ~effect_sun( ) { }
-
 	virtual void compile(
 		effect_compiler&			compiler,
 		custom_config_value const&	config
