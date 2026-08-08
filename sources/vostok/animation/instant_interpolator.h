1a2164966e2abcef08e5f4b8f096458606fbfd96
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-08 17:39:53 +0000

    animation: restore animation node caller cone

--- sources/vostok/animation/instant_interpolator.h
+++ sources/vostok/animation/instant_interpolator.h
@@ -20,8 +20,8 @@ public:
 	virtual	float	interpolated_value	( float current_transition_time ) const;
 	virtual	float	transition_time		( ) const;
 
-	virtual instant_interpolator* clone ( mixing::n_ary_tree_transition_tree_constructor& constructor ) const;
 	virtual	instant_interpolator* clone	( mutable_buffer& buffer ) const;
+	virtual instant_interpolator* clone ( mixing::n_ary_tree_transition_tree_constructor& constructor ) const;
 
 	virtual void	accept				( interpolator_visitor& visitor ) const;
 
