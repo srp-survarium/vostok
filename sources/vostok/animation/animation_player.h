5e860c7d750122c9d45a9d13f16c05039312081f
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-09 21:56:17 +0000

    animation: retire player reachability anchor

--- sources/vostok/animation/animation_player.h
+++ sources/vostok/animation/animation_player.h
@@ -13,9 +13,6 @@
 struct transform_getter;
 
 namespace vostok {
-
-void anchor_animation_player( );
-
 namespace render {
 namespace debug {
 	class renderer;
@@ -44,7 +41,6 @@ enum reserved_channel_ids_enum {
 class VOSTOK_ANIMATION_API animation_player
 {
 	friend struct ::transform_getter;
-	friend void ::vostok::anchor_animation_player( );
 
 public:
 						animation_player		( );
