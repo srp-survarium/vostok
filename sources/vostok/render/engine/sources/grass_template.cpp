b906cfff40d757d09368934baa6dfcd3d661f776
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-11 12:41:11 +0000

    match(render): align grass template constructor records

--- sources/vostok/render/engine/sources/grass_template.cpp
+++ sources/vostok/render/engine/sources/grass_template.cpp
@@ -7,9 +7,7 @@ namespace render {
 
 grass_template::grass_template( u32 const in_index, grass_render_model_ptr const& in_model_ptr )
 {
-	m_render_model = in_model_ptr;
-	m_index = in_index;
-	m_sizes = float3( 0.f, 0.f, 0.f );
+	m_render_model = in_model_ptr; m_index = in_index; m_sizes = float3( 0.f, 0.f, 0.f );
 }
 
 grass_template::~grass_template( )
