dc74f3e9dc8537e40bc00a39b6e8c0bd44945e93
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-11 10:00:22 +0000

    animation: match addition lexeme destructor layout

--- sources/vostok/animation/mixing_addition_lexeme_inline.h
+++ sources/vostok/animation/mixing_addition_lexeme_inline.h
@@ -30,10 +30,6 @@ inline addition_lexeme* addition_lexeme::cloned_in_buffer	( )
 	return				base_lexeme::cloned_in_buffer< addition_lexeme >( );
 }
 
-inline addition_lexeme::~addition_lexeme						( )
-{
-}
-
 template < typename T1, typename T2 >
 inline addition_lexeme& operator +							( T1& left, T2& right )
 {
