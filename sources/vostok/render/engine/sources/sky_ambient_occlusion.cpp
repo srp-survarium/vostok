2ed1cffbcbef61f35cced0dc9161e0dee7ac9798
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-11 10:29:30 +0000

    match(render): use incoming sky occlusion properties

--- sources/vostok/render/engine/sources/sky_ambient_occlusion.cpp
+++ sources/vostok/render/engine/sources/sky_ambient_occlusion.cpp
@@ -29,14 +29,14 @@ void sky_ambient_occlusion::set_properties(
 {
 	m_properties = in_properties;
 
-	if (m_properties.texture_invalidated)
+	if (in_properties.texture_invalidated)
 	{
 		m_texture = 0;
 		m_texture = resource_manager::ref().create_texture(m_properties.texture_name.c_str(), 0, 0, false, true, true, u32(-1));
 	}
 
-	float4x4 new_transform = math::create_translation(m_properties.location);
-	new_transform.set_scale(float3(m_properties.width, m_properties.height, m_properties.depth));
+	float4x4 new_transform = math::create_translation(in_properties.location);
+	new_transform.set_scale(float3(in_properties.width, in_properties.height, in_properties.depth));
 
 	m_aabb = math::create_identity_aabb();
 	m_aabb.modify(new_transform);
