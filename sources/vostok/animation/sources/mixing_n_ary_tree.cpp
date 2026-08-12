c0b83c03ff0e8a4b67c8e6ef7e9ba4fd5a73cb7d
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-12 17:55:04 +0000

    matching: restore animation pinned temporary lifetime

--- sources/vostok/animation/sources/mixing_n_ary_tree.cpp
+++ sources/vostok/animation/sources/mixing_n_ary_tree.cpp
@@ -153,7 +153,8 @@ void n_ary_tree::set_object_transform( n_ary_tree_animation_node& animation_node
 	current_frame_position frame_position;
 	frame frame_transform;
 	{
-		cubic_spline_skeleton_animation_pinned pinned_animation( interval.animation( ) );
+		cubic_spline_skeleton_animation_pinned pinned_animation =
+			cubic_spline_skeleton_animation_pinned( interval.animation( ) );
 		frame_transform = pinned_animation->bone( u32( 0 ) ).bone_frame(
 			state.animation_time * default_fps,
 			frame_position
