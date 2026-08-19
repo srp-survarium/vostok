f86d90a735c4a9794f2d2bcec615353c95d587fd
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-19 16:39:36 +0000

    match core binary config cook

--- sources/vostok/core/sources/configs_binary_config_cook.cpp
+++ sources/vostok/core/sources/configs_binary_config_cook.cpp
@@ -158,10 +158,10 @@ void binary_config_cook::on_fs_iterators_ready	(queries_result & results)
 	fs_new::virtual_path_string				source_path;
 	make_source_path							(& source_path, converted_path);
 
-	fs_new::physical_path_info const source_info	= get_physical_path_info(results[0].get_result_iterator());
+	vfs::vfs_iterator const & source_path_it	= results[0].get_result_iterator();
 
 	#ifndef MASTER_GOLD
-	if ( !source_info.exists() )
+	if ( !source_path_it )
 	{
 		parent->finish_query				(query_result_for_user::error_type_file_not_found, assert_on_fail_false);
 		return;
@@ -180,6 +180,7 @@ void binary_config_cook::on_fs_iterators_ready	(queries_result & results)
 
 
 
+	fs_new::physical_path_info const source_info	= get_physical_path_info(source_path_it);
 	fs_new::physical_path_info const converted_info	=	get_physical_path_info(converted_path_it);
 
 
