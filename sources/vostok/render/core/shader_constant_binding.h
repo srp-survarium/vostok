49cc881d112f2695c393911316fa5a513285d1cb
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-24 16:06:42 +0000

    Restore shader constant binding header split

--- sources/vostok/render/core/shader_constant_binding.h
+++ sources/vostok/render/core/shader_constant_binding.h
@@ -11,33 +11,13 @@ namespace render {
 class shader_constant_binding {
 public:
 	template < typename T >
-	shader_constant_binding( shared_string const& name, T const* source ) :
-		m_source( const_cast<T*>( source ), constant_type_traits<T>::size ),
-		m_name( name ),
-		m_type( static_cast<enum_constant_type>( constant_type_traits<T>::type ) ),
-		m_class_id( static_cast<enum_constant_class>( constant_type_traits<T>::class_id ) )
-	{
-	}
-
-	shader_constant_binding( shader_constant_binding const& other ) :
-		m_source( other.source( ) ),
-		m_name( other.name( ) ),
-		m_type( other.type( ) ),
-		m_class_id( other.class_id( ) )
-	{
-	}
-
-	shader_constant_binding& operator=( shader_constant_binding const& other )
-	{
-		this->~shader_constant_binding( );
-		new (this) shader_constant_binding( other );
-		return *this;
-	}
-
-	shared_string const& name( ) const { return m_name; }
-	shader_constant_source const& source( ) const { return m_source; }
-	enum_constant_class class_id( ) const { return m_class_id; }
-	enum_constant_type type( ) const { return m_type; }
+	inline shader_constant_binding( shared_string const& name, T const* source );
+	inline shader_constant_binding( shader_constant_binding const& other );
+	inline shader_constant_binding& operator=( shader_constant_binding const& other );
+	inline shared_string const& name( ) const;
+	inline shader_constant_source const& source( ) const;
+	inline enum_constant_class class_id( ) const;
+	inline enum_constant_type type( ) const;
 
 private:
 	shader_constant_source m_source;
@@ -46,17 +26,13 @@ private:
 	enum_constant_class m_class_id;
 };
 
-inline bool operator==(
-	shader_constant_binding const& left,
-	shader_constant_binding const& right
-)
-{
-	return left.name( ) == right.name( );
-}
+inline bool operator==( shader_constant_binding const& left, shader_constant_binding const& right );
 
 STATIC_SIZE_ASSERT( shader_constant_binding, 0x14 );
 
 } // namespace render
 } // namespace vostok
+
+#include <vostok/render/core/shader_constant_binding_inline.h>
 
 #endif // #ifndef VOSTOK_RENDER_CORE_SHADER_CONSTANT_BINDING_H_INCLUDED
