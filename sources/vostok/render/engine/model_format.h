392c006ecfded9bcb9456300a877e48b2f121b93
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-09-02 14:36:39 +0000

    render: match mesh type enum order

--- sources/vostok/render/engine/model_format.h
+++ sources/vostok/render/engine/model_format.h
@@ -35,13 +35,13 @@ enum mesh_type_enum {
 	mt_static_submesh			= 0x00,
 	mt_static_mesh				= 0x01,
 	mt_static_submesh_colored	= 0x02,
+	mt_user_mesh_editable		= 0x64,
+	mt_user_mesh_wire			= 0x65,
 	mt_skinned_mesh				= 0x28,
 	mt_skinned_submesh_1w		= 0x2c,
 	mt_skinned_submesh_2w		= 0x2d,
 	mt_skinned_submesh_3w		= 0x2e,
 	mt_skinned_submesh_4w		= 0x2f,
-	mt_user_mesh_editable		= 0x64,
-	mt_user_mesh_wire			= 0x65,
 	mt_grass_mesh				= 0xc8
 };
 
